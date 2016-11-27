cf062accd3ce7f0b03150c495e8ad1c3f2fe1a63
TyrboVenik <vjunnikov2010@yandex.ru>
2016-11-27 22:07:52 +0300

    LastReplicatorTest

---
+++ РК#3/zagar_server/src/main/java/replication/TestJsonStateReplicator.java
@@ -0,0 +1,45 @@
+package replication;
+
+import main.ApplicationContext;
+import matchmaker.MatchMaker;
+import model.GameSession;
+import model.Player;
+import network.ClientConnections;
+import network.packets.PacketReplicate;
+import org.eclipse.jetty.websocket.api.Session;
+import protocol.model.Cell;
+import protocol.model.Food;
+
+import java.io.*;
+import java.util.Map;
+
+/**
+ * Created by venik on 27.11.16.
+ */
+public class TestJsonStateReplicator implements Replicator {
+
+    @Override
+    public void replicate() {
+        String json = new String();
+        try (InputStream in = new FileInputStream(new File("src/main/resources/file.json"));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in))
+        ) {
+            json = reader.readLine();
+        }catch (Exception e){}
+
+        for (GameSession gameSession : ApplicationContext.instance().get(MatchMaker.class).getActiveGameSessions()) {
+            for (Map.Entry<Player, Session> connection : ApplicationContext.instance().get(ClientConnections.class).getConnections()) {
+                if (gameSession.getPlayers().contains(connection.getKey())) {
+                    try {
+                        new PacketReplicate(json).write(connection.getValue());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
