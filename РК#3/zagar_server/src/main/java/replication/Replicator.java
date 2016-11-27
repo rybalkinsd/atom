e8e0c5de7faea2afc1bddf752469298a9ebdae4c
Dmitriev_Svuatoslav <emerld@mail.ru>
2016-11-28 01:46:12 +0300

    Leaderboard improvements

--- РК#3/zagar_server/src/main/java/replication/Replicator.java
+++ РК#3/zagar_server/src/main/java/replication/Replicator.java
@@ -1,6 +1,19 @@
 package replication;
 
+import main.ApplicationContext;
+import matchmaker.MatchMaker;
+import model.GameSession;
 import model.Player;
+import network.ClientConnections;
+import network.packets.PacketLeaderBoard;
+import network.packets.PacketReplicate;
+import org.eclipse.jetty.websocket.api.Session;
+import utils.JSONDeserializationException;
+import utils.JSONHelper;
+import utils.PropertiesReader;
+
+import java.io.*;
+import java.util.Map;
 
 /**
  * @author Alpi
@@ -8,4 +21,27 @@ import model.Player;
  */
 public interface Replicator {
   void replicate();
+
+  default void sendLeaderboard() {
+    String leaderboard = new String();
+        try (InputStream in = new FileInputStream(new File(new PropertiesReader("src/main/resources/config.properties").getStringProperty("leaderboard")));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in))
+        ) {
+            leaderboard = reader.readLine();
+        }catch (Exception e){}
+    for (GameSession gameSession : ApplicationContext.instance().get(MatchMaker.class).getActiveGameSessions()) {
+      for (Map.Entry<Player, Session> connection : ApplicationContext.instance().get(ClientConnections.class).getConnections()) {
+        if (gameSession.getPlayers().contains(connection.getKey())) {
+          try {
+            new PacketLeaderBoard(JSONHelper.fromJSON(leaderboard,String[].class)).write(connection.getValue());
+          } catch (IOException e) {
+            e.printStackTrace();
+          } catch (JSONDeserializationException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    }
+  }
+
 }
--- РК#3/zagar_server/src/main/java/replication/TestJsonStateReplicator.java
+++ РК#3/zagar_server/src/main/java/replication/TestJsonStateReplicator.java
@@ -31,25 +31,25 @@ public class TestJsonStateReplicator implements Replicator {
             replicationJson = reader.readLine();
         }catch (Exception e){}
 
-        String leaderJson = new String();
+        /*String leaderJson = new String();
         try (InputStream in = new FileInputStream(new File(new PropertiesReader("src/main/resources/config.properties").getStringProperty("leaderboard")));
              BufferedReader reader = new BufferedReader(new InputStreamReader(in))
         ) {
             leaderJson = reader.readLine();
-        }catch (Exception e){}
+        }catch (Exception e){}*/
 
         for (GameSession gameSession : ApplicationContext.instance().get(MatchMaker.class).getActiveGameSessions()) {
             for (Map.Entry<Player, Session> connection : ApplicationContext.instance().get(ClientConnections.class).getConnections()) {
                 if (gameSession.getPlayers().contains(connection.getKey())) {
                     try {
                         new PacketReplicate(replicationJson).write(connection.getValue());
-                        String[] t = JSONHelper.fromJSON(leaderJson,String[].class);
-                        new PacketLeaderBoard(JSONHelper.fromJSON(leaderJson,String[].class)).write(connection.getValue());
+                        //String[] t = JSONHelper.fromJSON(leaderJson,String[].class);
+                        //new PacketLeaderBoard(JSONHelper.fromJSON(leaderJson,String[].class)).write(connection.getValue());
                     } catch (IOException e) {
                         e.printStackTrace();
-                    } catch (JSONDeserializationException e) {
+                    } /*catch (JSONDeserializationException e) {
                         e.printStackTrace();
-                    }
+                    }*/
                 }
             }
 
