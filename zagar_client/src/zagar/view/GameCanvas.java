7593759e4c9e68b945bd583fa9ea4d8200309d31
eugene <kashirin.e@list.ru>
2016-11-29 22:21:45 +0300

    Add additional condition to canvas grid render

--- zagar_client/src/zagar/view/GameCanvas.java
+++ zagar_client/src/zagar/view/GameCanvas.java
@@ -1,18 +1,12 @@
 package zagar.view;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import zagar.Game;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.ConcurrentModificationException;
-
-import javax.swing.JPanel;
 
 public class GameCanvas extends JPanel {
   private static final long serialVersionUID = 5570080027060608254L;
@@ -20,6 +14,8 @@ public class GameCanvas extends JPanel {
   private Font font = new Font("Ubuntu", Font.BOLD, 30);
   private Font fontLB = new Font("Ubuntu", Font.BOLD, 25);
   public Font fontCells = new Font("Ubuntu", Font.BOLD, 18);
+
+  public static final Logger log = LogManager.getLogger(GameCanvas.class);
 
   public GameCanvas() {
     screen = new BufferedImage(GameFrame.size.width, GameFrame.size.height, BufferedImage.TYPE_INT_ARGB);
@@ -30,11 +26,13 @@ public class GameCanvas extends JPanel {
 
   @Override
   protected void paintComponent(Graphics g) {
+    log.debug("paintComponentCall();");
     super.paintComponent(g);
     render();
   }
 
   public void render() {
+    log.debug("CANVAS RENDER");
     Graphics ggg = screen.getGraphics();
     Graphics2D g = ((Graphics2D) ggg);
     g.setColor(new Color(255, 255, 255));
@@ -42,13 +40,14 @@ public class GameCanvas extends JPanel {
 
     g.setColor(new Color(220, 220, 220));
 
+    log.debug("ANTIALIASING SET");
     if (Game.fps < 30) {
       g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
     } else {
       g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     }
 
-    if (Game.player.size() > 0) {
+    if (Game.player.size() > 0 && Game.zoom > 0.0) {
       int size = 1;
 
       float avgX = 0;
@@ -64,22 +63,37 @@ public class GameCanvas extends JPanel {
       avgX /= Game.player.size();
       avgY /= Game.player.size();
 
+
       g.setStroke(new BasicStroke(2));
 
-      for (double i = avgX - (GameFrame.size.width / 2) / Game.zoom; i < avgX + (GameFrame.size.width / 2) / Game.zoom; i += 100) {
-        i = (int) (i / 100) * 100;
-        int x = (int) ((i - avgX) * Game.zoom) + GameFrame.size.width / 2 - size / 2;
+      log.debug("RENDER GRID");
+      log.debug("ZOOM: " + Game.zoom);
+      log.debug("AVGX: " + avgX + "; AVGY: " + avgY);
+      int j;
+      for (
+              double i = avgX - (GameFrame.size.width / 2) / Game.zoom;
+              i < avgX + (GameFrame.size.width / 2) / Game.zoom;
+              i += 100
+              ) {
+        j = (int) (i / 100) * 100;
+//        i = (int) (i / 100) * 100;
+        int x = (int) ((j - avgX) * Game.zoom) + GameFrame.size.width / 2 - size / 2;
         g.drawLine((int) x, (int) Game.minSizeY, (int) x, (int) Game.maxSizeY);
       }
-      for (double i = avgY - (GameFrame.size.height / 2) / Game.zoom; i < avgY + (GameFrame.size.height / 2) / Game.zoom; i += 100) {
-        i = (int) (i / 100) * 100;
-        int y = (int) ((i - avgY) * Game.zoom) + GameFrame.size.height / 2 - size / 2;
+      for (
+              double i = avgY - (GameFrame.size.height / 2) / Game.zoom;
+              i < avgY + (GameFrame.size.height / 2) / Game.zoom;
+              i += 100
+              ) {
+        j = (int) (i / 100) * 100;
+        int y = (int) ((j - avgY) * Game.zoom) + GameFrame.size.height / 2 - size / 2;
         g.drawLine((int) Game.minSizeX, (int) y, (int) Game.maxSizeX, (int) y);
       }
     }
 
     g.setFont(fontCells);
 
+    log.debug("RENDER CELLS");
     for (int i2 = 0; i2 < Game.cells.length; i2++) {
       Cell cell = Game.cells[i2];
       if (cell != null) {
@@ -90,6 +104,7 @@ public class GameCanvas extends JPanel {
       }
     }
 
+    log.debug("RENDER LEADERBOARD");
     g.setFont(font);
 
     String scoreString = "Score: " + Game.score;
@@ -120,9 +135,11 @@ public class GameCanvas extends JPanel {
 
     g.dispose();
 
-    Graphics gg = this.getGraphics();
+    Graphics gg = getGraphics();
     gg.drawImage(screen, 0, 0, null);
     gg.dispose();
+
+    log.debug("CANVAS RENDER OK");
   }
 
   private int getStringWidth(Graphics2D g, String string) {
