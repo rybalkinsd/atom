0031abce8eb3c10fe9e13ef7935efa747a41b2c9
zamakhaevV <zamakhaev_vlad@bk.ru>
2017-02-25 19:26:38 +0300

    Fixed methods and test

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -16,7 +16,14 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = values[0];
+
+        for (int i = 0; i < values.length; ++i){
+            if (values[i] > max)
+                max = values[i];
+        }
+        
+        return max;
     }
 
     /**
@@ -26,7 +33,12 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        long sum = 0L;
+
+        for(int i = 0; i < values.length; ++i)
+            sum += values[i];
+
+        return sum;
     }
 
 
