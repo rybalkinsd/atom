fe604a9bf0a3c4a350e47bdaf956ca968beaaa97
Alex Mogilevskiy <alexplusm@ya.ru>
2017-02-26 14:55:32 +0300

    fix

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -33,7 +33,12 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        int sum = 0;
+        for (int i = 0; i < values.length; ++i)  {
+            sum += values[i];
+        }
+        return sum;
     }
 
 
