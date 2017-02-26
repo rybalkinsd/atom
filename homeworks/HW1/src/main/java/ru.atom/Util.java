f226d604584e18a23d44fcec8fbd26e48e71794a
Ксения Кирпичникова < >
2017-02-26 13:45:27 +0300

    Исправлен Style(2)

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -8,12 +8,12 @@ package ru.atom;
 
 public class Util {
 
-        /**
-        * Returns the greatest of {@code int} values.
-        *
-        * @param values an argument. Assume values.length > 0.
-        * @return the largest of values.
-        */
+    /**
+    * Returns the greatest of {@code int} values.
+    *
+    * @param values an argument. Assume values.length > 0.
+    * @return the largest of values.
+    */
 
     public static int max(int[] values) {
         if (values.length > 0) {
@@ -28,12 +28,12 @@ public class Util {
         throw new UnsupportedOperationException();
     }
 
-        /**
-        * Returns the sum of all {@code int} values.
-        *
-        * @param values an argument. Assume values.length > 0.
-        * @return the sum of all values.
-        */
+    /**
+    * Returns the sum of all {@code int} values.
+    *
+    * @param values an argument. Assume values.length > 0.
+    * @return the sum of all values.
+    */
 
     public static long sum(int[] values) {
         if (values.length > 0) {
