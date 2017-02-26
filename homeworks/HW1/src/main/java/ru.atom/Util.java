6ce2e4f46a57f4ecfaa7df993bb6cdf0bc9f9e02
Ксения Кирпичникова < >
2017-02-26 13:36:50 +0300

    Написаны функции

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -1,33 +1,49 @@
 package ru.atom;
 
-/**
- * In this assignment you need to implement the following util methods.
- * Note:
- *  throw new UnsupportedOperationException(); - is just a stub
- */
-public class Util {
-
-
-
     /**
-     * Returns the greatest of {@code int} values.
-     *
-     * @param values an argument. Assume values.length > 0.
-     * @return the largest of values.
-     */
-    public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+    * In this assignment you need to implement the following util methods.
+    * Note:
+    *  throw new UnsupportedOperationException(); - is just a stub
+    */
+
+    public class Util {
+
+        /**
+        * Returns the greatest of {@code int} values.
+        *
+        * @param values an argument. Assume values.length > 0.
+        * @return the largest of values.
+        */
+
+        public static int max(int[] values) {
+            if (values.length > 0) {
+                int max = values[0];
+                for (int i = 1; i < values.length; i++) {
+                    if (max < values[i]){
+                        max = values[i];
+                    }
+                }
+                return max;
+            }
+            throw new UnsupportedOperationException();
+        }
+
+        /**
+        * Returns the sum of all {@code int} values.
+        *
+        * @param values an argument. Assume values.length > 0.
+        * @return the sum of all values.
+        */
+
+        public static long sum(int[] values) {
+            if (values.length > 0) {
+                int sum = 0;
+                for (int i = 0;i < values.length;i++) {
+                    sum += values[i];
+                }
+                return sum;
+            }
+            throw new UnsupportedOperationException();
+        }
+
     }
-
-    /**
-     * Returns the sum of all {@code int} values.
-     *
-     * @param values an argument. Assume values.length > 0.
-     * @return the sum of all values.
-     */
-    public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
-    }
-
-
-}
