80eadac1a9480b95835582fd69cc1de1cdeb1cc8
Ксения Кирпичникова < >
2017-02-26 13:43:23 +0300

    Исправлен Style

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -1,12 +1,12 @@
 package ru.atom;
 
-    /**
-    * In this assignment you need to implement the following util methods.
-    * Note:
-    *  throw new UnsupportedOperationException(); - is just a stub
-    */
+/**
+* In this assignment you need to implement the following util methods.
+* Note:
+*  throw new UnsupportedOperationException(); - is just a stub
+*/
 
-    public class Util {
+public class Util {
 
         /**
         * Returns the greatest of {@code int} values.
@@ -15,18 +15,18 @@ package ru.atom;
         * @return the largest of values.
         */
 
-        public static int max(int[] values) {
-            if (values.length > 0) {
-                int max = values[0];
-                for (int i = 1; i < values.length; i++) {
-                    if (max < values[i]){
-                        max = values[i];
-                    }
+    public static int max(int[] values) {
+        if (values.length > 0) {
+            int max = values[0];
+            for (int i = 1; i < values.length; i++) {
+                if (max < values[i]) {
+                    max = values[i];
                 }
-                return max;
             }
-            throw new UnsupportedOperationException();
+            return max;
         }
+        throw new UnsupportedOperationException();
+    }
 
         /**
         * Returns the sum of all {@code int} values.
@@ -35,15 +35,15 @@ package ru.atom;
         * @return the sum of all values.
         */
 
-        public static long sum(int[] values) {
-            if (values.length > 0) {
-                int sum = 0;
-                for (int i = 0;i < values.length;i++) {
-                    sum += values[i];
-                }
-                return sum;
+    public static long sum(int[] values) {
+        if (values.length > 0) {
+            int sum = 0;
+            for (int i = 0;i < values.length;i++) {
+                sum += values[i];
             }
-            throw new UnsupportedOperationException();
+            return sum;
         }
-
+        throw new UnsupportedOperationException();
     }
+
+}
