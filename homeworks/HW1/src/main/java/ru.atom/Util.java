d09a8b15dd98bedc7fdb8f18d1c6bcfd1fb4f4e5
YawnD <elagina.darja@yandex.ru>
2017-02-26 18:24:12 +0300

    HW1

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -16,7 +16,13 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = values[0];
+        for (Integer i : values) {
+            if (values[i] < max) {
+                max = values[i];
+            }
+        }
+        return max;
     }
 
     /**
@@ -26,7 +32,11 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        long sum = 0;
+        for (Integer i : values) {
+            sum += values[i];
+        }
+        return sum;
     }
 
 
