2187fff9c5660fef3fc50bb4f64fb00775d37688
Морозов Вячеслав <v.morozow@mail.ru>
2017-02-27 19:55:13 +0300

    Add files via upload

---
+++ Util.java
@@ -0,0 +1,43 @@
+package ru.atom;
+
+/**
+ * In this assignment you need to implement the following util methods.
+ * Note:
+ *  throw new UnsupportedOperationException(); - is just a stub
+ */
+public class Util {
+
+
+
+    /**
+     * Returns the greatest of {@code int} values.
+     *
+     * @param values an argument. Assume values.length > 0.
+     * @return the largest of values.
+     */
+    public static int max(int[] values) {
+        int maxValue = values[0];
+        for (int i: values) {
+            if (i > maxValue) {
+                maxValue = i;
+            }
+        }
+        return maxValue;
+    }
+
+    /**
+     * Returns the sum of all {@code int} values.
+     *
+     * @param values an argument. Assume values.length > 0.
+     * @return the sum of all values.
+     */
+    public static long sum(int[] values) {
+        long sumValue = 0;
+        for (int i: values) {
+            sumValue += i;
+        }
+        return sumValue;
+    }
+
+
+}
