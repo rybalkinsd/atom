1b8944eb8be9b1be8de7dc3b75ef03bfd68fa381
NikulinIlya <ilia.nikulin@gmail.com>
2017-02-28 02:44:07 +0300

    Удалены лишние отступы

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -1,4 +1,4 @@
-package ru.atom;
+﻿package ru.atom;
 
 /**
  * In this assignment you need to implement the following util methods.
@@ -16,13 +16,13 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-	    int maxVal = 0;
-		for (int i = 0; i < values.length; i++) {
-			if (values[i] > maxVal) {
-				maxVal = values[i];
-			}
-		}
-		return maxVal;
+	int maxVal = 0;
+	for (int i = 0; i < values.length; i++) {
+            if (values[i] > maxVal) {
+		maxVal = values[i];
+            }
+	}
+	return maxVal;
     }
 
     /**
@@ -32,11 +32,11 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-		long sum = 0L;
-		for (int i = 0; i < values.length; i++) {
-			sum += values[i];
-		}
-		return sum;
+	long sum = 0L;
+	for (int i = 0; i < values.length; i++) {
+            sum += values[i];
+	}
+	return sum;
     }
 
 
