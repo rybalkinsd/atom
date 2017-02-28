5315ef77d10b9b239133c011c27af8ee05b299eb
SalvadorDali777 <Salvador777@bk.ru>
2017-02-28 19:05:03 +0300

    homework1

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -6,9 +6,6 @@ package ru.atom;
  *  throw new UnsupportedOperationException(); - is just a stub
  */
 public class Util {
-
-
-
     /**
      * Returns the greatest of {@code int} values.
      *
@@ -27,7 +24,6 @@ public class Util {
         }
         return maxn;
     }
-
     /**
      * Returns the sum of all {@code int} values.
      *
