cc5ad99708e1b8b2518a85ebdb515890068ac4ba
Kartoxa1011 <mrantoshka1011@mail.ru>
2017-03-02 00:47:47 +0300

    Fixed checkstyle

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -1,7 +1,5 @@
 package ru.atom.geometry;
-
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * Template class for
  */
@@ -9,11 +7,10 @@ public class Point /* super class and interfaces here if necessary */ implements
     // fields
     // and methods
     private int x, y;
-    public Point(int x, int y) { //constructor
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
     /**
      * @param o - other object to check equality with
      * @return true if two points are equal and not null.
@@ -24,18 +21,19 @@ public class Point /* super class and interfaces here if necessary */ implements
         if (o == null || getClass() != o.getClass()) return false;
         // cast from Object to Point
         Point point = (Point) o;
-        if ((this.x == point.x) && (this.y == point.y)){
+        if ((this.x == point.x) && (this.y == point.y)) {
             return true;
         }
+
         // your code here
         return false;
     }
-    @Override //переопределение коллайдера
+    @Override
     public boolean isColliding(Collider other) {
-        if (this == other){
+        if (this == other) {
             return true;
         }else {
-            if (this.equals(other)){
+            if (this.equals(other)) {
                 return true;
             }
         }
