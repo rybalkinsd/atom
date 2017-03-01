0d2c83d6c02e72d7607f449906db4cab559feb08
gammaker <vav-94@mail.ru>
2017-03-01 22:18:56 +0300

    Implemented Bar-Bar collision

--- lecture02/src/main/java/ru/atom/geometry/Bar.java
+++ lecture02/src/main/java/ru/atom/geometry/Bar.java
@@ -29,6 +29,9 @@ public class Bar implements Collider {
         if (other instanceof Point) {
             return isColliding((Point) other);
         }
+        if (other instanceof Bar) {
+            return isColliding((Bar) other);
+        }
         return false;
     }
 
@@ -36,5 +39,24 @@ public class Bar implements Collider {
         boolean intersectsX = point.x >= left && point.x <= left + width;
         boolean intersectsY = point.y >= bottom && point.y <= bottom + height;
         return intersectsX && intersectsY;
+    }
+
+    boolean isColliding(Bar bar) {
+        if (left > bar.left+bar.width ||
+            left+width < bar.left ||
+            bottom+height < bar.bottom ||
+            bottom > bar.bottom+bar.height) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override public boolean equals(Object other) {
+        if(!(other instanceof Bar)) {
+            return false;
+        }
+        Bar bar = (Bar) other;
+        return left == bar.left && bottom == bar.bottom &&
+                width == bar.width && height == bar.height;
     }
 }
