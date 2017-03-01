d5316a96f2dd29dbb67440d52891166773280bba
gammaker <vav-94@mail.ru>
2017-03-01 20:33:04 +0300

    Style fix 2

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -9,8 +9,7 @@ public class Point implements Collider {
     public int x;
     public int y;
 
-    public Point(int x, int y)
-    {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
