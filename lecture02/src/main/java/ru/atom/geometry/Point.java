585508c9061041b439b3e2be5b3a16ca0799b732
ilysk <ilya.lysko@gmail.com>
2017-03-01 20:29:34 +0300

    Did first task

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -5,9 +5,15 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
-    // fields
+public class Point implements Collider {
+    protected int x;
+    protected int y;
     // and methods
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     /**
      * @param o - other object to check equality with
@@ -21,7 +27,18 @@ public class Point /* super class and interfaces here if necessary */ {
         // cast from Object to Point
         Point point = (Point) o;
 
-        // your code here
-        throw new NotImplementedException();
+        if (this.x == point.x && this.y == point.y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isColliding(Collider otherPoint) {
+        if (this == otherPoint) return true;
+        if (otherPoint == null || getClass() != otherPoint.getClass()) return false;
+
+        return this.equals(otherPoint);
     }
 }
