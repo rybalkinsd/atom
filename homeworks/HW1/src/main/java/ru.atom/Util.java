c4211455b30a1af8d3afdd643d513f66dc913fa5
ruslbizhNetcr <ruslbizhNetcr@example.ru>
2017-03-01 12:41:00 +0300

    Fixed Util.java by Ruslan Bizhanov

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -16,7 +16,18 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        if(values.length == 0){
+            throw new UnsupportedOperationException();
+        }
+        else {
+            int rez = values[0];
+            for (int i = 1; i < values.length; ++i) {
+                if (rez < values[i]) {
+                    rez = values[i];
+                }
+            }
+            return rez;
+        }
     }
 
     /**
@@ -26,7 +37,16 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        if(values.length == 0){
+            throw new UnsupportedOperationException();
+        }
+        else {
+            long sum = 0;
+            for (int i = 0; i < values.length; ++i) {
+                sum += values[i];
+            }
+            return sum;
+        }
     }
 
 
