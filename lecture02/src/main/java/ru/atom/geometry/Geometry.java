029a85ecb97c32c6455835bad5c0f161f3d685d3
rybalkinsd <yan.brikl@gmail.com>
2017-03-01 15:43:28 +0300

    lecture02 presentation & source

---
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -0,0 +1,36 @@
+package ru.atom.geometry;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+/**
+ *  ^ Y
+ *  |
+ *  |
+ *  |
+ *  |          X
+ *  .---------->
+ */
+
+public final class Geometry {
+    
+    private Geometry() {
+    }
+
+    /**
+     * Bar is a rectangle, which borders are parallel to coordinate axis
+     * Like selection bar in desktop, this bar is defined by two opposite corners
+     * Bar is not oriented
+     * (It is not relevant, which opposite corners you choose to define bar)
+     * @return new Bar
+     */
+    public static Collider createBar(int firstPointX, int firstCornerY, int secondCornerX, int secondCornerY) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * 2D point
+     * @return new Point
+     */
+    public static Collider createPoint(int x, int y) {
+        throw new NotImplementedException();
+    }
+}
---
+++ lecture02/src/main/java/ru/atom/message/example2/Storable.java
@@ -0,0 +1,10 @@
+package ru.atom.message.example2;
+
+import java.io.File;
+
+/**
+ * Created by sergey on 2/28/17.
+ */
+public interface Storable {
+    void saveTo(File file);
+}
