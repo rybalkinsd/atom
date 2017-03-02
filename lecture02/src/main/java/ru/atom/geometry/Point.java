30d83c3c79da437387c16240b9c385276b29038e
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-02 20:32:17 +0300

    finally?

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -44,7 +44,7 @@ public class Point implements Collider {
     @Override
     public boolean equals(Object o) {
         if (this == o)return true;
-        if (o == null || getClass() != o.getClass()return false;
+        if (o == null || getClass() != o.getClass())return false;
 
         // cast from Object to Point
         Point point = (Point) o;
