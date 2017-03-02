f93824477dd4b44686ba13a49d533a4ed63d22be
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-02 19:11:35 +0300

    stylecheck

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -10,7 +10,7 @@ public class Point implements Collider {
     private int x;
     private int y;
 
-    public Point ( int x, int y ) {
+    public Point(int x, int y) {
         this.setX(x);
         this.setY(y);
     }
@@ -22,30 +22,31 @@ public class Point implements Collider {
     public int getY() {
         return this.y;
     }
-    public void setX ( int x ) {
+    public void setX(int x) {
         this.x = x;
     }
-    public void setY ( int y ) {
+    public void setY(int y) {
         this.y = y;
     }
 
     @Override
-    public boolean isColliding ( Collider other ) {
-        return this.equals( (Point) other );
+    public boolean isColliding(Collider other) {
+        return this.equals((Point) other);
     }
 
     /**
      * @param o - other object to check equality with
      * @return true if two points are equal and not null.
      */
+
     @Override
     public boolean equals( Object o ) {
-        if ( this == o ) return true;
-        if ( o == null || getClass() != o.getClass() ) return false;
+        if(this == o)return true;
+        if(o == null || getClass() != o.getClass())return false;
 
         // cast from Object to Point
         Point point = (Point) o;
-        if ( this.getX() == point.getX() && this.getY() == point.getY() ) {
+        if(this.getX() == point.getX() && this.getY() == point.getY()) {
             return true;
         } else {
             return false;
