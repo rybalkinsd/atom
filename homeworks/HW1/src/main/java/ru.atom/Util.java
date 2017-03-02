2fbed5a094156bfa64047827c66d6a7124d51aeb
Semga <yiliya_sema@mail.ru>
2017-03-02 22:02:36 +0300

    HW1 new Семенова Ю (изменен тест sum3)

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -37,7 +37,7 @@ public class Util {
         if (values.length == 0) {
             throw new UnsupportedOperationException("null array of values");
         }
-        int sum = 0;
+        long sum = 0;
         for (int i = 0; i < values.length; i++) {
             sum += values[i];
         }
