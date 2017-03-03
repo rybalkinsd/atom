a18139e86257d26080d1b27340561583fcb9d13e
Arif Ergashev <orif95@mail.ru>
2017-03-04 02:56:01 +0300

    PointPointCollisinTest работает

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -23,7 +23,9 @@ public final class Geometry {
      * @return new Bar
      */
     public static Collider createBar(int firstPointX, int firstCornerY, int secondCornerX, int secondCornerY) {
-        throw new NotImplementedException();
+        Point p = new Point(1,2);
+        //throw new Exception("df");
+        return p;
     }
 
     /**
@@ -31,6 +33,7 @@ public final class Geometry {
      * @return new Point
      */
     public static Collider createPoint(int x, int y) {
-        throw new NotImplementedException();
+        Point point = new Point(x, y);
+        return point;
     }
 }
--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -5,9 +5,32 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
-    // fields
-    // and methods
+public class Point implements Collider {
+    private int firstPointX;
+    private int secondPointY;
+
+    public Point(int x, int y){
+        firstPointX = x;
+        secondPointY = y;
+    }
+
+    public void setFirstPointX(int point){
+        firstPointX = point;
+    }
+
+    public void setSecondPointY(int point){
+        secondPointY = point;
+    }
+
+    public int getFirstPointX(){
+        return firstPointX;
+    }
+
+    public int getSecondPointY(){
+        return secondPointY;
+    }
+
+
 
     /**
      * @param o - other object to check equality with
@@ -15,13 +38,26 @@ public class Point /* super class and interfaces here if necessary */ {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
         // cast from Object to Point
         Point point = (Point) o;
+        if(this.getFirstPointX() == point.getFirstPointX() && this.getSecondPointY() == point.getSecondPointY()) {
+            return true;
+        }
+        else
+            return false;
+    }
 
-        // your code here
-        throw new NotImplementedException();
+
+    @Override
+    public boolean isColliding(Collider other) {
+        if (this.equals(other))
+            return true;
+        else
+            return false;
     }
 }
