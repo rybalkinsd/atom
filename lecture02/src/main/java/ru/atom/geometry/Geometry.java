d60be69d4f78198f451b9ae9f69f668eacbdbced
Brylkin Dmitry <dbrylkin96@gmail.com>
2017-03-03 22:55:45 +0300

    Homework2.Part1.Brylkin Dmitry

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -24,6 +24,7 @@ public final class Geometry {
      */
     public static Collider createBar(int firstPointX, int firstCornerY, int secondCornerX, int secondCornerY) {
         throw new NotImplementedException();
+
     }
 
     /**
@@ -31,6 +32,7 @@ public final class Geometry {
      * @return new Point
      */
     public static Collider createPoint(int x, int y) {
-        throw new NotImplementedException();
+        Point point = new Point(x, y);
+        return point;
     }
 }
--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -5,7 +5,19 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
+public class Point implements Collider {
+
+    private int x;
+    private int y;
+
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+
     // fields
     // and methods
 
@@ -16,12 +28,30 @@ public class Point /* super class and interfaces here if necessary */ {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
+
         if (o == null || getClass() != o.getClass()) return false;
+
 
         // cast from Object to Point
         Point point = (Point) o;
 
         // your code here
-        throw new NotImplementedException();
+
+        if (x == point.x && y == point.y)
+            return true;
+        else  return false;
+    }
+
+
+    @Override
+    public boolean isColliding(Collider other) {
+        if (this.equals(other) == true)
+            return true;
+        else return false;
     }
 }
+
+
+
+
+
