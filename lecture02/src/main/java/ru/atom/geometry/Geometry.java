50873caa024dd68a745714705c4e9f870805e77e
Kartoxa1011 <mrantoshka1011@mail.ru>
2017-03-03 17:59:23 +0300

    Fixed Checkstyle in Geometry

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -11,10 +11,8 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 
 public final class Geometry {
-    
-    private Geometry() {
-    }
 
+    private Geometry() {}
     /**
      * Bar is a rectangle, which borders are parallel to coordinate axis
      * Like selection bar in desktop, this bar is defined by two opposite corners
@@ -22,13 +20,16 @@ public final class Geometry {
      * (It is not relevant, which opposite corners you choose to define bar)
      * @return new Bar
      */
+
     public static Collider createBar(int firstPointX, int firstCornerY, int secondCornerX, int secondCornerY) {
         throw new NotImplementedException();
     }
+
     /**
      * 2D point
      * @return new Point
      */
+
     public static Collider createPoint(int x, int y) {
         Point point = new Point(x,y);
         return point;
