99b64cbe6f57cea96976cb20ce2e515716dc82ed
Вячеслав Морозов < >
2017-03-03 19:22:13 +0300

    'FixedFor1Task'

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -5,7 +5,11 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 /**
  * Template class for
  */
+<<<<<<< HEAD
 public class Point /* super class and interfaces here if necessary */ {
+=======
+public class Point implements Collider /* super class and interfaces here if necessary */ {
+>>>>>>> 803adbb... 'FixedFor1Task'
     // fields
     // and methods
 
@@ -13,6 +17,22 @@ public class Point /* super class and interfaces here if necessary */ {
      * @param o - other object to check equality with
      * @return true if two points are equal and not null.
      */
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -22,6 +42,15 @@ public class Point /* super class and interfaces here if necessary */ {
         Point point = (Point) o;
 
         // your code here
+<<<<<<< HEAD
         throw new NotImplementedException();
+=======
+        return (this.getX() == point.getX() && this.getY() == point.getY());
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        return (this.equals(other));
+>>>>>>> 803adbb... 'FixedFor1Task'
     }
 }
