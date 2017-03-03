2778d9ce206f38e4c324028ad39d4add9d51a37f
delez96 <kvakvakevkav@gmail.com>
2017-03-04 01:23:11 +0300

    Norma

--- lecture02/src/main/java/ru/atom/geometry/Bar.java
+++ lecture02/src/main/java/ru/atom/geometry/Bar.java
@@ -43,7 +43,7 @@ public class Bar implements Collider {
     public boolean isColliding(Collider other) {
 
         if (this == other) return true;
-        if (other == null) return false;
+        if (other == null)  return false;
 
 
         // cast from Object to Point
@@ -52,7 +52,10 @@ public class Bar implements Collider {
 
             Point point = (Point) other;
 
-            return ((point.getX() <= this.secondCornerX) && (point.getX() >= this.firstPointX) && (point.getY() <= this.secondCornerY) && (point.getY() >= this.firstCornerY));
+            return ((point.getX() <= this.secondCornerX)
+                    && (point.getX() >= this.firstPointX)
+                    && (point.getY() <= this.secondCornerY)
+                    && (point.getY() >= this.firstCornerY));
 
         }
         if (other instanceof Bar) {
@@ -60,22 +63,39 @@ public class Bar implements Collider {
 
 
             // your code here
-            return ((bar.secondCornerX >= firstPointX && bar.secondCornerX <= secondCornerX && bar.secondCornerY <= secondCornerY &&
-                    bar.secondCornerY >= firstCornerY)
-                    || (bar.firstPointX >= firstPointX && bar.firstPointX <= secondCornerX && bar.secondCornerY <= secondCornerY &&
-                    bar.secondCornerY >= firstCornerY)
-                    || (bar.firstPointX >= firstPointX && bar.firstPointX <= secondCornerX && bar.firstCornerY <= secondCornerY &&
-                    bar.firstCornerY >= firstCornerY)
-                    || (bar.secondCornerX >= firstPointX && bar.secondCornerX <= secondCornerX && bar.firstCornerY <= secondCornerY &&
-                    bar.firstCornerY >= firstCornerY)
+            return ((bar.secondCornerX >= firstPointX
+                    && bar.secondCornerX <= secondCornerX
+                    && bar.secondCornerY <= secondCornerY
+                    && bar.secondCornerY >= firstCornerY)
+                    || (bar.firstPointX >= firstPointX
+                    && bar.firstPointX <= secondCornerX
+                    && bar.secondCornerY <= secondCornerY
+                    && bar.secondCornerY >= firstCornerY)
+                    || (bar.firstPointX >= firstPointX
+                    && bar.firstPointX <= secondCornerX
+                    && bar.firstCornerY <= secondCornerY
+                    && bar.firstCornerY >= firstCornerY)
+                    || (bar.secondCornerX >= firstPointX
+                    && bar.secondCornerX <= secondCornerX
+                    && bar.firstCornerY <= secondCornerY
+                    && bar.firstCornerY >= firstCornerY)
 
-                    || (secondCornerX >= bar.firstPointX && secondCornerX <= bar.secondCornerX && firstCornerY <= bar.secondCornerY &&
-                    firstCornerY >= bar.firstCornerY)
-                    || (firstPointX >= bar.firstPointX && firstPointX <= bar.secondCornerX && secondCornerY <= bar.secondCornerY && secondCornerY >= bar.firstCornerY)
-                    || (secondCornerX >= bar.firstPointX && secondCornerX <= bar.secondCornerX && secondCornerY <= bar.secondCornerY &&
-                    secondCornerY >= bar.firstCornerY)
-                    || (firstPointX >= bar.firstPointX && firstPointX <= bar.secondCornerX && firstCornerY <= bar.secondCornerY &&
-                    firstCornerY >= bar.firstCornerY));
+                    || (secondCornerX >= bar.firstPointX
+                    && secondCornerX <= bar.secondCornerX
+                    && firstCornerY <= bar.secondCornerY
+                    && firstCornerY >= bar.firstCornerY)
+                    || (firstPointX >= bar.firstPointX
+                    && firstPointX <= bar.secondCornerX
+                    && secondCornerY <= bar.secondCornerY
+                    && secondCornerY >= bar.firstCornerY)
+                    || (secondCornerX >= bar.firstPointX
+                    && secondCornerX <= bar.secondCornerX
+                    && secondCornerY <= bar.secondCornerY
+                    && secondCornerY >= bar.firstCornerY)
+                    || (firstPointX >= bar.firstPointX
+                    && firstPointX <= bar.secondCornerX
+                    && firstCornerY <= bar.secondCornerY
+                    && firstCornerY >= bar.firstCornerY));
             } else return  false;
         }
 
