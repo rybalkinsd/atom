fd4ec5cd052c11294fa5331dc110cea49821ecc3
apomosov <a.pomosov@hh.ru>
2017-03-04 14:46:28 +0300

    Homework2

---
+++ homeworks/HW2/src/test/java/ru/atom/TestGameSessionCreator.java
@@ -0,0 +1,16 @@
+package ru.atom;
+
+import ru.atom.model.GameSession;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Create sample game session with all kinds of objects that will present in bomber-man game
+ */
+public final class TestGameSessionCreator {
+    private TestGameSessionCreator(){}
+    static GameSession createGameSession() {
+        GameSession gameSession = new GameSession();
+        //TODO populate your game session with sample objects
+        throw new NotImplementedException();
+    }
+}
