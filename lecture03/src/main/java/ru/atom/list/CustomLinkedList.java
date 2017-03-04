c355f5e701af2ba71cd5997f500ba908d4d80fc2
ilysk <ilya.lysko@gmail.com>
2017-03-04 21:58:33 +0300

    Edited import style (because of gradle checkstyle)

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -2,7 +2,12 @@ package ru.atom.list;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 
 public class CustomLinkedList<E> extends ListNode implements List<E> {
