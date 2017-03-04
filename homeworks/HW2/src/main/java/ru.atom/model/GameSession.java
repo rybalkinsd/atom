604d7326b28872750cba78b38ab6537f0e425c09
apomosov <a.pomosov@hh.ru>
2017-03-04 14:46:28 +0300

    Homework2

---
+++ homeworks/HW2/src/main/java/ru.atom/model/GameSession.java
@@ -0,0 +1,33 @@
+package ru.atom.model;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameSession implements Tickable {
+    private final static Logger log = LogManager.getLogger(GameSession.class);
+    private List<GameObject> gameObjects = new ArrayList<>();
+
+    public List<GameObject> getGameObjects() {
+        return new ArrayList<>(gameObjects);
+    }
+    
+    public void addGameObject(GameObject gameObject) {
+        gameObjects.add(gameObject);
+    }
+
+    @Override
+    public void tick(long elapsed) {
+        log.info("tick");
+        for (GameObject gameObject : gameObjects) {
+            if (gameObject instanceof Tickable) {
+                ((Tickable) gameObject).tick(elapsed);
+            }
+            if(gameObject instanceof Temporary && ((Temporary) gameObject).isDead()) {
+                gameObjects.remove(gameObject);
+            }
+        }
+    }
+}
---
+++ homeworks/HW2/src/main/java/ru.atom/model/Movable.java
@@ -0,0 +1,18 @@
+package ru.atom.model;
+
+import ru.atom.geometry.Point;
+
+/**
+ * GameObject that can move during game
+ */
+public interface Movable extends Positionable, Tickable {
+    /**
+     * Tries to move entity towards specified direction
+     * @return final position after movement
+     */
+    Point move(Direction direction);
+    
+    enum Direction {
+        UP, DOWN, RIGHT, LEFT, IDLE
+    }
+}
