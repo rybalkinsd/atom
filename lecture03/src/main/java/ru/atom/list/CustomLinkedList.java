f4a486d13d4868c3641d1a551e1f6ef57413ee58
GolovPavel <golovpasha@gmail.com>
2017-03-05 12:15:15 +0300

    All CustomLinkedList tests passed

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -8,66 +8,178 @@ import java.util.List;
 import java.util.ListIterator;
 
 
-public class CustomLinkedList<E> implements List<E> {
+public class CustomLinkedList<E> implements List<E>, Iterable<E> {
+
+    private ListNode<E> header;
+    private ListNode<E> tail;
+    private int size;
+
+    CustomLinkedList() {
+        this.tail = null;
+        this.header = new ListNode<E>(null, tail, tail);
+        this.size = 0;
+    }
 
     @Override
     public int size() {
-        throw new NotImplementedException();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new NotImplementedException();
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        throw new NotImplementedException();
+        if (size == 0) {
+            return false;
+        }
+
+        ListNode<E> tmpNode = header;
+
+        while (tmpNode != tail) {
+            tmpNode = tmpNode.nextNode;
+            if (tmpNode.element.equals(o)) {
+                return true;
+            }
+        }
+        return false;
+
     }
 
     @Override
     public Iterator<E> iterator() {
-        throw new NotImplementedException();
+        return new Iterator<E>() {
+            ListNode<E> currentNode = header;
+
+            @Override
+            public boolean hasNext() {
+                return currentNode.nextNode != header;
+            }
+
+            @Override
+            public E next() {
+                currentNode = currentNode.nextNode;
+                return currentNode.element;
+            }
+        };
     }
 
     @Override
     public boolean add(E e) {
-        throw new NotImplementedException();
+        if (tail == null) {
+            tail = new ListNode<E>(e, header, header);
+            header.nextNode = tail;
+            header.prevNode = tail;
+            size++;
+        } else {
+            ListNode<E> newNode = new ListNode<E>(e, header, tail);
+            tail.nextNode = newNode;
+            tail = newNode;
+            header.prevNode = tail;
+            size++;
+        }
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new NotImplementedException();
+        if (size == 0) {
+            return false;
+        }
+
+        ListNode<E> currNode = header.nextNode;
+        if (o == null) {
+            while (currNode != header) {
+                if (currNode.element == null) {
+                    ListNode<E> prevNode = currNode.prevNode;
+                    ListNode<E> nextNode = currNode.nextNode;
+                    prevNode.nextNode = nextNode;
+                    nextNode.prevNode = prevNode;
+                    size--;
+                    return true;
+                }
+                currNode = currNode.nextNode;
+            }
+            return false;
+        } else {
+            while (currNode != header) {
+                if (currNode.element.equals(o)) {
+                    ListNode<E> prevNode = currNode.prevNode;
+                    ListNode<E> nextNode = currNode.nextNode;
+                    prevNode.nextNode = nextNode;
+                    nextNode.prevNode = prevNode;
+                    size--;
+                    return true;
+                }
+                currNode = currNode.nextNode;
+            }
+            return false;
+        }
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        throw new NotImplementedException();
+        for (Object element : c) {
+            if (!this.contains(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public void clear() {
-        throw new NotImplementedException();
+        this.tail = null;
+        this.header = new ListNode<E>(null, tail, tail);
+        this.size = 0;
     }
 
     @Override
     public E get(int index) {
-        throw new NotImplementedException();
+        if (index > size - 1 || index < 0) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+        ListNode<E> currNode = header;
+        for (int i = 0; i <= index; i++) {
+            currNode = currNode.nextNode;
+        }
+        return currNode.element;
     }
 
     @Override
     public int indexOf(Object o) {
-        throw new NotImplementedException();
+        int index = 0;
+        ListNode<E> currNode = header.nextNode;
+
+        if (o == null) {
+            while (currNode != header) {
+                if (currNode.element == null) {
+                    return index;
+                }
+                index++;
+                currNode = currNode.nextNode;
+            }
+        } else {
+            while (currNode != header) {
+                if (currNode.element.equals(o)) {
+                    return index;
+                }
+                index++;
+                currNode = currNode.nextNode;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        throw new NotImplementedException();
+        for (E element : c) {
+            this.add(element);
+        }
+        return true;
     }
-
-
-
-
 
 
     /*
