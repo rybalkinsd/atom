f6c371f70698ef9519dc5208285da54026ed9662
Ксения Кирпичникова < >
2017-03-06 23:11:13 +0300

    Переопределены все методы CustomLinkedList

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -1,7 +1,10 @@
 package ru.atom.list;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.NoSuchElementException;
+import java.lang.IndexOutOfBoundsException;
 
+import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -9,60 +12,186 @@ import java.util.ListIterator;
 
 
 public class CustomLinkedList<E> implements List<E> {
+    private ListNode header;
+
+    CustomLinkedList() {
+        header = new ListNode();
+        header.setNext(header);
+        header.setPrev(header);
+        header.setContent((E) null);
+    }
+
 
     @Override
     public int size() {
-        throw new NotImplementedException();
+        ListNode<E> elem = header;
+        int num = 0;
+        if (isEmpty()) return 0;
+        else {
+            do {
+                num++;
+                elem = elem.getNext();
+            } while (elem.getNext() != header);
+            return num;
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        throw new NotImplementedException();
+        return (header.getNext() == header);
     }
 
     @Override
     public boolean contains(Object o) {
-        throw new NotImplementedException();
+        if (!isEmpty()) {
+            ListNode<E> elem = header;
+            if (elem.getNext().getContent().getClass().isInstance(o)) {
+                do {
+                    elem = elem.getNext();
+                    if ((E) o == elem.getContent()) return true;
+                } while (elem.getNext() != header);
+            }
+        }
+        return false;
     }
 
     @Override
     public Iterator<E> iterator() {
-        throw new NotImplementedException();
+        Iterator<E> iter = new Iterator<E>() {
+
+            private ListNode<E> elem = header;
+
+            @Override
+            public boolean hasNext() {
+                return (elem.getNext() != header);
+            }
+
+            @Override
+            public E next() {
+                if (hasNext()) {
+                    elem = elem.getNext();
+                    return elem.getContent();
+                } else throw new NoSuchElementException();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return iter;
     }
 
     @Override
     public boolean add(E e) {
-        throw new NotImplementedException();
+        ListNode<E> elem = header;
+
+        ListNode<E> newElem = new ListNode<>();
+        newElem.setContent((E)e);
+
+        if (isEmpty()) {
+            newElem.setPrev(header);
+            header.setNext(newElem);
+        } else {
+            elem = header.getPrev();
+            newElem.setPrev(elem);
+            elem.setNext(newElem);
+        }
+        newElem.setNext(header);
+        header.setPrev(newElem);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new NotImplementedException();
+
+        if (!isEmpty()) {
+
+            ListNode<E> elem = header;
+            if (elem.getNext().getContent().getClass().isInstance(o)) {
+                do {
+                    elem = elem.getNext();
+                    if ((E) o == elem.getContent()) {
+                        elem.getPrev().setNext(elem.getNext());
+                        elem.getNext().setPrev(elem.getPrev());
+                        return true;
+                    }
+                } while (elem.getNext() != header);
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        throw new NotImplementedException();
+        if (isEmpty()) return false;
+        else {
+            for (Object elem : c) {
+                if (!contains(elem)) return false;
+            }
+            return true;
+        }
     }
 
     @Override
     public void clear() {
-        throw new NotImplementedException();
+        ListNode<E> elem = header;
+        do {
+            elem.setContent(null);
+            elem.setPrev(null);
+            elem = elem.getNext();
+            elem.getPrev().setNext(null);
+        } while (elem.getNext() != header);
+        elem.getPrev().setNext(null);
+        elem.getPrev().setContent(null);
+        elem.getPrev().setPrev(null);
+        header.setNext(header);
+        header.setPrev(header);
     }
 
     @Override
     public E get(int index) {
-        throw new NotImplementedException();
+        if (index < size() && index >= 0) {
+            int num = 0;
+            ListNode<E> elem = header.getNext();
+            while (num != index) {
+                elem = elem.getNext();
+                num++;
+            }
+            return elem.getContent();
+        } else throw new IndexOutOfBoundsException();
+
     }
 
     @Override
     public int indexOf(Object o) {
-        throw new NotImplementedException();
+        if (!isEmpty()) {
+            ListNode<E> elem = header;
+            int num = -1;
+            if (elem.getNext().getContent().getClass().isInstance(o)) {
+                do {
+                    elem = elem.getNext();
+                    num++;
+                    if ((E) o == elem.getContent()) return num;
+                } while (elem.getNext() != header);
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        throw new NotImplementedException();
+        ListNode<E> elem = header.getPrev();
+        for (E elemCollection : c) {
+            ListNode<E> newElem = new ListNode<>();
+            newElem.setContent(elemCollection);
+            elem.setNext(newElem);
+            newElem.setPrev(elem);
+            newElem.setNext(header);
+            header.setPrev(newElem);
+            elem = newElem;
+        }
+        return true;
     }
 
 
--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,39 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    private E content;
+    private ListNode next;
+    private ListNode prev;
+
+
+    public E getContent() {
+
+        return content;
+    }
+
+    public ListNode getNext() {
+
+        return next;
+    }
+
+    public ListNode getPrev() {
+
+        return prev;
+    }
+
+    public void setContent(E content) {
+        this.content = content;
+    }
+
+    public void setNext(ListNode next) {
+
+        this.next = next;
+    }
+
+    public void setPrev(ListNode prev) {
+
+        this.prev = prev;
+    }
+
+
 }
