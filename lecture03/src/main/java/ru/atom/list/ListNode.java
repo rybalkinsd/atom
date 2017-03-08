7def494c82d6b14dc5e5d66f45bb28f8cbf47886
delez96 <kvakvakevkav@gmail.com>
2017-03-08 17:13:58 +0300

    All tests passed

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -1,7 +1,23 @@
 package ru.atom.list;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    E element;
+    ListNode<E> prev;
+    ListNode<E> next;
+    private ListNode<E> size;
+
+    public  ListNode(E element, ListNode<E> prev, ListNode<E> next){
+      this.element = element;
+      this.prev = prev;
+      this.next = next;
+    }
+
+
+
 }
