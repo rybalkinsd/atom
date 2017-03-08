05d8ac810c807d796bc5b3b72dad6d2142b8bb1a
Areksei <areksei59@gmail.com>
2017-03-08 21:59:43 +0300

    fixed lecture03
    fixed style

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,13 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    E item;
+    ListNode<E> next;
+    ListNode<E> prev;
+
+    ListNode(ListNode<E> prev, E element, ListNode<E> next) {
+        this.item = element;
+        this.next = next;
+        this.prev = prev;
+    }
 }
