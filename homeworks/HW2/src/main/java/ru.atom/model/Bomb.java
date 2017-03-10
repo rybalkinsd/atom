de610b0c72907557c9eb6728e0afd2a757edbe1c
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-10 20:45:22 +0300

    some explosions

---
+++ homeworks/HW2/src/main/java/ru.atom/model/Bomb.java
@@ -0,0 +1,34 @@
+package ru.atom.mode1;
+
+import ru.atom.geometry.Point;
+
+public class Bomb implements Temporry  {
+    private int id;
+    private Point position;
+    private static final long bomb_lifetime = 1000;
+    private long elapsedTime;
+
+    public Bomb(Point position) {
+        this.id = Gamesession.SetObjectId();
+        this.position = position;
+        this elapsedTime =0L;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public long getLifetimeMillis() {
+        returm bomb_lifetime;
+    }
+
+    @Override
+    public boolean isDead() {
+        if(elapsedTime >= bomb_lifetime) {
+            return true;
+        }
+        return false;
+    }
+}
