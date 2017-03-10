e29500291878b07a648d64b547c77447059dbd4e
Kartoxa1011 <mrantoshka1011@mail.ru>
2017-03-11 01:37:15 +0300

    Create UnbreakbleWall

--- homeworks/HW2/src/main/java/ru.atom/model/UnbreakableWall.java
+++ homeworks/HW2/src/main/java/ru.atom/model/UnbreakableWall.java
@@ -1,7 +1,17 @@
 package ru.atom.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.atom.geometry.Point;
+
 /**
  * Created by home on 10.03.2017.
  */
-public class UnbreakableWall {
+public class UnbreakableWall extends AbstractGameObject implements Positionable {
+    private static final Logger log = LogManager.getLogger(UnbreakableWall.class);
+
+    public UnbreakableWall(Point position) {
+        super(position.getX(), position.getY());
+        log.info("Unbreakable wall was set id={}, x={}, y={}", getId(), position.getX(), position.getY());
+    }
 }
