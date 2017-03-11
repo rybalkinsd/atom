a8709346e5ef7fd67f9c50109d9bb28236fecfea
Alpi <Alpieex@gmail.com>
2017-03-11 16:24:59 +0300

    lecture4 sources

--- lecture04/src/main/java/ru/atom/http/server/ChatResource.java
+++ lecture04/src/main/java/ru/atom/http/server/ChatResource.java
@@ -15,55 +15,55 @@ import javax.ws.rs.core.Response;
 
 @Path("/chat")
 public class ChatResource {
-  private static final Logger log = LogManager.getLogger(ChatResource.class);
+    private static final Logger log = LogManager.getLogger(ChatResource.class);
 
-  private static final ConcurrentArrayQueue<String> logined = new ConcurrentArrayQueue<>();
-  private static final ConcurrentArrayQueue<String> chat = new ConcurrentArrayQueue<>();
+    private static final ConcurrentArrayQueue<String> logined = new ConcurrentArrayQueue<>();
+    private static final ConcurrentArrayQueue<String> chat = new ConcurrentArrayQueue<>();
 
-  @POST
-  @Consumes("application/x-www-form-urlencoded")
-  @Path("/login")
-  public Response login(@QueryParam("name") String name) {
-    if (name.length() > 30) {
-      return Response.status(Response.Status.BAD_REQUEST).entity("Too long name, sorry :(").build();
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Path("/login")
+    public Response login(@QueryParam("name") String name) {
+        if (name.length() > 30) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Too long name, sorry :(").build();
+        }
+        if (name.toLowerCase().contains("gitler")) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Gitler not allowed, sorry :(").build();
+        }
+        if (logined.contains(name)) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Already logined").build();
+        }
+        log.info("[" + name + "]" + " logined");
+        logined.add(name);
+        chat.add("[" + name + "] joined");
+        return Response.ok().build();
     }
-    if (name.toLowerCase().contains("gitler")) {
-      return Response.status(Response.Status.BAD_REQUEST).entity("Gitler not allowed, sorry :(").build();
-    }
-    if (logined.contains(name)) {
-      return Response.status(Response.Status.BAD_REQUEST).entity("Already logined").build();
-    }
-    log.info("[" + name + "]" + " logined");
-    logined.add(name);
-    chat.add("[" + name + "] joined");
-    return Response.ok().build();
-  }
 
-  @GET
-  @Produces("text/plain")
-  @Path("/online")
-  public Response online() {
-    return Response.ok(String.join("\n", logined)).build();
-  }
-
-  @POST
-  @Consumes("application/x-www-form-urlencoded")
-  @Path("/say")
-  public Response say(@QueryParam("name") String name, @FormParam("msg") String msg) {
-    if (!logined.contains(name)) {
-      return Response.status(Response.Status.UNAUTHORIZED).entity("Not logined").build();
+    @GET
+    @Produces("text/plain")
+    @Path("/online")
+    public Response online() {
+        return Response.ok(String.join("\n", logined)).build();
     }
-    if (msg.length() > 140) {
-      return Response.status(Response.Status.BAD_REQUEST).entity("Too long message").build();
-    }
-    chat.add("[" + name + "]: " + msg);
-    return Response.ok().build();
-  }
 
-  @GET
-  @Produces("text/plain")
-  @Path("/chat")
-  public Response chat() {
-    return Response.ok(String.join("\n", chat)).build();
-  }
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Path("/say")
+    public Response say(@QueryParam("name") String name, @FormParam("msg") String msg) {
+        if (!logined.contains(name)) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Not logined").build();
+        }
+        if (msg.length() > 140) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Too long message").build();
+        }
+        chat.add("[" + name + "]: " + msg);
+        return Response.ok().build();
+    }
+
+    @GET
+    @Produces("text/plain")
+    @Path("/chat")
+    public Response chat() {
+        return Response.ok(String.join("\n", chat)).build();
+    }
 }
--- lecture04/src/main/java/ru/atom/http/server/HttpServer.java
+++ lecture04/src/main/java/ru/atom/http/server/HttpServer.java
@@ -5,22 +5,22 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class HttpServer {
-  public static void main(String[] args) throws Exception {
-    ServletContextHandler context = new ServletContextHandler();
-    context.setContextPath("/");
+    public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
 
-    Server jettyServer = new Server(8080);
-    jettyServer.setHandler(context);
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
 
-    ServletHolder jerseyServlet = context.addServlet(
-            org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-    jerseyServlet.setInitOrder(0);
+        ServletHolder jerseyServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
 
-    jerseyServlet.setInitParameter(
-            "jersey.config.server.provider.packages",
-            "ru.atom.http"
-    );
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.packages",
+                "ru.atom.http"
+        );
 
-    jettyServer.start();
-  }
+        jettyServer.start();
+    }
 }
