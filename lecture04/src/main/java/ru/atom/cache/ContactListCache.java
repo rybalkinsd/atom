8da9fa558a35f62cf8b4fa5518d53f0e93e6798b
KazantsevaA <ankazantseva@inbox.ru>
2017-03-12 01:06:42 +0300

    lecture04, Казанцева Анжелика

--- lecture04/src/main/java/ru/atom/cache/ContactListCache.java
+++ lecture04/src/main/java/ru/atom/cache/ContactListCache.java
@@ -2,33 +2,51 @@ package ru.atom.cache;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * What about Map?
  */
 public class ContactListCache extends AbstractCache<Person, List<? extends Person>> {
+    private int size = 0;
+    public Map<Person, List<? extends Person>> friendList;
+
     public ContactListCache(int capacity) {
         super(capacity);
+        friendList = new HashMap<Person, List<? extends Person>>(capacity);
     }
 
     @Override
     public boolean put(Person person, List<? extends Person> people) {
-        throw new NotImplementedException();
+        if (size == capacity) removeAny();
+        friendList.put(person, people);
+        size++;
+        return true;
+        //throw new NotImplementedException();
     }
 
     @Override
     public List<? extends Person> get(Person person) {
-        throw new NotImplementedException();
+        return friendList.get(person);
+        //throw new NotImplementedException();
     }
 
     @Override
     public int getSize() {
-        throw new NotImplementedException();
+        return size;
+        //throw new NotImplementedException();
     }
 
     private boolean removeAny() {
-        throw new NotImplementedException();
+        for (Person key: friendList.keySet()) {
+            friendList.remove(key);
+            size--;
+            return true;
+        }
+        return false;
+        //throw new NotImplementedException();
     }
 
 }
