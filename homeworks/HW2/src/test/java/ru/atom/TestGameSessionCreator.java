3aa1d0dbb06db9c00c5e61dcecdd7edb26ac83b7
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-11 17:26:56 +0300

    creator

--- homeworks/HW2/src/test/java/ru/atom/TestGameSessionCreator.java
+++ homeworks/HW2/src/test/java/ru/atom/TestGameSessionCreator.java
@@ -1,7 +1,14 @@
 package ru.atom;
 
 import ru.atom.model.GameSession;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import ru.atom.geometry.Point;
+import ru.atom.model.AbstractGameObject;
+import ru.atom.model.GameSession;
+import ru.atom.model.Player;
+import ru.atom.model.Bomb;
+import ru.atom.model.BreakableWall;
+import ru.atom.model.UnbreakableWall;
+
 
 /**
  * Create sample game session with all kinds of objects that will present in bomber-man game
@@ -13,6 +20,20 @@ public final class TestGameSessionCreator {
     static GameSession createGameSession() {
         GameSession gameSession = new GameSession();
         //TODO populate your game session with sample objects
-        throw new NotImplementedException();
+        gameSession.addGameObject(new Bomb(new Point(0, 0)));
+        gameSession.addGameObject(new Bomb(new Point(6, 6)));
+
+        gameSession.addGameObject(new Player(new Point(1, 2)));
+        gameSession.addGameObject(new Player(new Point(2, 8)));
+
+
+
+        gameSession.addGameObject(new BreakableWall(new Point(4, 5)));
+        gameSession.addGameObject(new BreakableWall(new Point(2, 8)));
+
+        gameSession.addGameObject(new UnbreakableWall(new Point(3, 3)));
+        gameSession.addGameObject(new UnbreakableWall(new Point(4, 7)));
+
+        return gameSession;
     }
 }
