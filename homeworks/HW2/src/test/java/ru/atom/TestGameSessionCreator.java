2910fe45d4f56c4e9dec6871e78a7700f9129aa4
SergDob <makasy2@rambler.ru>
2017-03-11 22:10:44 +0300

    all checkstyle

--- homeworks/HW2/src/test/java/ru/atom/TestGameSessionCreator.java
+++ homeworks/HW2/src/test/java/ru/atom/TestGameSessionCreator.java
@@ -18,14 +18,14 @@ public final class TestGameSessionCreator {
 
     static GameSession createGameSession() {
         GameSession gameSession = new GameSession();
-        gameSession.addGameObject(new Bomb (5,5));
-        gameSession.addGameObject(new Bomb (0,100));
-        gameSession.addGameObject(new Bonus (10, 10));
-        gameSession.addGameObject(new Bonus (100, 200));
-        gameSession.addGameObject(new Box (50, 50));
-        gameSession.addGameObject(new Box (0,0));
-        gameSession.addGameObject(new Fire (20, 20));
-        gameSession.addGameObject(new Girl (150, 150));
-        return  gameSession;
+        gameSession.addGameObject(new Bomb(5, 5));
+        gameSession.addGameObject(new Bomb(0, 100));
+        gameSession.addGameObject(new Bonus(10, 10));
+        gameSession.addGameObject(new Bonus(100, 200));
+        gameSession.addGameObject(new Box(50, 50));
+        gameSession.addGameObject(new Box(0, 0));
+        gameSession.addGameObject(new Fire(20, 20));
+        gameSession.addGameObject(new Girl(150, 150));
+        return gameSession;
     }
 }
