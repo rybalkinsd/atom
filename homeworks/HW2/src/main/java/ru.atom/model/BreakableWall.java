b1619b0611521b0b7b2a25b1419d75ff7190032d
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-11 17:22:23 +0300

    wall

---
+++ homeworks/HW2/src/main/java/ru.atom/model/BreakableWall.java
@@ -0,0 +1,16 @@
+package ru.atom.model;
+
+import javafx.geometry.Pos;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.atom.geometry.Point;
+
+
+public class BreakableWall extends AbstractGameObject {
+    private static final Logger log = LogManager.getLogger(BreakableWall.class);
+
+    public BreakableWall(Point position) {
+        super(position.getX(), position.getY());
+        log.info("New breakable wall is set id={}, x={}, y={}", getId(), position.getX(), position.getY());
+    }
+}
