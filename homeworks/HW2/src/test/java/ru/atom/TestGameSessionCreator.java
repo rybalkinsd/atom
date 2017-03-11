70181fc5d589d7b88eb6c28e1630c52fb3e27742
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-11 13:04:48 +0300

    session test

--- homeworks/HW2/src/test/java/ru/atom/TestGameSessionCreator.java
+++ homeworks/HW2/src/test/java/ru/atom/TestGameSessionCreator.java
@@ -1,8 +1,11 @@
 package ru.atom;
 
 import ru.atom.geometry.Point;
-import ru.atom.model.Bomb;
+import ru.atom.model.AbstractGameObject;
 import ru.atom.model.GameSession;
+import ru.atom.model.Bomb;
+import ru.atom.model.BreakableWall;
+import ru.atom.model.UnbreakableWall;
 import ru.atom.model.Player;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -18,9 +21,17 @@ public final class TestGameSessionCreator {
     static GameSession createGameSession() {
         GameSession gameSession = new GameSession();
         //TODO populate your game session with sample objects
-        gameSession.addGameObject(new Player(1, 1));
-        gameSession.addGameObject(new Player(10, 10));
-        gameSession.addGameObject(new Bomb(3, 4));
+        gameSession.addGameObject(new Bomb(new Point(0, 0)));
+        gameSession.addGameObject(new Bomb(new Point(4, 4));
+
+        gameSession.addGameObject(new Player(new Point(1, 1)));
+        gameSession.addGameObject(new Player(new Point(8, 8)));
+
+        gameSession.addGameObject(new BreakableWall(new Point(5, 5)));
+        gameSession.addGameObject(new BreakableWall(new Point(2, 2)));
+
+        gameSession.addGameObject(new UnbreakableWall(new Point(7, 7)));
+        gameSession.addGameObject(new UnbreakableWall(new Point(4, 4)));
         return gameSession;
     }
 }
