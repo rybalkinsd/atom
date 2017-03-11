40a20ccaa262398540aeb61875916cac384f87c6
elBroom <Zarina1458@yandex.ru>
2017-03-11 19:51:40 +0300

    ContactListCacheTest Done Sayfullina Zarina

--- lecture04/src/main/java/ru/atom/cache/ContactListCache.java
+++ lecture04/src/main/java/ru/atom/cache/ContactListCache.java
@@ -1,34 +1,40 @@
 package ru.atom.cache;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * What about Map?
  */
 public class ContactListCache extends AbstractCache<Person, List<? extends Person>> {
+    private HashMap<Person, List<? extends Person>> memory;
+
     public ContactListCache(int capacity) {
         super(capacity);
+        this.memory = new HashMap<>(capacity);
     }
 
     @Override
     public boolean put(Person person, List<? extends Person> people) {
-        throw new NotImplementedException();
+        if (this.getSize() == this.getCapacity()) {
+            this.removeAny();
+        }
+        return this.memory.put(person, people) == people;
     }
 
     @Override
     public List<? extends Person> get(Person person) {
-        throw new NotImplementedException();
+        return this.memory.get(person);
     }
 
     @Override
     public int getSize() {
-        throw new NotImplementedException();
+        return this.memory.size();
     }
 
     private boolean removeAny() {
-        throw new NotImplementedException();
+        Person key = this.memory.keySet().iterator().next();
+        return this.memory.remove(key) != null;
     }
 
 }
--- lecture04/src/main/java/ru/atom/cache/Person.java
+++ lecture04/src/main/java/ru/atom/cache/Person.java
@@ -13,6 +13,17 @@ public class Person {
     }
 
     // your code here
+    public int hashCode() {
+        return this.firstName.hashCode() + this.firstName.hashCode();
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+
+        return this.hashCode() == person.hashCode();
+    }
 
     public String getFirstName() {
         return firstName;
