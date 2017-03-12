b1ace3cfa96422169243316e0f93489cbbaaf7a8
Egor <rentgen.94@mail.ru>
2017-03-12 14:53:35 +0300

    Try to pass checkstyle

--- lecture04/src/test/java/ru/atom/http/ChatClient.java
+++ lecture04/src/test/java/ru/atom/http/ChatClient.java
@@ -1,6 +1,10 @@
 package ru.atom.http;
 
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import sun.util.resources.cldr.ms.CalendarData_ms_MY;
 
