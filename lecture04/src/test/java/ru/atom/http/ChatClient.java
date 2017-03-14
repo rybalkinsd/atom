0a463e6e6631969cbfe13e2414c562a95eeeeb2e
zamakhaevV <zamakhaev_vlad@bk.ru>
2017-03-14 21:05:19 +0300

    ChatClient (прислал сообщение по время лекции)

--- lecture04/src/test/java/ru/atom/http/ChatClient.java
+++ lecture04/src/test/java/ru/atom/http/ChatClient.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class ChatClient {
     private static final OkHttpClient client = new OkHttpClient();
     private static final String PROTOCOL = "http://";
-    private static final String HOST = "localhost";
+    private static final String HOST = "wtfis.ru";
     private static final String PORT = ":8080";
 
     //GET host:port/chat/online
@@ -41,11 +41,22 @@ public class ChatClient {
     //GET host:port/chat/say?name=my_name
     //Body: "my_message"
     public static Response say(String name, String msg) throws IOException {
-        throw new NotImplementedException();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request = new Request.Builder()
+                .post(RequestBody.create(mediaType, "msg='" + msg + "'"))
+                .url(PROTOCOL + HOST + PORT + "/chat/say?name=" + name)
+               .build();
+
+        return client.newCall(request).execute();
     }
 
     //GET host:port/chat/chat
     public static Response viewChat() throws IOException {
-        throw new NotImplementedException();
+        Request request = new Request.Builder()
+                .get()
+                .url(PROTOCOL + HOST + PORT + "/chat/chat/")
+                .build();
+
+        return client.newCall(request).execute();
     }
 }
--- lecture04/src/test/java/ru/atom/http/ChatClientTest.java
+++ lecture04/src/test/java/ru/atom/http/ChatClientTest.java
@@ -7,10 +7,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-@Ignore
 public class ChatClientTest {
-    private static String MY_NAME_IN_CHAT = "I_AM_STUPID";
-    private static String MY_MESSAGE_TO_CHAT = "SOMEONE_KILL_ME";
+    private static String MY_NAME_IN_CHAT = "zamvl";
+    private static String MY_MESSAGE_TO_CHAT = "zamakhaev vlad";
 
     @Test
     public void viewOnline() throws IOException {
