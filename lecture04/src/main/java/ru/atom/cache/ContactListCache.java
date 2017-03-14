3bb1c7de6c1e300bfaeace4f10c5ce4002709860
Kartoxa1011 <mrantoshka1011@mail.ru>
2017-03-14 22:31:58 +0300

    Cash

--- lecture04/src/main/java/ru/atom/cache/ContactListCache.java
+++ lecture04/src/main/java/ru/atom/cache/ContactListCache.java
@@ -2,33 +2,47 @@ package ru.atom.cache;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * What about Map?
  */
 public class ContactListCache extends AbstractCache<Person, List<? extends Person>> {
+
+    private HashMap<Person, List> friend = new HashMap<Person, List>();
+
     public ContactListCache(int capacity) {
         super(capacity);
     }
 
     @Override
     public boolean put(Person person, List<? extends Person> people) {
-        throw new NotImplementedException();
+        if (friend.size() < capacity) {
+            friend.put(person, people);
+        } else {
+            removeAny();
+            friend.put(person, people);
+        }
+        return true;
     }
 
     @Override
     public List<? extends Person> get(Person person) {
-        throw new NotImplementedException();
+        return friend.get(person);
     }
 
     @Override
     public int getSize() {
-        throw new NotImplementedException();
+        return  friend.size();
     }
 
     private boolean removeAny() {
-        throw new NotImplementedException();
+        for (HashMap.Entry entry : friend.entrySet()) {
+            friend.remove(entry.getKey());
+            return true;
+        }
+        return false;
     }
 
 }
