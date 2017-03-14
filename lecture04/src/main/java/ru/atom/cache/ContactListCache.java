fd599d239c0678e77c517d5ccb073e59c75486ba
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-14 21:24:59 +0300

    userlist created

--- lecture04/src/main/java/ru/atom/cache/ContactListCache.java
+++ lecture04/src/main/java/ru/atom/cache/ContactListCache.java
@@ -1,34 +1,50 @@
 package ru.atom.cache;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * What about Map?
+ * What about Map?//почитать
  */
 public class ContactListCache extends AbstractCache<Person, List<? extends Person>> {
+    private int size = 0;
+    public Map<Person, List<? extends Person>> userList;
+
     public ContactListCache(int capacity) {
         super(capacity);
+        userList = new HashMap<Person, List<? extends Person>>(capacity);
     }
 
     @Override
     public boolean put(Person person, List<? extends Person> people) {
-        throw new NotImplementedException();
+        if (size == capacity) removeAny();
+        userList.put(person, people);
+        size++;
+        return true;
+
     }
 
     @Override
     public List<? extends Person> get(Person person) {
-        throw new NotImplementedException();
+        return userList.get(person);
+
     }
 
     @Override
     public int getSize() {
-        throw new NotImplementedException();
+        return size;
+
     }
 
     private boolean removeAny() {
-        throw new NotImplementedException();
+        for (Person key: userList.keySet()) {
+            userList.remove(key);
+            size--;
+            return true;
+        }
+        return false;
     }
 
 }
