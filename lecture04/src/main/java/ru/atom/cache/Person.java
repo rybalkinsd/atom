904c9be7953a93ba70b432afa2aad5436d805c61
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-14 21:25:36 +0300

    created user for map

--- lecture04/src/main/java/ru/atom/cache/Person.java
+++ lecture04/src/main/java/ru/atom/cache/Person.java
@@ -1,5 +1,8 @@
 package ru.atom.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Sergey Rybalkin on 11/03/17.
  */
@@ -7,12 +10,11 @@ public class Person {
     private final String firstName;
     private final String familyName;
 
+
     public Person(String firstName, String familyName) {
         this.firstName = firstName;
         this.familyName = familyName;
     }
-
-    // your code here
 
     public String getFirstName() {
         return firstName;
@@ -20,5 +22,25 @@ public class Person {
 
     public String getFamilyName() {
         return familyName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (getFirstName() != null ? !getFirstName().equals(person.getFirstName()) : person.getFirstName() != null)
+            return false;
+        return (getFamilyName() != null ? getFamilyName().equals(person.getFamilyName()) : person.getFamilyName()
+                == null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFirstName() != null ? getFirstName().hashCode() : 0;
+        result = 31 * result + (getFamilyName() != null ? getFamilyName().hashCode() : 0);
+        return result;
     }
 }
