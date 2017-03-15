783bdc91018fb5122dac793e02582f210ed6cb6b
rybalkinsd <yan.brikl@gmail.com>
2017-03-15 17:40:38 +0300

    lecture05 sources, presentation and practice

--- lecture05/source/src/main/java/ru/atom/model/scripts/persons/persons_create.sql
+++
@@ -1,9 +0,0 @@
--- DROP TABLE persons;
-
-CREATE TABLE IF NOT EXISTS persons
-(
-    id      SERIAL PRIMARY KEY NOT NULL,
-    name    VARCHAR(100)       NOT NULL,
-    gender  VARCHAR(10)        NOT NULL,
-    age     INTEGER            NOT NULL
-);
