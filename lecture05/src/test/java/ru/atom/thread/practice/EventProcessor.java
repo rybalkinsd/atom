8d258dd3769c4e9e65a9629fde22e507e04fe09e
elBroom <Zarina1458@yandex.ru>
2017-03-17 08:16:43 +0300

    lecture05 Sayfullina Zarina

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,7 +1,9 @@
 package ru.atom.thread.practice;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,15 +11,42 @@ import java.util.List;
  * @since 15.03.17
  */
 public class EventProcessor {
+    private static final Logger log = LogManager.getLogger(EventProcessor.class);
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new NotImplementedException();
+        List<Thread> threads = new ArrayList<>();
+        for (EventProducer eventProducer:eventProducers) {
+            Thread thread = new Thread(eventProducer);
+            log.info(thread.getName() + " created");
+            threads.add(thread);
+            log.info(thread.getName() + " start");
+            thread.start();
+            try {
+                log.info(thread.getName() + " join");
+                thread.join();
+            } catch (InterruptedException e) {
+                log.info(thread.getName() + " interrupt");
+                return;
+            }
+        }
+
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new NotImplementedException();
+        return countTotalNumberOfTypeEvents(Event.EventType.GOOD);
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new NotImplementedException();
+        return countTotalNumberOfTypeEvents(Event.EventType.BAD);
+    }
+
+    private static long countTotalNumberOfTypeEvents(Event.EventType type) {
+        long count = 0;
+        for (Event event: EventQueue.getInstance()) {
+            if (event.getEventType() == type) {
+                count++;
+            }
+        }
+        return count;
     }
 }
