bab452db97bc3bf4f4a8fc318366af698b29b30b
Ксения Кирпичникова < >
2017-03-17 22:51:14 +0300

    Реализованы функции

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,5 +1,7 @@
 package ru.atom.thread.practice;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
@@ -9,15 +11,33 @@ import java.util.List;
  * @since 15.03.17
  */
 public class EventProcessor {
-    public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new NotImplementedException();
+    private static final Logger log = LogManager.getLogger(EventProcessor.class);
+
+    public static void produceEvents(List<EventProducer> eventProducers) throws Exception {
+        for (int i = 0; i < eventProducers.size(); i++) {
+            Thread ext = new Thread(eventProducers.get(i));
+            ext.setName(eventProducers.get(i).getClass().getName());
+            log.info(ext.getName()  + " is created");
+            ext.start();
+            log.info(ext.getName() + " started");
+            ext.join();
+            log.info(ext.getName() + " joined");
+        }
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new NotImplementedException();
+        long totalNumberOfGoodEvents = 0;
+        for (Event ev:EventQueue.getInstance()) {
+            if (ev.getEventType() == Event.EventType.GOOD) totalNumberOfGoodEvents++;
+        }
+        return totalNumberOfGoodEvents;
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new NotImplementedException();
+        long totalNumberOfBadEvents = 0;
+        for (Event ev:EventQueue.getInstance()) {
+            if (ev.getEventType() == Event.EventType.BAD) totalNumberOfBadEvents++;
+        }
+        return totalNumberOfBadEvents;
     }
 }
