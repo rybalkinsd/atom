b1ae9ed37446e5aea5ca6ef7acf4b62839507332
GolovPavel <golovpasha@gmail.com>
2017-03-18 10:03:27 +0300

    EventProcessorTest passed

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,5 +1,6 @@
 package ru.atom.thread.practice;
 
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
@@ -9,15 +10,45 @@ import java.util.List;
  * @since 15.03.17
  */
 public class EventProcessor {
+
+    private static long goodEventsCount = 0;
+    private static long badEventsCount = 0;
+    private static long totalCount = 0;
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new NotImplementedException();
+        for (EventProducer producer : eventProducers) {
+            Thread thread = new Thread(producer);
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            countEvents(producer);
+        }
+    }
+
+    private static void countEvents(EventProducer producer) {
+        if (producer instanceof GoodEventProducer) {
+            long diff;
+            diff = EventQueue.getInstance().size() - totalCount;
+            goodEventsCount += diff;
+            totalCount = EventQueue.getInstance().size();
+        } else if (producer instanceof BadEventProducer) {
+            long diff;
+            diff = EventQueue.getInstance().size() - totalCount;
+            badEventsCount += diff;
+            totalCount = EventQueue.getInstance().size();
+        } else {
+            throw new NotImplementedException();
+        }
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new NotImplementedException();
+        return goodEventsCount;
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new NotImplementedException();
+        return badEventsCount;
     }
 }
