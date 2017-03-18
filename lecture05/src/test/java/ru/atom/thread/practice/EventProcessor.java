93cf5469f8e99eaca6ec83060c23ac27da5c4fce
zamakhaevV <zamakhaev_vlad@bk.ru>
2017-03-18 20:08:33 +0300

    Implement methods, fix test

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -2,22 +2,51 @@ package ru.atom.thread.practice;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author apomosov
  * @since 15.03.17
  */
 public class EventProcessor {
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new NotImplementedException();
+        List<Thread> producerThreads = eventProducers.stream().map(Thread::new).collect(Collectors.toList());
+        producerThreads.forEach(Thread::start);
+
+        for (Thread t : producerThreads){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new NotImplementedException();
+        long goodEvents = 0;
+
+        for (Event e : EventQueue.getInstance()) {
+            if (e.getEventType() == Event.EventType.GOOD) {
+                ++goodEvents;
+            }
+        }
+
+        return goodEvents;
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new NotImplementedException();
+        long badEvents = 0;
+
+        for (Event e : EventQueue.getInstance()) {
+            if (e.getEventType() == Event.EventType.BAD) {
+                ++badEvents;
+            }
+        }
+
+        return badEvents;
     }
 }
