402f3068e6e42d00681c01e18a97312e100334db
Egor <rentgen.94@mail.ru>
2017-03-18 13:23:21 +0300

    Lecture05. EventProcessor done.

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -2,22 +2,45 @@ package ru.atom.thread.practice;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author apomosov
  * @since 15.03.17
  */
 public class EventProcessor {
+    private static BlockingQueue<Event> events = EventQueue.getInstance();
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new NotImplementedException();
+        for (EventProducer eventProduce : eventProducers) {
+            Thread eventThread = new Thread(eventProduce);
+            eventThread.start();
+            try {
+                eventThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new NotImplementedException();
+        return countEventsWithType(Event.EventType.GOOD);
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new NotImplementedException();
+        return countEventsWithType(Event.EventType.BAD);
+    }
+
+    private static long countEventsWithType(Event.EventType type) {
+        long total = 0L;
+        for (Event event : events) {
+            if (event.getEventType() == type) {
+                total++;
+            }
+        }
+        return total;
     }
 }
