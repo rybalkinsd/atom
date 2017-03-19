696d5bc78693a118454abf0ecb3d73e054422720
zinnurovilnur <vip.zinnurov@mail.ru>
2017-03-20 02:32:48 +0300

    исправил cs

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -9,10 +9,12 @@ import java.util.List;
  * @since 15.03.17
  */
 public class EventProcessor {
-    static long good = 0, bad = 0;
+    static long good = 0;
+    static long bad = 0;
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        for(EventProducer event : eventProducers) {
-            if(event.getClass().toString().equals("class ru.atom.thread.practice.BadEventProducer")) {
+        for (EventProducer event : eventProducers) {
+            if (event.getClass().toString().equals("class ru.atom.thread.practice.BadEventProducer")) {
                 event.run();
                 bad += EventQueue.getInstance().size();
                 EventQueue.getInstance().clear();
