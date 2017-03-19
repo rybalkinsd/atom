948d615c01b747e1ef2cc9505475f946424d7d6c
Russiancold <elykovalex@gmail.com>
2017-03-19 22:07:08 +0300

    Threads

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,7 +1,9 @@
 package ru.atom.thread.practice;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,15 +11,37 @@ import java.util.List;
  * @since 15.03.17
  */
 public class EventProcessor {
+    private static final Logger log = LogManager.getLogger(EventProcessor.class);
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new NotImplementedException();
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (EventProducer it: eventProducers)
+            threads.add(new Thread(it));
+        for (Thread it: threads) {
+            it.start();
+            try {
+                it.join();
+            } catch (InterruptedException e) {
+                log.info("Interrupted");
+            }
+        }
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new NotImplementedException();
+        long numberOfGoodEvents = 0;
+        for (Event it:EventQueue.getInstance()) {
+            if (it.getEventType() == Event.EventType.GOOD)
+                numberOfGoodEvents++;
+        }
+        return numberOfGoodEvents;
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new NotImplementedException();
+        long numberOfBadEvents = 0;
+        for (Event it:EventQueue.getInstance()) {
+            if (it.getEventType() == Event.EventType.GOOD)
+                numberOfBadEvents++;
+        }
+        return numberOfBadEvents;
     }
 }
