35728a40cda0078b3689ab7b0c283b4f0836abd4
Hellooooy <Alexplusm@ya.ru>
2017-03-22 12:38:43 +0300

    event processor

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,23 +1,44 @@
 package ru.atom.thread.practice;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 
 import java.util.List;
+
+import static ru.atom.thread.practice.Event.EventType.BAD;
+import static ru.atom.thread.practice.Event.EventType.GOOD;
 
 /**
  * @author apomosov
  * @since 15.03.17
  */
 public class EventProcessor {
+
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new NotImplementedException();
+        for (EventProducer eventProducer : eventProducers)
+            new Thread(eventProducer).start();
+
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new NotImplementedException();
+        long counterGood = 0L;
+        for (Event e : EventQueue.getInstance()) {
+            if (e.getEventType() == GOOD) counterGood++;
+        }
+        return counterGood;
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new NotImplementedException();
+        long counterBad = 0L;
+        for (Event e : EventQueue.getInstance()) {
+            if (e.getEventType() == BAD) counterBad++;
+        }
+        return counterBad;
     }
 }
