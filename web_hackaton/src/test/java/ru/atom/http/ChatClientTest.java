f81bd3d6b19b2a24f872216318f062c7d5fe2f48
Ксения Кирпичникова < >
2017-03-22 21:00:49 +0300

    Hackaton

--- web_hackaton/src/test/java/ru/atom/http/ChatClientTest.java
+++ web_hackaton/src/test/java/ru/atom/http/ChatClientTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-@Ignore
+
 public class ChatClientTest {
-    private static String MY_NAME_IN_CHAT = "sasha";
+    private static String MY_NAME_IN_CHAT = "K";
     private static String MY_MESSAGE_TO_CHAT = "Всем привет в этом чатике!";
 
     @Test
