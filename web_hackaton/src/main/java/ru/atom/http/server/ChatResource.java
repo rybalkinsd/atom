a01aac05a5dc98bf42079ae1a6883cd33717a694
Dmitry Bragin <dmbragin@esc-center.ru>
2017-03-22 18:38:03 +0300

    init task

--- web_hackaton/src/main/java/ru/atom/http/server/ChatResource.java
+++ web_hackaton/src/main/java/ru/atom/http/server/ChatResource.java
@@ -16,33 +16,55 @@ import javax.ws.rs.core.Response;
 @Path("/chat")
 public class ChatResource {
     private static final Logger log = LogManager.getLogger(ChatResource.class);
+    private static final ConcurrentArrayQueue<String> logined = new ConcurrentArrayQueue<>();
+    private static final ConcurrentArrayQueue<String> chat = new ConcurrentArrayQueue<>();
 
     @POST
     @Consumes("application/x-www-form-urlencoded")
     @Path("/login")
     public Response login(@QueryParam("name") String name) {
-        return Response.status(Response.Status.NO_CONTENT).build();
+        if (name.length() > 30) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Too long name, sorry :(").build();
+        }
+        if (logined.contains(name)) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Already logined").build();
+        }
+        log.info("[" + name + "] logined");
+        logined.add(name);
+        chat.add("[" + name + "] joined");
+        return Response.ok().build();
     }
 
     @GET
     @Produces("text/plain")
     @Path("/online")
     public Response online() {
-        return Response.status(Response.Status.NO_CONTENT).build();
+        return Response.ok(String.join("\n", logined)).build();
     }
 
     @POST
     @Consumes("application/x-www-form-urlencoded")
     @Path("/say")
     public Response say(@QueryParam("name") String name, @FormParam("msg") String msg) {
-        return Response.status(Response.Status.NO_CONTENT).build();
+        if (!logined.contains(name)) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Not logined").build();
+        }
+        if (msg == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("No message provided").build();
+        }
+        if (msg.length() > 140) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Too long message").build();
+        }
+        log.info("[" + name + "]: " + msg);
+        chat.add("[" + name + "]: " + msg);
+        return Response.ok().build();
     }
 
     @GET
     @Produces("text/plain")
     @Path("/chat")
     public Response chat() {
-        return Response.status(Response.Status.NO_CONTENT).build();
+        return Response.ok(String.join("\n", chat)).build();
     }
 
     @POST
