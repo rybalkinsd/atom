e2877e16117d2d7a80d195255df01e883942f604
Arif Ergashev <orif95@mail.ru>
2017-03-22 20:59:17 +0300

    Эргашев, Рахлинский, Замахаев

--- web_hackaton/src/main/java/ru/atom/http/server/ChatResource.java
+++ web_hackaton/src/main/java/ru/atom/http/server/ChatResource.java
@@ -3,6 +3,7 @@ package ru.atom.http.server;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.util.ConcurrentArrayQueue;
+import ru.atom.http.client.ChatClient;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -12,16 +13,37 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-@Path("/chat")
+@Path("/")
 public class ChatResource {
     private static final Logger log = LogManager.getLogger(ChatResource.class);
+    private static final ConcurrentArrayQueue<String> logined = new ConcurrentArrayQueue<>();
+    private static final ConcurrentArrayQueue<String> chat = new ConcurrentArrayQueue<>();
+    private String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
 
     @POST
     @Consumes("application/x-www-form-urlencoded")
     @Path("/login")
-    public Response login(@QueryParam("name") String name) {
-        return Response.status(Response.Status.NO_CONTENT).build();
+    public Response login(@QueryParam("name") String name) throws IOException {
+        if (name.length() > 30) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Too long name :(").build();
+        }
+        if (name.toLowerCase().contains("gitler")) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Gitler not allowed, sorry :(").build();
+        }
+        if (logined.contains(name)) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Already logined").build();
+        }
+        log.info("[" + name + "] logined");
+        logined.add(name);
+        chat.add("[" + name + "] joined");
+        return Response.ok().build();
     }
 
     @GET
@@ -35,20 +57,40 @@ public class ChatResource {
     @Consumes("application/x-www-form-urlencoded")
     @Path("/say")
     public Response say(@QueryParam("name") String name, @FormParam("msg") String msg) {
-        return Response.status(Response.Status.NO_CONTENT).build();
+        if (!logined.contains(name)) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Not logined").build();
+        }
+        if (msg == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("No message provided").build();
+        }
+        if (msg.length() > 140) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Too long message").build();
+        }
+        String temp;
+        if (msg.contains("http") || msg.contains("https") || msg.contains("www")) {
+            temp = "<a href=\"" + msg + "\">" + msg + "</a>";
+        } else {
+            temp = msg;
+        }
+        log.info("[" + name + "]: " + temp);
+        chat.add("[" + name + "]: " + temp);
+        return Response.ok().build();
     }
 
     @GET
     @Produces("text/plain")
     @Path("/chat")
     public Response chat() {
-        return Response.status(Response.Status.NO_CONTENT).build();
+        return Response.ok(String.join("\n", chat)).build();
     }
 
     @POST
     @Consumes("application/x-www-form-urlencoded")
     @Path("/logout")
     public Response logout(@QueryParam("name") String name) {
-        return Response.status(Response.Status.NO_CONTENT).build();
+        log.info("[" + name + "] logined out");
+        logined.remove(name);
+        chat.add("[" + name + "] leaved");
+        return Response.ok().build();
     }
 }
