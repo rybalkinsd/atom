63e7e3cb9b2c3bb106854364198917549651dc18
Ilya Lysko <ilya.lysko@gmail.com>
2017-03-22 21:10:37 +0300

    Сделали так, что все собирается. Лыско Илья, Баранов Михаил, Фищенко Элла.

--- web_hackaton/src/test/java/ru/atom/http/ChatClientTest2.java
+++ web_hackaton/src/test/java/ru/atom/http/ChatClientTest2.java
@@ -1,4 +1,5 @@
 package ru.atom.http;
+
 import javax.ws.rs.core.Response;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,5 +17,37 @@ public class ChatClientTest2 {
         Response response = chatResource.login("1234567890123456789012345678901234567890");
         Assert.assertEquals("Too long name, sorry :(",(String)response.getEntity());
     }
+
+    @Test
+    public void post2() throws IOException {
+        ChatResource chatResource1 = new ChatResource();
+        chatResource1.login("Ella");
+        ChatResource chatResource2 = new ChatResource();
+        Response response = chatResource2.login("Ella");
+        Assert.assertEquals("Already logined",(String)response.getEntity());
+    }
+
+    @Test
+    public void post3() throws IOException {
+        ChatResource chatResource = new ChatResource();
+        Response response = chatResource.login("NewName");
+        Assert.assertEquals(200,response.getStatus());
+    }
+
+    @Test
+    public void online() throws IOException {
+        ChatResource chatResource = new ChatResource();
+        Response response = chatResource.online();
+        Assert.assertEquals(200,response.getStatus());
+    }
+
+    @Test
+    public void say() throws IOException {
+        ChatResource chatResource = new ChatResource();
+        Response response = chatResource.online();
+        Assert.assertEquals(200,response.getStatus());
+    }
+
+
 
 }
