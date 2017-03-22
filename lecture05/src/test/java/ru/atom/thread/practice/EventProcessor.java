7ec683b7acc64017d412e22ebec80e00c0735900
ruslbizhNetcr <ruslbizhNetcr@example.ru>
2017-03-22 13:45:21 +0300

    lecture 5 last version by Ruslan Bizhanov

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,20 +1,36 @@
 package ru.atom.thread.practice;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author apomosov
  * @since 15.03.17
  */
 public class EventProcessor {
+    public static final Logger log = LogManager.getLogger(EventProcessor.class);
+
     public static void produceEvents(List<EventProducer> eventProducers) {
+        ArrayList<Thread> threads = new ArrayList<>(eventProducers.size());
         for (EventProducer ev : eventProducers) {
-            ev.run();
+            threads.add(new Thread(ev));
+        }
+
+        threads.forEach(Thread::start);
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                log.warn("InterruptedException");
+            }
         }
     }
+
 
     public static long countTotalNumberOfGoodEvents() {
         Iterator it = EventQueue.getInstance().iterator();
