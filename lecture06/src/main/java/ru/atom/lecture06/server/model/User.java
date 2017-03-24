fc647039fbff59167d1e3847370851ae4c42e172
rybalkinsd <yan.brikl@gmail.com>
2017-03-25 01:13:10 +0300

    lecture06

---
+++ lecture06/src/main/java/ru/atom/lecture06/server/model/User.java
@@ -0,0 +1,35 @@
+package ru.atom.lecture06.server.model;
+
+/**
+ * Created by sergey on 3/25/17.
+ */
+public class User {
+    private int id;
+    private String login;
+
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public User setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                '}';
+    }
+}
