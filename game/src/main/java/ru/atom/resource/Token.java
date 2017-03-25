973682700989153e9f032cd2d6f13fb638518bfa
elBroom <Zarina1458@yandex.ru>
2017-03-25 18:09:09 +0300

    rk01

---
+++ game/src/main/java/ru/atom/resource/Token.java
@@ -0,0 +1,30 @@
+package ru.atom.resource;
+
+
+/**
+ * Created by zarina on 23.03.17.
+ */
+public class Token {
+    private Long token;
+    private User user;
+
+    public Token(User user) {
+        this.user = user;
+        this.token = System.currentTimeMillis() * 100000L + (long) user.hashCode();
+    }
+
+    public Long getToken() {
+        return token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String toString() {
+        return getToken().toString();
+    }
+
+
+}
