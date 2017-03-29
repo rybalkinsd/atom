6449c7ee4f0c34665bcfa84c8a47b8e6c094a593
johnnyk <ivan.kozin.96@gmail.com>
2017-03-29 19:04:46 +0300

    Rk1 the hottest fix

--- tasks/1-in-memory-account-server/src/main/java/ru/atom/authserver/AuthFilter.java
+++ tasks/1-in-memory-account-server/src/main/java/ru/atom/authserver/AuthFilter.java
@@ -36,7 +36,7 @@ public class AuthFilter implements ContainerRequestFilter {
 
     private void validate(String tokenString) throws Exception {
         Long token = Long.valueOf(tokenString);
-        if(!TokenContainer.validate(token)) {
+        if (!TokenContainer.validate(token)) {
             throw new Exception();
         }
     }
