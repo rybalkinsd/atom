762e132cb179ac86df75a479f1f005e6ec1a0fc7
GolovPavel <golovpasha@gmail.com>
2017-03-29 18:44:13 +0300

    Revert "jsonlib added"
    This reverts commit 2ad5901fb5d277e29a14fb4684e78f6af36f90bd.

--- tasks/1-in-memory-account-server/src/main/java/ru/atom/DataResourceRk.java
+++ tasks/1-in-memory-account-server/src/main/java/ru/atom/DataResourceRk.java
@@ -1,7 +1,5 @@
 package ru.atom;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,10 +19,26 @@ public class DataResourceRk {
     @GET
     @Produces("application/json")
     @Path("/users")
-    public Response users() throws JsonProcessingException {
+    public Response users() {
         Collection<UserRk> logginedUsers = UserContainerRk.getLogginedUsers();
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(logginedUsers);
-        return Response.ok("{\"users\" : " + json + "}").build();
+
+        if (logginedUsers.isEmpty()) {
+            logger.info("UserRk list is showned");
+            return Response.ok("{\"users\" : []}").build();
+        }
+
+        StringBuilder builder = new StringBuilder(
+                "{\"users\" : [");
+
+        for (UserRk user: logginedUsers) {
+            builder.append("{")
+                    .append(user.getName())
+                    .append("}, ");
+        }
+
+        builder.delete(builder.length() - 2, builder.length());
+        builder.append("]}");
+        logger.info("UserRk list is showned");
+        return Response.ok(builder.toString()).build();
     }
 }
