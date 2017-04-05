e3ba6677f1cf8bf2d48e2f10cdd79ac82335d9b7
Ilya Lysko <ilya.lysko@gmail.com>
2017-04-06 00:10:41 +0300

    update for travis

--- lecture07/src/main/java/ru/atom/lecture07/server/model/Message.java
+++ lecture07/src/main/java/ru/atom/lecture07/server/model/Message.java
@@ -1,6 +1,13 @@
 package ru.atom.lecture07.server.model;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
