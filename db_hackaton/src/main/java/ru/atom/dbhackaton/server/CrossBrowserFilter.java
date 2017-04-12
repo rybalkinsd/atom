2eced481592113b69b25f51685f05e76d47e7763
rybalkinsd <yan.brikl@gmail.com>
2017-04-12 12:25:10 +0300

    db hackaton initial

---
+++ db_hackaton/src/main/java/ru/atom/dbhackaton/server/CrossBrowserFilter.java
@@ -0,0 +1,22 @@
+package ru.atom.dbhackaton.server;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CrossBrowserFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext request,
+                       ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
