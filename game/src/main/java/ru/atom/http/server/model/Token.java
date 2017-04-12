5ee5780a37c3eb5814b34bceb59ba9ebd4931c1c
Areksei <areksei59@gmail.com>
2017-04-12 22:04:20 +0300

    token

--- game/src/main/java/ru/atom/http/server/model/Token.java
+++ game/src/main/java/ru/atom/http/server/model/Token.java
@@ -1,29 +1,68 @@
 package ru.atom.http.server.model;
 
 
-/**
- * Created by zarina on 23.03.17.
- */
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "token", schema = "game")
 public class Token {
-    private Long token;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    @OneToOne(cascade = CascadeType.PERSIST)
     private User user;
 
-    public Token(User user) {
-        this.user = user;
-        this.token = System.currentTimeMillis() * 100000L + (long) user.hashCode();
-    }
+    @Column
+    @CreationTimestamp
+    private Date time;
 
-    public Long getToken() {
-        return token;
+    @Column(name = "value", nullable = false)
+    private Long token;
+
+    public Integer getId() {
+        return id;
     }
 
     public User getUser() {
         return user;
     }
 
+    public Long getToken() {
+        return token;
+    }
+
+    public Token setUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public Token setToken(Long token) {
+        this.token = token;
+        return this;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public Token setTime(Date timestamp) {
+        this.time = timestamp;
+        return this;
+    }
+
     @Override
     public String toString() {
-        return getToken().toString();
+            return "Token{" +
+                    "id=" + id +
+                    "userId=" + user.getName() +
+                    ", timestamp=" + time +
+                    ", value='" + token + '\'' +
+                    '}';
     }
 
 
