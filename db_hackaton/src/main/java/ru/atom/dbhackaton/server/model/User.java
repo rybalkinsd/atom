e6d8ee9c862073f673938ea1e6e613e68f061407
nikfuri <nikfurirus@gmail.com>
2017-04-12 18:55:27 +0300

    User

---
+++ db_hackaton/src/main/java/ru/atom/dbhackaton/server/model/User.java
@@ -0,0 +1,56 @@
+package ru.atom.dbhackaton.server.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "user", schema = "hackaton")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password_hash", nullable = false)
+    private Integer passwordHash;
+
+    @Column(name = "register_date", nullable = false)
+    private Date registerDate;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(Integer passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+}
