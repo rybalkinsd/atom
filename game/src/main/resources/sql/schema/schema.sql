98223732c78a6a510c5cfa81bd34aa436b6f6a3e
elBroom <Zarina1458@yandex.ru>
2017-04-13 02:01:32 +0300

    add MM schema

--- game/src/main/resources/sql/schema/schema.sql
+++ game/src/main/resources/sql/schema/schema.sql
@@ -17,8 +17,30 @@ drop table if exists game.token;
 create table game.token (
   id     serial       not null,
   user_id integer      not null references game.user on delete cascade,
-  create_at   timestamp    not null,
   value  varchar(140) not null,
+  create_at   timestamp    not null,
+
+  primary key (id)
+);
+
+drop table if exists game.game;
+create table game.game (
+  id    serial             not null,
+  player1 integer      not null references game.user,
+  player2 integer      not null references game.user,
+  player3 integer      not null references game.user,
+  player4 integer      not null references game.user,
+  create_at   timestamp    not null,
+
+  primary key (id)
+);
+
+drop table if exists game.score;
+create table game.score (
+  id    serial             not null,
+  user_id integer      not null references game.user on delete cascade,
+  score integer default 0,
+  game_id integer not null references game.game,
 
   primary key (id)
 );
