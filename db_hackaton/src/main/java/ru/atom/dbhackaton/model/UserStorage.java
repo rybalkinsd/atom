8a26863bd4ec643d9600aed74ab66671227d0976
Ilya Lysko <ilya.lysko@gmail.com>
2017-04-12 19:39:34 +0300

    Changes in UserStorage.

--- db_hackaton/src/main/java/ru/atom/dbhackaton/model/UserStorage.java
+++ db_hackaton/src/main/java/ru/atom/dbhackaton/model/UserStorage.java
@@ -1,6 +1,7 @@
 package ru.atom.dbhackaton.model;
 
 import org.hibernate.Session;
+import ru.atom.dbhackaton.hibernate.RegistredEntity;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,26 +9,26 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by vladfedorenko on 26.03.17.
  */
 public class UserStorage {
-    private static ConcurrentHashMap<String, User> storage = new ConcurrentHashMap();
+    private static ConcurrentHashMap<String, RegistredEntity> storage = new ConcurrentHashMap();
 
-    public void insert(Session session, User user) {
+    public void insert(Session session, RegistredEntity user) {
         session.saveOrUpdate(user);
     }
 
-    public static User getByName(Session session, User user) {
+    public static User getByName(Session session, RegistredEntity user) {
         return (User) session
-                .createQuery("from User where login = :user")
+                .createQuery("from RegistredEntity where login = :user")
                 .setParameter("name", user)
                 .uniqueResult();
     }
 
-    public boolean userExists(Session session, String user) {
+    public boolean userExists(Session session, RegistredEntity user) {
         org.hibernate.Query query = session.createQuery("from users where username = :user");
         query.setParameter("username", user);
         return query.uniqueResult() != null;
     }
 
-    public void dropUser(Session session, User userToDelete) {
+    public void dropUser(Session session, RegistredEntity userToDelete) {
         session.delete(userToDelete);
     }
 }
