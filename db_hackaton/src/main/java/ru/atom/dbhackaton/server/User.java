858b2b3cd8a9b279421e13b9fb98c943f8d24919
gammaker <vav-94@mail.ru>
2017-04-12 18:58:42 +0300

    Hibernate

--- db_hackaton/src/main/java/ru/atom/dbhackaton/server/User.java
+++ db_hackaton/src/main/java/ru/atom/dbhackaton/server/User.java
@@ -1,12 +1,30 @@
 package ru.atom.dbhackaton.server;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
 /**
  * Created by gammaker on 25.03.2017.
  */
 
+@Entity
+@Table(name = "user", schema = "chat")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "login", unique = true, nullable = false, length = 20)
     public final String name; //unique identifier of user
+
+    @Column(name = "password", nullable = false, length = 20)
     public final String password;
+
     private Token token;
 
     public User(String name, String password) {
