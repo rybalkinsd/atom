9de07b97c98c8ca0595d8215eb47838dfc3fb0cc
Seregey Smirnov <kurchatov1996@yandex.ru>
2017-04-12 18:42:55 +0300

    Merge

---
+++ db_hackaton/src/main/java/ru/atom/User.java
@@ -0,0 +1,42 @@
+package ru.atom;
+
+/**
+ * Created by serega on 26.03.17.
+ */
+public class User {
+    private String name;
+    private transient String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof User)) return false;
+        User user = (User) o;
+        return this.getName().equals(user.getName()) && this.getPassword().equals(user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "name=" + name + ", password=" + password + "}";
+    }
+}
