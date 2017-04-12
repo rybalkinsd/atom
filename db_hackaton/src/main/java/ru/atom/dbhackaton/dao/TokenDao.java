24aace15fd06a5405715c1be65ba51d414468609
SaenkoDmitry <dmitryssaenko@gmail.com>
2017-04-12 19:55:35 +0300

    added dao

---
+++ db_hackaton/src/main/java/ru/atom/dbhackaton/dao/TokenDao.java
@@ -0,0 +1,36 @@
+package ru.atom.dbhackaton.dao;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import ru.atom.dbhackaton.model.Token;
+import ru.atom.dbhackaton.model.User;
+
+import java.util.List;
+
+/**
+ * Created by dmitriy on 12.04.17.
+ */
+public class TokenDao {
+
+    private static final Logger log = LogManager.getLogger(UserDao.class);
+
+    private static TokenDao instance = new TokenDao();
+
+    public static TokenDao getInstance() {
+        return instance;
+    }
+
+    private TokenDao() {}
+
+    public List<Token> getAll(Session session) {
+        return session.createCriteria(Token.class).list();
+    }
+
+    public Token getByStrToken(Session session, String strToken) {
+        return (Token) session
+                .createQuery("from Token where token = :strToken")
+                .setParameter("strToken", strToken)
+                .uniqueResult();
+    }
+}
