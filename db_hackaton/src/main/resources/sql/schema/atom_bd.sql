d6fc76c7d87c5c2dab25a22e0b2c72fa6dbee7b9
Dmitry Bragin <dmbragin@esc-center.ru>
2017-04-12 18:40:52 +0300

    add sql sheme and hibernate config

---
+++ db_hackaton/src/main/resources/sql/schema/atom_bd.sql
@@ -0,0 +1,23 @@
+begin;
+
+drop schema if exists auth cascade;
+create schema auth;
+
+drop table if exists auth.user;
+create table auth.user (
+  id    serial              not null,
+  login varchar(20)         unique not null,
+  passwd varchar(20)        not null,
+  registration_date         TIMESTAMP not null DEFAULT NOW(),
+  primary key (id)
+);
+
+drop table if exists auth.token;
+create table auth.token (
+  id    serial      not null,
+  token varchar(20) unique not null,
+  user_id integer   unique not null references auth.user on delete cascade,
+  primary key (id)
+);
+
+commit;
