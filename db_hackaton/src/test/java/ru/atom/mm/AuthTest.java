258fd0fbdae481ff095136e4fe23a13bb8a926c1
vic0din <Ska-vlad007@yandex.ru>
2017-04-19 18:05:23 +0300

    some tests were added

--- db_hackaton/src/test/java/ru/atom/mm/AuthTest.java
+++ db_hackaton/src/test/java/ru/atom/mm/AuthTest.java
@@ -1,16 +1,46 @@
 package ru.atom.mm;
 
-import org.junit.Ignore;
+import okhttp3.Response;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.After;
+import ru.atom.client.AuthClient;
 import ru.atom.dbhackaton.resource.User;
+import ru.atom.dbhackaton.server.AuthServer;
 
 import java.util.Random;
 
 /**
  * Created by BBPax on 19.04.17.
  */
-@Ignore
 public class AuthTest {
     private User newUser = new User().setLogin("user" + new Random()
             .nextInt(999999))
             .setPassword("password");
+
+    @Before
+    public void setUp() throws Exception {
+        AuthServer.startUp();
+        AuthClient.registration(newUser);
+    }
+    
+    @Test
+    public void registrationTest() throws Exception {
+        Response response = AuthClient.registration(newUser);
+        Assert.assertEquals(400, response.code());
+    }
+
+    @Test
+    public void loginTest() throws Exception {
+        Response response = AuthClient.login(newUser);
+        Assert.assertEquals(200, response.code());
+        response = AuthClient.login(new User().setLogin("notExisted").setPassword("wrongpass"));
+        Assert.assertEquals(400, response.code());
+    }
+
+    @After
+    public void shutdown() throws Exception {
+        AuthServer.shutdown();
+    }
 }
