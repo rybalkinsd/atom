f3903f6a5355b18aee487e6c8f21c9ff7b4b8492
Alpi <Alpieex@gmail.com>
2017-04-27 01:08:00 +0300

    Ticker

---
+++ homeworks/HW3/src/main/java/ru.atom/Ticker.java
@@ -0,0 +1,38 @@
+package ru.atom.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class Ticker {
+    private static final Logger log = LogManager.getLogger(Ticker.class);
+    private static final int FPS = 60;
+    private static final long FRAME_TIME = 1000 / FPS;
+    private long tickNumber = 0;
+
+    public void loop() {
+        while (!Thread.currentThread().isInterrupted()) {
+            long started = System.currentTimeMillis();
+            act(FRAME_TIME);
+            long elapsed = System.currentTimeMillis() - started;
+            if (elapsed < FRAME_TIME) {
+                log.info("All tick finish at {} ms", elapsed);
+                LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(FRAME_TIME - elapsed));
+            } else {
+                log.warn("tick lag {} ms", elapsed - FRAME_TIME);
+            }
+            log.info("{}: tick ", tickNumber);
+            tickNumber++;
+        }
+    }
+
+    private void act(long time) {
+        //Your logic here
+    }
+
+    public long getTickNumber() {
+        return tickNumber;
+    }
+}
