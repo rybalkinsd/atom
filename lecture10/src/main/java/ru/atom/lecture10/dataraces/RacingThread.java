dea3825598e341d2ab4fb4ee9e91a160b0342d40
Alpi <Alpieex@gmail.com>
2017-04-26 14:08:33 +0300

    lecture10

---
+++ lecture10/src/main/java/ru/atom/lecture10/dataraces/RacingThread.java
@@ -0,0 +1,24 @@
+package ru.atom.lecture10.dataraces;
+
+public final class RacingThread extends Thread {
+    private final Stopper checker;
+    private static int value;
+
+    public RacingThread(Stopper checker) {
+        this.checker = checker;
+    }
+
+    public void run() {
+        while (!checker.stop()) {
+            increment();
+        }
+    }
+
+    public void increment() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
