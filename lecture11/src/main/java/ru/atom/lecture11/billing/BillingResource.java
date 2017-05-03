04498d58598ff3955b9716ec3dcda29bb9e440d1
Alpi <Alpieex@gmail.com>
2017-05-03 03:11:37 +0300

    lecture11

--- lecture11/src/main/java/ru/atom/lecture11/billing/BillingResource.java
+++ lecture11/src/main/java/ru/atom/lecture11/billing/BillingResource.java
@@ -18,26 +18,30 @@ public class BillingResource {
     @POST
     @Consumes("application/x-www-form-urlencoded")
     @Path("/addUser")
-    public Response addUser(@FormParam("user") String user, @FormParam("money") Integer money) {
-        if(user == null || money == null){
+    public Response addUser(@FormParam("user") String user,
+                            @FormParam("money") Integer money) {
+        if (user == null || money == null) {
             return Response.status(401).entity("Not such info\n").build();
         }
         userToMoney.put(user, money);
-        return Response.ok("Succesfully created user [" + user + "] with money " + userToMoney.get(user) + "\n").build();
+        return Response.ok("Succesfully created user [" + user + "] with money " + userToMoney.get(user) + "\n")
+                .build();
     }
-    
+
     @POST
     @Consumes("application/x-www-form-urlencoded")
     @Path("/sendMoney")
-    public Response sendMoney(@FormParam("from") String fromUser, @FormParam("to") String toUser, @FormParam("money") Integer money) {
+    public Response sendMoney(@FormParam("from") String fromUser,
+                              @FormParam("to") String toUser,
+                              @FormParam("money") Integer money) {
         if (fromUser == null || toUser == null || money == null) {
-          return Response.status(401).entity("Not such info\n").build();
+            return Response.status(401).entity("Not such info\n").build();
         }
         if (!userToMoney.containsKey(fromUser) || !userToMoney.containsKey(toUser)) {
-          return Response.status(401).entity("No such user\n").build();
+            return Response.status(401).entity("No such user\n").build();
         }
         if (userToMoney.get(fromUser) < money) {
-          return Response.status(401).entity("Not enough money to send\n").build();
+            return Response.status(401).entity("Not enough money to send\n").build();
         }
         userToMoney.put(fromUser, userToMoney.get(fromUser) - money);
         userToMoney.put(toUser, userToMoney.get(toUser) + money);
