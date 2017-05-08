a069ea17bafaa6622c8eb03f796c868b123fcd8b
Alpi <Alpieex@gmail.com>
2017-05-08 13:16:07 +0300

    lecture12

---
+++ lecture12/src/main/java/org/openjdk/jol/info/GraphWalker.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jol.info;
+
+import org.openjdk.jol.util.ObjectUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Basic class to walk object graphs.
+ *
+ * @author Aleksey Shipilev
+ */
+public class GraphWalker {
+
+    private final Set<Object> visited;
+    private final Object[] roots;
+    private final Collection<GraphVisitor> visitors;
+
+    public GraphWalker(Object... roots) {
+        this.roots = roots;
+        this.visited = Collections.newSetFromMap(new IdentityHashMap<Object, Boolean>());
+        this.visitors = new ArrayList<GraphVisitor>();
+    }
+
+    public void addVisitor(GraphVisitor v) {
+        visitors.add(v);
+    }
+
+    public void walk() {
+        List<GraphPathRecord> curLayer = new ArrayList<GraphPathRecord>();
+        List<GraphPathRecord> newLayer = new ArrayList<GraphPathRecord>();
+
+        int rootId = 1;
+        boolean single = (roots.length == 1);
+        for (Object root : roots) {
+            String label = single ? "" : ("<r" + rootId + ">");
+            GraphPathRecord e = new GraphPathRecord(label, 0, root);
+            visited.add(root);
+            visitObject(e);
+            curLayer.add(e);
+            rootId++;
+        }
+
+        while (!curLayer.isEmpty()) {
+            newLayer.clear();
+            for (GraphPathRecord next : curLayer) {
+                for (GraphPathRecord ref : peelReferences(next)) {
+                    if (visited.add(ref.obj())) {
+                        visitObject(ref);
+                        newLayer.add(ref);
+                    }
+                }
+            }
+            curLayer.clear();
+            curLayer.addAll(newLayer);
+        }
+    }
+
+    private void visitObject(GraphPathRecord record) {
+        for (GraphVisitor v : visitors) {
+            v.visit(record);
+        }
+    }
+
+    private List<GraphPathRecord> peelReferences(GraphPathRecord r) {
+        Object o = r.obj();
+
+        if (o == null) {
+            // Nothing to do here
+            return Collections.emptyList();
+        }
+
+        if (o.getClass().isArray() && o.getClass().getComponentType().isPrimitive()) {
+            // Nothing to do here
+            return Collections.emptyList();
+        }
+
+        List<GraphPathRecord> result = new ArrayList<GraphPathRecord>();
+
+        if (o.getClass().isArray()) {
+            int c = 0;
+            for (Object e : (Object[]) o) {
+                if (e != null) {
+                    result.add(new GraphPathRecord(r.path() + "[" + c + "]", r.depth() + 1, e));
+                }
+                c++;
+            }
+        } else {
+            for (Field f : getAllReferences(o.getClass())) {
+                Object e = ObjectUtils.value(o, f);
+                if (e != null) {
+                    result.add(new GraphPathRecord(r.path() + "." + f.getName(), r.depth() + 1, e));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private Collection<Field> getAllReferences(Class<?> klass) {
+        List<Field> results = new ArrayList<Field>();
+
+        for (Field f : klass.getDeclaredFields()) {
+            if (Modifier.isStatic(f.getModifiers())) continue;
+            if (f.getType().isPrimitive()) continue;
+            results.add(f);
+        }
+
+        Class<?> superKlass = klass;
+        while ((superKlass = superKlass.getSuperclass()) != null) {
+            for (Field f : superKlass.getDeclaredFields()) {
+                if (Modifier.isStatic(f.getModifiers())) continue;
+                if (f.getType().isPrimitive()) continue;
+                results.add(f);
+            }
+        }
+
+        return results;
+    }
+
+}
--- lecture12/src/main/java/ru/atom/lecture12/GuessSize.java
+++ lecture12/src/main/java/ru/atom/lecture12/GuessSize.java
@@ -1,36 +1,4 @@
-package ru.atom.lecture12.jol;
-
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-package jol;
+package ru.atom.lecture12;
 
 import org.openjdk.jol.info.ClassLayout;
 import org.openjdk.jol.info.GraphLayout;
@@ -38,26 +6,16 @@ import org.openjdk.jol.vm.VM;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static java.lang.System.in;
 import static java.lang.System.out;
 
-/**
- * @author Aleksey Shipilev
- */
 public class GuessSize {
-
-    /*
-     * This sample showcases the basic field layout.
-     * You can see a few notable things here:
-     *   a) how much the object header consumes;
-     *   b) how fields are laid out;
-     *   c) how the external alignment beefs up the object size
-     */
-
   public static void main(String[] args) throws Exception {
     out.println(VM.current().details());
     int[] intArray = new int[1000];
@@ -65,6 +23,8 @@ public class GuessSize {
     ArrayList<Integer> arrayList = new ArrayList<>();
     LinkedList<Integer> linkedList = new LinkedList<>();
     HashSet<Integer> hashSet = new HashSet<>();
+    HashMap<Integer, Integer> hashMap = new HashMap<>();
+    ConcurrentHashMap<Integer, Integer> concurrentHashMap = new ConcurrentHashMap<>();
 
     for (int i = 0; i < 1000; i++) {
       Integer io = i;
@@ -73,27 +33,47 @@ public class GuessSize {
       arrayList.add(io);
       linkedList.add(io);
       hashSet.add(io);
+      hashMap.put(i,i);
+      concurrentHashMap.put(i,i);
     }
 
     arrayList.trimToSize();
 
     PrintWriter pw = new PrintWriter(out);
+    pw.println("====================================================================");
+    pw.println(">> new String(\"Hello, World!\")");
+    pw.println(GraphLayout.parseInstance("Hello, World!").toPrintable());
+    pw.println("====================================================================");
     pw.println(">> new Integer(10)");
     pw.println(GraphLayout.parseInstance(new Integer(10)).toPrintable());
+    pw.println("====================================================================");
     pw.println(">> new Long(10)");
     pw.println(GraphLayout.parseInstance(new Long(10)).toPrintable());
+    pw.println("====================================================================");
     pw.println(">> new int[1000]");
     pw.println(GraphLayout.parseInstance((Object) intArray).toFootprint());
+    pw.println("====================================================================");
     pw.println(">> new Integer[1000]");
     pw.println(GraphLayout.parseInstance((Object) integerArray).toFootprint());
+    pw.println("====================================================================");
     pw.println(">> new ArrayList<Integer>(1000)");
     pw.println(GraphLayout.parseInstance(arrayList).toFootprint());
+    pw.println("====================================================================");
     pw.println(">> new LinkedList<Integer>(1000)");
     pw.println(GraphLayout.parseInstance(linkedList).toFootprint());
+    pw.println("====================================================================");
     pw.println(">> new HashSet<Integer>(1000)");
     pw.println(GraphLayout.parseInstance(hashSet).toFootprint());
+    pw.println("====================================================================");
+    pw.println(">> new HashMap<Integer>(1000)");
+    pw.println(GraphLayout.parseInstance(hashMap).toFootprint());
+    pw.println("====================================================================");
+    pw.println(">> new ConcurrentHashMap<Integer>(1000)");
+    pw.println(GraphLayout.parseInstance(concurrentHashMap).toFootprint());
+    pw.println("====================================================================");
     pw.println(">> new ArrayList<Integer>(1000).stream()");
     pw.println(GraphLayout.parseInstance(arrayList.stream()).toFootprint());
+    pw.println("====================================================================");
     pw.close();
   }
 }
