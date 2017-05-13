9f82c763dae7fd932e7d1830d510b98ff383572a
Sergey Rybalkin <yan.brikl@gmail.com>
2017-05-13 16:13:25 +0300

    lecture12

--- lecture12/src/main/java/ru/atom/lecture12/GarbageProducer.java
+++ lecture12/src/main/java/ru/atom/lecture12/GarbageProducer.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class GarbageProducer {
     static List<Object> l;
     
-    public static void main(String... args) {
+    public static void main(String... args) throws InterruptedException {
+//        Thread.sleep(30_000);
+
         l = new ArrayList<>();
         for (int c = 0; c < 100_000_000; c++) {
             l.add(new Object());
