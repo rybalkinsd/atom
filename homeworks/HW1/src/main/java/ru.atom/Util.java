367a36e39ccaa039634237cbee05e5283fc6a1ca
Kirill Slesarev <kirslesarev@gmail.com>
2017-09-22 22:49:41 +0300

    Добавление реализации методов max и sum

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -3,11 +3,9 @@ package ru.atom;
 /**
  * In this assignment you need to implement the following util methods.
  * Note:
- *  throw new UnsupportedOperationException(); - is just a stub
+ * throw new UnsupportedOperationException(); - is just a stub
  */
 public class Util {
-
-
 
     /**
      * Returns the greatest of {@code int} values.
@@ -16,7 +14,11 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int tecmax = values[0];
+        for (int i = 1; i < values.length; i++)
+            if (tecmax < values[i])
+                tecmax = values[i];
+        return (tecmax);
     }
 
     /**
@@ -25,9 +27,12 @@ public class Util {
      * @param values an argument. Assume values.length > 0.
      * @return the sum of all values.
      */
-    public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
-    }
 
+    public static long sum(int[] values) {
+        int tecsum = values[0];
+        for (int i = 1; i < values.length; i++)
+            tecsum = tecsum + values[i];
+        return (tecsum);
+    }
 
 }
