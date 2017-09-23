dbcca882435c1987047327a2479fe16a073b5d40
Igor Frolov <adromaryn@gmail.com>
2017-09-23 18:58:43 +0300

    Fix max and sum methods

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -16,7 +16,13 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int res = values[0];
+        for (int num: values) {
+            if (num > res) {
+                res = num;
+            }
+        }
+        return res;
     }
 
     /**
@@ -26,7 +32,11 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        long sum = 0;
+        for (int num: values) {
+            sum += num;
+        }
+        return sum;
     }
 
 
