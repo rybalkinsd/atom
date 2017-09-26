a44c7735b8da6de8e73216c837f48c8a14cf10ac
DLB <dlb@MacBook-Pro-DLB.local>
2017-09-26 16:41:22 +0300

    first commit

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -27,7 +27,34 @@ public class Util {
      */
     public static long sum(int[] values) {
         throw new UnsupportedOperationException();
+	package ru.atom;
+
+public class Util {
+
+    
+    public static int max(int[] values) {
+        int max = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > values[max]) {
+                max = i;
+            }
+        }
+        return values[max];
     }
+
+    
+    public static long sum(int[] values) {
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        return sum;
+    }
+
+
+}
+   
+ }
 
 
 }
