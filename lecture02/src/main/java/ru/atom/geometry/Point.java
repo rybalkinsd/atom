e2ceb0e652ed037ff70c7ec3aab02e58ee53be27
Anonimous102 <gabdrahmanov.ilnar97@gmail.com>
2017-09-27 23:27:00 +0300

    practice completed

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,14 +3,16 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
+public class Point implements Collider/* super class and interfaces here if necessary */ {
     // fields
     // and methods
-
+    public int x;
+    public int y;
     /**
      * @param o - other object to check equality with
      * @return true if two points are equal and not null.
      */
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -18,8 +20,17 @@ public class Point /* super class and interfaces here if necessary */ {
 
         // cast from Object to Point
         Point point = (Point) o;
-
+        return x == point.x && y == point.y;
         // your code here
-        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        return this.equals(other);
+    }
+
+    public Point(int x,int y) {
+        this.x = x;
+        this.y = y;
     }
 }
