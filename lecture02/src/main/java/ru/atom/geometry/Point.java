da905440435accbf966b89f955fc6698ac31c164
najada12 <najada12@ya.ru>
2017-09-28 01:02:41 +0300

    Point and Bar

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,36 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
-    // fields
-    // and methods
+public class Point implements Collider {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        if (this.equals(other)) return true;
+        else {
+            if (!(other instanceof Bar)) return false;
+            Bar bar = (Bar) other;
+            Point first = bar.getFirstPoint();
+            Point second = bar.getSecondPoint();
+            if (first.x <= this.x && second.x >= this.x && first.y <= this.y && second.y >= this.y) return true;
+            else return false;
+        }
+    }
 
     /**
      * @param o - other object to check equality with
@@ -19,7 +46,9 @@ public class Point /* super class and interfaces here if necessary */ {
         // cast from Object to Point
         Point point = (Point) o;
 
-        // your code here
-        throw new UnsupportedOperationException();
+        if (this.x == point.x && this.y == point.y) return true;
+        else return false;
+
+
     }
 }
