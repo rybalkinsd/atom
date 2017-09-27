a3a30d03158ad159ac5c996fc9c38366f2d26058
Roman Vlasov <roma_vlasoff@mail.ru>
2017-09-27 23:48:38 +0300

    Add the implementation of Collider interface to Bar and Point classes, implement Geometry constructors for Bar and Point instances

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,14 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
-    // fields
-    // and methods
+public class Point implements Collider {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     /**
      * @param o - other object to check equality with
@@ -19,7 +24,25 @@ public class Point /* super class and interfaces here if necessary */ {
         // cast from Object to Point
         Point point = (Point) o;
 
-        // your code here
-        throw new UnsupportedOperationException();
+        return point.getX() == this.getX() && point.getY() == this.getY();
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        if (other instanceof Point) {
+            return equals(other);
+        } else if (other instanceof Bar) {
+            return Utils.barIsCollidingWithPoint((Bar) other, this);
+        }
+
+        return false;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
     }
 }
---
+++ lecture02/src/main/java/ru/atom/geometry/Utils.java
@@ -0,0 +1,10 @@
+package ru.atom.geometry;
+
+public final class Utils {
+    public static boolean barIsCollidingWithPoint(Bar bar, Point point) {
+        return bar.getFirstCornerX() <= point.getX() &&
+                bar.getFirstCornerY() <= point.getY() &&
+                bar.getSecondCornerX() >= point.getX() &&
+                bar.getSecondCornerY() >= point.getY();
+    }
+}
