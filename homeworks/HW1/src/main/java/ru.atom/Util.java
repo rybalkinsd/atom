e28141eb160576ced6a95297715ee60c1e66a142
Vladimir Erofeev <nolmegar@gmail.com>
2017-09-27 17:03:52 +0300

    asdfasdf

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -3,7 +3,6 @@ public class Util {
 
 
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
         int maximum = values[0];
         for (int i = 1; i < values.length; i++) {
             if (maximum < values[i]) maximum = values[i];
@@ -12,7 +11,6 @@ public class Util {
     }
 
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
         long summa = 0;
         for (int i : values) {
             summa += i;
