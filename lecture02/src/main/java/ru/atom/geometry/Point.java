4c3c6f3076eb666c1c9f99cdd3b631491e59dd4b
theSonOfMan <petr.galaburda@gmail.com>
2017-09-27 22:35:06 +0300

    Added Bar class, fixed Point class, implemented Collider in Bar and Point classes

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,19 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
+public class Point implements Collider/* super class and interfaces here if necessary */ {
     // fields
     // and methods
+
+    private int x = 0;
+    private int y = 0;
+
+    Point(){}
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     /**
      * @param o - other object to check equality with
@@ -13,13 +23,40 @@ public class Point /* super class and interfaces here if necessary */ {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) return true;
 
         // cast from Object to Point
         Point point = (Point) o;
+        return (this.x == point.x) && (this.x == point.y);
+    }
 
-        // your code here
-        throw new UnsupportedOperationException();
+    @Override
+    public boolean isColliding(Collider collider) {
+        if (collider instanceof Point)
+            return this.equals(collider);
+        else if (collider instanceof Bar)
+            return (this.x >= ((Bar) collider).getFirst().x)
+                    && (this.x <= ((Bar) collider).getSecond().x)
+                    && (this.y >= ((Bar) collider).getFirst().y)
+                    && (this.y <= ((Bar) collider).getSecond().y);
+        else
+            return false;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
