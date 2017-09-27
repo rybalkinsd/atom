dd6e22b420bdb8f70a921b49708039fa15a79e39
Sssssstudent <DEDAN3209@yandex.ru>
2017-09-27 20:59:32 +0400

    homework2

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,27 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
+public class Point implements Collider/* super class and interfaces here if necessary */ {
     // fields
     // and methods
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(Point that) {
+        x = that.x;
+        y = that.y;
+    }
+
 
     /**
      * @param o - other object to check equality with
@@ -13,13 +31,41 @@ public class Point /* super class and interfaces here if necessary */ {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         // cast from Object to Point
         Point point = (Point) o;
+        if (this.x == point.x && this.y == point.y) {
+            return true;
+        } else {
+            return false;
+        }
 
-        // your code here
-        throw new UnsupportedOperationException();
+    }
+
+    public boolean isColliding(Collider other) {
+        if (other.getClass() == Point.class) {
+            Point point = (Point)other;
+            if (point.x == x && point.y == y) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (other.getClass() == Bar.class) {
+            Bar bar = (Bar) other;
+            if (bar.isColliding(this)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
     }
 }
