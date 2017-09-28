bb62730b41b6b5a6ecd2e9f56da0461a3faec6e4
Ien Mayer <iceforcy@gmail.com>
2017-09-28 20:55:27 +0300

    'homework1'

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -1,33 +1,41 @@
-package ru.atom;
+package ru.atom; 
 
-/**
- * In this assignment you need to implement the following util methods.
- * Note:
- *  throw new UnsupportedOperationException(); - is just a stub
- */
-public class Util {
+/** 
+* In this assignment you need to implement the following util methods. 
+* Note: 
+* throw new UnsupportedOperationException(); - is just a stub 
+*/ 
+public class Util { 
 
+/** 
+* Returns the greatest of {@code int} values. 
+* 
+* @param values an argument. Assume values.length > 0. 
+* @return the largest of values. 
+*/ 
+public static int max(int[] values) { 
+int max = values[0]; 
+for (int i = 0; i < values.length; i++) { 
+if (values[i] > max) 
+max = values[i]; 
+} 
+return max; 
+} 
 
+/** 
+* Returns the sum of all {@code int} values. 
+* 
+* @param values an argument. Assume values.length > 0. 
+* @return the sum of all values. 
+*/ 
+public static long sum(int[] values) { 
 
-    /**
-     * Returns the greatest of {@code int} values.
-     *
-     * @param values an argument. Assume values.length > 0.
-     * @return the largest of values.
-     */
-    public static int max(int[] values) {
-        throw new UnsupportedOperationException();
-    }
+long sum = 0; 
+for (int i = 0; i < values.length; i++) 
+sum = sum + values[i]; 
 
-    /**
-     * Returns the sum of all {@code int} values.
-     *
-     * @param values an argument. Assume values.length > 0.
-     * @return the sum of all values.
-     */
-    public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
-    }
-
+//throw new UnsupportedOperationException(); 
+return sum; 
+} 
 
 }
