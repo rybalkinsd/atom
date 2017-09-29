21c8de7fa9b85d7b68ae9c0afdfcd9b53d1ecb7e
Vladimir Erofeev <nolmegar@gmail.com>
2017-09-29 22:48:51 +0300

    pls

--- lecture02/src/main/java/ru/atom/geometry/Bar.java
+++ lecture02/src/main/java/ru/atom/geometry/Bar.java
@@ -22,8 +22,8 @@ public class Bar implements Collider {
 
 
         Bar bar = (Bar) o;
-        if ((bar.x1 == x1 || bar.x1 == x2) && (bar.x2 == x2 || bar.x2 == x1) &&
-                (bar.y1 == y1 || bar.y1 == y2) && (bar.y2 == y1 || bar.y2 == y2)) return true;
+        if ((bar.x1 == x1 || bar.x1 == x2) && (bar.x2 == x2 || bar.x2 == x1)
+                && (bar.y1 == y1 || bar.y1 == y2) && (bar.y2 == y1 || bar.y2 == y2)) return true;
         else return false;
 
 
@@ -36,8 +36,8 @@ public class Bar implements Collider {
             if (x1 <= point.x && point.x <= x2 && y1 <= point.y && point.y <= y2) return true;
         } else {
             Bar bar = (Bar) other;
-            if (((x1 <= bar.x1 && bar.x1 <= x2) || (x1 <= bar.x2 && bar.x2 <= x2)) &&
-                    ((y1 <= bar.y1 && bar.y1 <= y2) || (y1 <= bar.y2 && bar.y2 <= y2))) return true;
+            if (((x1 <= bar.x1 && bar.x1 <= x2) || (x1 <= bar.x2 && bar.x2 <= x2))
+                    && ((y1 <= bar.y1 && bar.y1 <= y2) || (y1 <= bar.y2 && bar.y2 <= y2))) return true;
             if (bar.x1 < x1 && x2 < bar.x2 && bar.y1 < y1 && y2 < bar.y2) return true;
         }
 
