c1752520be041f389eebe359059688ae5aef5c73
Dmitrii Latyshev <LDDD5@yandex.ru>
2017-10-04 23:03:02 +0300

    implemented linked list

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,43 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    public ListNode() {
+        this.value = null;
+        this.next  = null;
+        this.prev  = null;
+    }
+
+    public ListNode(E value, ListNode<E> prev, ListNode<E> next) {
+        this.value = value;
+        this.next  = next;
+        this.prev  = prev;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public ListNode<E> next() {
+        return next;
+    }
+
+    public ListNode<E> prev() {
+        return prev;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public void setNext(ListNode<E> node) {
+        next = node;
+    }
+
+    public void setPrev(ListNode<E> node) {
+        prev = node;
+    }
+
+    private ListNode<E> next;
+    private ListNode<E> prev;
+    private E           value;
 }
