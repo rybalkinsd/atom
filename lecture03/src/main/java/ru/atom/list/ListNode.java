fb734b96daafcf03ae8e687c7c5486fe023c381e
IlyaDolinskiy <dolinsky94@yandex.ru>
2017-10-06 00:21:46 +0300

    lecture03

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -1,7 +1,22 @@
 package ru.atom.list;
 
+import java.util.LinkedList;
+
 /**
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+//    private E object;
+//    private ListNode<E> next;
+//    private ListNode<E> prev;
+
+    E item;
+    ListNode<E> next;
+    ListNode<E> prev;
+
+    ListNode(ListNode<E> prev, E element, ListNode<E> next) {
+        this.item = element;
+        this.next = next;
+        this.prev = prev;
+    }
 }
