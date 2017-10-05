ae237e62fcd6f2300237f7e95b7e437385d491fb
Vadim <vadsamokh@mail.ru>
2017-10-05 08:48:51 +0300

    Fixed functions

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,19 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    E object;
+    ListNode<E> next;
+    ListNode<E> prev;
+
+    public ListNode(E object) {
+        this.object = object;
+        this.next = this;
+        this.prev = this;
+    }
+
+    public ListNode(E object, ListNode<E> next, ListNode<E> prev) {
+        this.object = object;
+        this.next = next;
+        this.prev = prev;
+    }
 }
