d3bafe726f8d7ddf5fe1232d199842cc8121df96
ThisIsSad <greedthisissad@gmail.com>
2017-10-05 17:04:01 +0300

    lecture03 practise

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,8 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    public E object;
+    public ListNode<E> next;
+    public ListNode<E> prev;
+
 }
