e3b9407fe4319faeeb0b86fba979963551327803
Kroshkin Maxim <kroshkinmaxim@yandex.ru>
2017-10-06 02:40:56 +0300

    implemented ListNode class, absent methods in CustomLinkedList and iterator for CustomLinkedList

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,11 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    E object;
+    ListNode<E> next = null;
+    ListNode<E> prev = null;
+
+    ListNode(E object) {
+        this.object = object;
+    }
 }
