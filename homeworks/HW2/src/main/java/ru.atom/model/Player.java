26bacde0ef8ebd620de447c534f705e70da07eb0
alexwally <parfen.a.v@gmail.com>
2017-10-18 00:53:40 +0300

    Implemented interface Movable

--- homeworks/HW2/src/main/java/ru.atom/model/Player.java
+++ homeworks/HW2/src/main/java/ru.atom/model/Player.java
@@ -1,4 +1,53 @@
 package ru.atom.model;
 
-public class Player {
+import ru.atom.geometry.Point;
+
+public class Player extends AbstractGameObject implements Tickable, Movable {
+    private int speed = 1;
+
+    public Player(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public Point move(Direction direction, long time) {
+
+        int coordinateX = getPosition().getX();
+        int coordinateY = getPosition().getY();
+        long distance = time * getSpeed();
+
+        switch (direction) {
+            case UP:
+                coordinateY += distance;
+                break;
+            case DOWN:
+                coordinateY -= distance;
+                break;
+            case LEFT:
+                coordinateX -= distance;
+                break;
+            case RIGHT:
+                coordinateX += distance;
+                break;
+            case IDLE:
+                break;
+            default:
+                break;
+        }
+
+        return position = new Point(coordinateX, coordinateY);
+    }
+
+    @Override
+    public void tick(long elapsed) {
+
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 }
