8306e8b15fbc61b5e03b6eb48870bc3373853df4
Arif Ergashev <orif95@mail.ru>
2017-10-26 14:14:21 +0300

    I've done thread practise yet

--- lecture05/src/main/java/ru/atom/boot/mm/ConnectionController.java
+++ lecture05/src/main/java/ru/atom/boot/mm/ConnectionController.java
@@ -6,7 +6,10 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.atom.thread.mm.Connection;
 import ru.atom.thread.mm.ConnectionQueue;
 
@@ -19,8 +22,10 @@ public class ConnectionController {
 
     /**
      * curl test
-     *
-     * curl -i -X POST -H "Content-Type: application/x-www-form-urlencoded" localhost:8080/connection/connect -d 'id=1&name=bomberman'
+     * <p>
+     * curl -i -X POST -H "Content-Type: application/x-www-form-urlencoded"
+     * localhost:8080/connection/connect -d 'id=1&name=bomberman'
+     * </p>
      */
     @RequestMapping(
             path = "connect",
@@ -36,8 +41,9 @@ public class ConnectionController {
 
     /**
      * curl test
-     *
+     * <p>
      * curl -i localhost:8080/connection/list'
+     * </p>
      */
     public String list() {
         throw new UnsupportedOperationException();
