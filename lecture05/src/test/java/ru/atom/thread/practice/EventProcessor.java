9a1cfc632ee6c8eceb3eb2acfbd7f9fb49ff559a
azarnovdaniil <azarnow.d@gmail.com>
2017-10-27 23:01:52 +0300

    thread practice

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,21 +1,48 @@
 package ru.atom.thread.practice;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author apomosov
  * @since 15.03.17
  */
 public class EventProcessor {
+
+    private static long countGoodEvent = 0;
+    private static long countBadEvent = 0;
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new UnsupportedOperationException();
+        for(EventProducer eventProducer : eventProducers) {
+            Thread thread = new Thread(eventProducer);
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Event event = null;
+        while (true) {
+            event = EventQueue.getInstance().poll();
+            if(event == null) {
+                return;
+            }
+            if(event.getEventType().equals(Event.EventType.BAD)) {
+                countBadEvent++;
+            }
+            if(event.getEventType().equals(Event.EventType.GOOD)) {
+                countGoodEvent++;
+            }
+        }
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new UnsupportedOperationException();
+        return countGoodEvent;
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new UnsupportedOperationException();
+        return countBadEvent;
     }
 }
