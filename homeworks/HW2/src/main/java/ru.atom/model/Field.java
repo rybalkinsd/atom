a6d149fb3db2f7bc0e56afceeae3c5f17865b936
IlyaDolinskiy <dolinsky94@yandex.ru>
2017-10-27 21:19:03 +0300

    fix checkstyle

--- homeworks/HW2/src/main/java/ru.atom/model/Field.java
+++ homeworks/HW2/src/main/java/ru.atom/model/Field.java
@@ -1,7 +1,7 @@
 package ru.atom.model;
 
 public class Field extends GameObjectAbstract {
-    public Field(int lengthX, int lengthY){
+    public Field(int lengthX, int lengthY) {
         super(lengthX, lengthY);
     }
 }
