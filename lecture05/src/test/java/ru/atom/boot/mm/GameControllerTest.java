13de32b270870f02d8b902dda1fd7bc2a0082c2b
eranight <abelnigt@gmail.com>
2017-10-28 20:02:11 +0300

    GameController tests

--- lecture05/src/test/java/ru/atom/boot/mm/GameControllerTest.java
+++ lecture05/src/test/java/ru/atom/boot/mm/GameControllerTest.java
@@ -2,16 +2,27 @@ package ru.atom.boot.mm;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import ru.atom.thread.mm.GameRepository;
+import ru.atom.thread.mm.GameSession;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
 public class GameControllerTest {
 
     @Test
-    @Ignore
     public void list() throws Exception {
-        assertTrue(false);
+        GameController gameController = new GameController();
+
+        assertEquals("[]", gameController.list());
+
+        GameRepository.put(new GameSession(null));
+        GameRepository.put(new GameSession(null));
+        GameRepository.put(new GameSession(null));
+
+        assertThat(gameController.list()).isNotEmpty();
     }
 
 }
