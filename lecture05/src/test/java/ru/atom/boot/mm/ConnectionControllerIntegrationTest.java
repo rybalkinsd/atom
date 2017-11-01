378ea171743c5fa7a6502c6aa70417584c39fe64
najada12 <najada12@ya.ru>
2017-11-01 10:07:34 +0300

    ConnectionController and tests

--- lecture05/src/test/java/ru/atom/boot/mm/ConnectionControllerIntegrationTest.java
+++ lecture05/src/test/java/ru/atom/boot/mm/ConnectionControllerIntegrationTest.java
@@ -8,9 +8,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -22,15 +27,25 @@ public class ConnectionControllerIntegrationTest {
     @Test
     public void connect() throws Exception {
         mockMvc.perform(post("/connection/connect")
-                    .content("id=1&name=a")
-                    .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .content("id=1&name=a")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @Ignore
     public void list() throws Exception {
-        assertTrue(false);
+        mockMvc.perform(post("/connection/connect")
+                .content("id=1&name=a")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED));
+        mockMvc.perform(post("/connection/connect")
+                .content("id=2&name=b")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED));
+        mockMvc.perform(post("/connection/connect")
+                .content("id=3&name=c")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED));
+        mockMvc.perform(get("/connection/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[{\"playerId\":1,\"name\":\"a\"},{\"playerId\":2,\"name\":\"b\"},{\"playerId\":3,\"name\":\"c\"}]"));
     }
 
 }
--- lecture05/src/test/java/ru/atom/boot/mm/ConnectionControllerTest.java
+++ lecture05/src/test/java/ru/atom/boot/mm/ConnectionControllerTest.java
@@ -2,17 +2,19 @@ package ru.atom.boot.mm;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import ru.atom.thread.mm.ConnectionQueue;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ConnectionControllerTest {
 
     @Test
-    @Ignore
     public void connect() throws Exception {
         ConnectionController connectionHandler = new ConnectionController();
-        assertThat(connectionHandler.list()).isEmpty();
+        ConnectionQueue.getInstance().clear();
+        assertEquals(connectionHandler.list(), "[]");
 
         connectionHandler.connect(1, "a");
         connectionHandler.connect(2, "b");
@@ -22,9 +24,12 @@ public class ConnectionControllerTest {
     }
 
     @Test
-    @Ignore
     public void list() throws Exception {
-        assertTrue(false);
+        ConnectionController connectionHandler = new ConnectionController();
+        connectionHandler.connect(1, "a");
+        connectionHandler.connect(2, "b");
+        connectionHandler.connect(3, "c");
+        assertEquals("[{\"playerId\":1,\"name\":\"a\"},{\"playerId\":2,\"name\":\"b\"},{\"playerId\":3,\"name\":\"c\"}]", new ConnectionController().list());
     }
 
 }
