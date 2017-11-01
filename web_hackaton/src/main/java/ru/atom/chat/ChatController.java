27a5b6a7f6d7354d7e38d129e0309182f54910b9
alexwally <parfen.a.v@gmail.com>
2017-11-02 00:05:06 +0300

    Implemented saving history to file on server, chat/say, chat/online, chat/logout, timestamps and different colors for name and timestamp

--- web_hackaton/src/main/java/ru/atom/chat/ChatController.java
+++ web_hackaton/src/main/java/ru/atom/chat/ChatController.java
@@ -1,7 +1,9 @@
 package ru.atom.chat;
 
+import ch.qos.logback.core.util.TimeUtil;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.tomcat.jni.Time;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.Queue;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -27,9 +34,14 @@ public class ChatController {
     private Deque<String> messages = new ConcurrentLinkedDeque<>();
     private Set<String> online = new HashSet<>();
 
+    private List<String> history = new ArrayList<>(100);
+
+    //+ different colors for name and timestamp
+
     /**
      * curl -X POST -i localhost:8080/chat/login -d "name=I_AM_STUPID"
      */
+    //+ timestamp
     @RequestMapping(
             path = "login",
             method = RequestMethod.POST,
@@ -42,51 +54,82 @@ public class ChatController {
         if (!online.add(name)) {
             return new ResponseEntity<>("Already logged in", HttpStatus.BAD_REQUEST);
         }
-        messages.addFirst("[" + name + "] is online");
+        Date ts = new Date();
+        SimpleDateFormat dateNow = new SimpleDateFormat("E dd.MM.yyyy hh:mm");
+        messages.addFirst("<text> <font color=#008000>" + dateNow.format(ts) + " " +
+                        "</font> <font color=#FF8C00>" + "[" + name + "]" +
+                        "</font> </text>" + "is online");
         log.info(name + " logged in");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     /**
-     * curl -X POST -i localhost:8080/chat/logout?name=I_AM_STUPID"
+     * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
      */
+    //++ + timestamp
     @RequestMapping(
             path = "logout",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity logout(@RequestParam("name") String name) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (name == null || name.isEmpty()) {
+            return new ResponseEntity<>("No name provided", HttpStatus.BAD_REQUEST);
+        }
+        if (!online.remove(name))
+            return new ResponseEntity<>(name + "are not logged in", HttpStatus.BAD_REQUEST);
+        Date ts = new Date();
+        SimpleDateFormat dateNow = new SimpleDateFormat("E dd.MM.yyyy hh:mm");
+        messages.addFirst("<text> <font color=#008000>" + dateNow.format(ts) + " " +
+                        "</font> <font color=#FF8C00>" + "[" + name + "]" +
+                        "</font> </text>" + "is offline");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
     /**
      * curl -i localhost:8080/chat/online
      */
+    //++
     @RequestMapping(
             path = "online",
             method = RequestMethod.GET,
             produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity online() {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> online() {
+        return new ResponseEntity<>(online.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("\n")),
+                HttpStatus.OK);
     }
 
 
     /**
      * curl -X POST -i localhost:8080/chat/say -d "name=I_AM_STUPID&msg=Hello everyone in this chat"
      */
+    //++ + timestamp + save history to file
     @RequestMapping(
             path = "say",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
+        if (!online.contains(name)) {
+            return new ResponseEntity<>("You are not logged in", HttpStatus.BAD_REQUEST);
+        }
+        if (msg == null || msg.isEmpty())
+            return new ResponseEntity<>("No message", HttpStatus.BAD_REQUEST);
+        Date ts = new Date();
+        SimpleDateFormat dateNow = new SimpleDateFormat("E dd.MM.yyyy hh:mm");
+        messages.addFirst("<text> <font color=#008000>" + dateNow.format(ts) + " " +
+                "</font> <font color=#FF8C00>" + "[" + name + "]" +
+                "</font> </text>" + ": " + msg);
+        addToHistory(messages.getFirst());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
     /**
-     * curl -i localhost:8080/hello/chat
+     * curl -i localhost:8080/chat/chat
      */
     @RequestMapping(
             path = "chat",
@@ -97,5 +140,21 @@ public class ChatController {
                 .map(Object::toString)
                 .collect(Collectors.joining("\n")),
                 HttpStatus.OK);
+    }
+
+    public void addToHistory(String msg) {
+        if (history.size() > 100) {
+            history.remove(0);
+        }
+        history.add(msg);
+        String path = "/home/alex/atom/web_hackaton/src/main/resources/History/history.txt";
+        try (FileWriter writer = new FileWriter(path,false)) {
+            String text = history.get(history.size() - 1);
+            writer.write(text);
+            writer.flush();
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
