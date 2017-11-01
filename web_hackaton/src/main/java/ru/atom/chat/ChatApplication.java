2182673b1cc7946a9fad6fb0b191298ed4355cf6
alexwally <parfen.a.v@gmail.com>
2017-11-02 00:00:29 +0300

    Not useful changes

--- web_hackaton/src/main/java/ru/atom/chat/ChatApplication.java
+++ web_hackaton/src/main/java/ru/atom/chat/ChatApplication.java
@@ -1,11 +1,30 @@
 package ru.atom.chat;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+//добавить восстановление из истории?
 @SpringBootApplication
 public class ChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
+    }
+
+    @Bean
+    public BeanPostProcessor loadHistory() {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+                return bean;
+            }
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                return bean;
+            }
+        };
     }
 }
