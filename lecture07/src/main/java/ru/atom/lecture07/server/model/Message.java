11e1cc23d36f7dcd964205e42f4b9c2f6a5284b3
eranight <abelnigt@gmail.com>
2017-11-12 00:00:49 +0300

    Fixes for checkstyle

--- lecture07/src/main/java/ru/atom/lecture07/server/model/Message.java
+++ lecture07/src/main/java/ru/atom/lecture07/server/model/Message.java
@@ -1,17 +1,25 @@
 package ru.atom.lecture07.server.model;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
-@Table(name="message", schema = "chat")
+@Table(name = "message", schema = "chat")
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Column(name = "time", unique = false, nullable = false)
--- lecture07/src/main/java/ru/atom/lecture07/server/model/User.java
+++ lecture07/src/main/java/ru/atom/lecture07/server/model/User.java
@@ -1,6 +1,12 @@
 package ru.atom.lecture07.server.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 @Entity
