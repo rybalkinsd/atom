5d5ffd22b5c831d40783f53453d02b9b7be8e82d
alexwally <parfen.a.v@gmail.com>
2017-11-15 15:07:22 +0300

    Implemented chat/logout, chat/say and chat/chat

--- lecture07/src/main/java/ru/atom/lecture07/server/service/ChatService.java
+++ lecture07/src/main/java/ru/atom/lecture07/server/service/ChatService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import ru.atom.lecture07.server.controller.ChatController;
 import ru.atom.lecture07.server.dao.MessageDao;
 import ru.atom.lecture07.server.dao.UserDao;
+import ru.atom.lecture07.server.model.Message;
 import ru.atom.lecture07.server.model.User;
 
 import javax.transaction.Transactional;
@@ -20,6 +21,7 @@ public class ChatService {
 
     @Autowired
     private UserDao userDao;
+    @Autowired
     private MessageDao messageDao;
 
     @Nullable
@@ -35,9 +37,30 @@ public class ChatService {
         log.info("[" + login + "] logged in");
     }
 
+    @Transactional
+    public void logout(@NotNull User user) {
+        userDao.delete(user);
+        log.info("[" + user.getLogin() + "] logged out");
+    }
+
     @NotNull
     @Transactional
     public List<User> getOnlineUsers() {
         return Lists.newArrayList(userDao.findAll());
+    }
+
+    @Transactional
+    public void addMessage(@NotNull User user, @NotNull String msg) {
+        Message message = new Message();
+        message.setUser(user);
+        message.setValue(msg);
+        messageDao.save(message);
+        log.info("[" + user.getLogin() + "] say " + "[" + msg + "]");
+    }
+
+    @NotNull
+    @Transactional
+    public List<Message> getMessages() {
+        return Lists.newArrayList(messageDao.findAll());
     }
 }
