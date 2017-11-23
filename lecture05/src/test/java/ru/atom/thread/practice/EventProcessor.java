013a19283fe86cba792c5cf7247388bc7d77516b
Aleksandr Nikolaychik <aleks.nik.w@gmail.com>
2017-11-23 20:53:41 +0300

    lecture05 practice done, implemented only event processor

--- lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
+++ lecture05/src/test/java/ru/atom/thread/practice/EventProcessor.java
@@ -1,21 +1,36 @@
 package ru.atom.thread.practice;
 
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * @author apomosov
  * @since 15.03.17
  */
 public class EventProcessor {
+
+    private static BlockingQueue<Event> queue = EventQueue.getInstance();
+
     public static void produceEvents(List<EventProducer> eventProducers) {
-        throw new UnsupportedOperationException();
+        for (EventProducer eventProducer : eventProducers)
+            eventProducer.run();
     }
 
     public static long countTotalNumberOfGoodEvents() {
-        throw new UnsupportedOperationException();
+        long countGood = 0L;
+        for (Event event : queue) {
+            if (event.getEventType() == Event.EventType.GOOD)
+                countGood++;
+        }
+        return countGood;
     }
 
     public static long countTotalNumberOfBadEvents() {
-        throw new UnsupportedOperationException();
+        long countBad = 0L;
+        for (Event event : queue) {
+            if (event.getEventType() == Event.EventType.BAD)
+                countBad++;
+        }
+        return countBad;
     }
 }
