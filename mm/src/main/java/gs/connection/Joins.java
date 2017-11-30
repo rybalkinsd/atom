06ebab3d3fcb7a6fb73587ecf2e549b7be8da23c
Russiancold <elykovalex@gmail.com>
2017-11-30 23:22:58 +0700

    minor fixes

--- mm/src/main/java/gs/connection/Joins.java
+++ mm/src/main/java/gs/connection/Joins.java
@@ -2,7 +2,7 @@ package gs.connection;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ThreadSafeStorage {
+public class Joins {
     private static ConcurrentHashMap<String, Long> joins = new ConcurrentHashMap<String, Long>();
 
     public static ConcurrentHashMap<String, Long> getInstance() {
