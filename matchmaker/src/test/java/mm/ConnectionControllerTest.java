d012c2b3bc264e09814ec8c269b2092fd585a38b
najada12 <najada12@ya.ru>
2017-12-02 03:10:39 +0300

    tests and checkstyle

---
+++ matchmaker/src/test/java/mm/ConnectionControllerTest.java
@@ -0,0 +1,31 @@
+package mm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConnectionControllerTest {
+    @Autowired
+    private ConnectionController connectionController;
+
+    @MockBean
+    private Matchmaker matchmaker;
+
+    @Test
+    public void join() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        ResponseEntity<String> responseEntity = new ResponseEntity<String>("101", headers, HttpStatus.OK);
+        assertEquals(responseEntity, connectionController.join("Alice"));
+    }
+}
--- mmgs/src/main/java/gs/GameSession.java
+++ mmgs/src/main/java/gs/GameSession.java
@@ -3,7 +3,6 @@ package gs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
