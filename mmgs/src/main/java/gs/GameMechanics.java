8a77840150e50a7f153de1cddecd2f1a9f168480
bibliophagist <greedthisissad@gmail.com>
2017-12-05 14:50:25 +0300

    ReplicaIGuess

---
+++ mmgs/src/main/java/gs/GameMechanics.java
@@ -0,0 +1,27 @@
+package gs;
+
+import gs.tick.Tickable;
+
+public class GameMechanics implements Tickable {
+
+    public void read(){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public void doMechanic(){
+
+    }
+
+    public void writeReplica(){
+
+    }
+
+    @Override
+    public void tick(long elapsed){
+
+    }
+}
