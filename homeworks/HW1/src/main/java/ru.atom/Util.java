15335f3b2b4ec27af837c265bfda61f97ffe11da
deniskokosss <deniskokosss@mail.ru>
2018-02-22 20:49:17 +0300

    Denis Kokosinskiy

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -16,7 +16,12 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int _max=0;
+        for (int i : values) {
+	    if (i > _max)
+		_max = i;
+	}
+	return _max;
     }
 
     /**
@@ -26,7 +31,10 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        int _sum=0;
+	for(int i : values){
+		_sum += i;
+	return _sum;
     }
 
 
