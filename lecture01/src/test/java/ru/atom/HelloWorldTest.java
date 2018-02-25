5aacd709c1f389d1f715ee4e0dbe029d2b289857
a.pomosov <alpieex@gmail.com>
2018-02-25 19:42:31 +0300

    lecture01 new gradle structure

--- lecture01/src/test/java/ru/atom/HelloWorldTest.java
+++ lecture01/src/test/java/ru/atom/HelloWorldTest.java
@@ -2,11 +2,13 @@ package ru.atom;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class HelloWorldTest {
     @Test
-    public void getHelloWorld() {
-        assertEquals("WRONG STRING", HelloWorld.getHelloWorld());
+    public void getHelloWorld() throws Exception {
+        assertEquals("Hello, World!", HelloWorld.getHelloWorld());
+        assertFalse("Some strange string".equals(HelloWorld.getHelloWorld()));
     }
 }
