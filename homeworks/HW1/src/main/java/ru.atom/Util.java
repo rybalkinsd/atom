a5c8d11df6c0cad5e80874f06da71d9a01374672
ololowl <36707886+ololowl@users.noreply.github.com>
2018-02-28 21:00:01 +0300

    morgunova olga

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -34,7 +34,7 @@ public class Util {
      */
     public static long sum(int[] values) {
         int len = values.length;
-        int sum = 0;
+        long sum = 0;
         for (int i = 0; i < len; ++i) {
             sum += values[i];
         }
