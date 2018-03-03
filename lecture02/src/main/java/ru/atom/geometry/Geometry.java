103b894cdecf2582e8afa4fa61f6bfd409d2ae54
Andrew Chupakhin <atom1543@yandex.ru>
2018-03-04 00:09:58 +0300

    Added class Bar, Point, Geometry.

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -21,8 +21,10 @@ public final class Geometry {
      * (It is not relevant, which opposite corners you choose to define bar)
      * @return new Bar
      */
-    public static Collider createBar(int firstCornerX, int firstCornerY, int secondCornerX, int secondCornerY) {
-        throw new UnsupportedOperationException();
+    public static Collider createBar(int firstCornerX, int firstCornerY,
+                                     int secondCornerX, int secondCornerY) {
+        return new Bar(firstCornerX, firstCornerY,
+                       secondCornerX, secondCornerY);
     }
 
     /**
@@ -30,6 +32,6 @@ public final class Geometry {
      * @return new Point
      */
     public static Collider createPoint(int x, int y) {
-        throw new UnsupportedOperationException();
+        return new Point(x, y);
     }
 }
