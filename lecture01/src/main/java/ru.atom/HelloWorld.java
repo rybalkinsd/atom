e0c28eda318b9dd826c0a9ff254b55ed0156bed5
Raschudesny <rifkinsm@gmail.com>
2018-03-04 05:31:58 +0300

    Homework2? okay

--- lecture01/src/main/java/ru.atom/HelloWorld.java
+++ lecture01/src/main/java/ru.atom/HelloWorld.java
@@ -3,6 +3,7 @@ package ru.atom;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(getHelloWorld());
+        System.out.println(null == null);
     }
 
     public static String getHelloWorld() {
