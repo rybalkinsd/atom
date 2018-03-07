af3011931492feb222aee7a9dfb876bcd472a6bd
atash0804 <atash0804@yandex.ru>
2018-03-07 20:58:18 +0300

    Artem Tashevtsev 3 lecture
    Done 3 lecture task

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -1,7 +1,12 @@
 package ru.atom.list;
 
+import java.awt.*;
+
 /**
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    ListNode<E> next;
+    ListNode<E> prev;
+    E elem;
 }
