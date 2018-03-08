2ad519d474bd876b61a76c1ddd21ddbb24a0872b
Dmitry Belamov <belamov44@gmail.com>
2018-03-08 11:05:27 +0500

    fixed sum type

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -31,7 +31,7 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        int sum = 0;
+        long sum = 0;
         for (int item : values) {
             sum += item;
         }
