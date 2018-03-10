27df1947512460588d92e31204be9a159b896b4b
DeviantBadge <zhenya_vorobyov@mail.ru>
2018-03-10 08:08:04 -0800

    Lecture 3 classWork

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -1,7 +1,41 @@
 package ru.atom.list;
 
+import java.util.List;
+import java.util.RandomAccess;
+
 /**
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    private ListNode<E> next = null;
+    private ListNode<E> prev = null;
+    E value;
+
+    ListNode(E value)  {
+        this.value = value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public void setNext(ListNode<E> next) {
+        this.next = next;
+    }
+
+    public void setPrev(ListNode<E> prev) {
+        this.prev = prev;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public ListNode<E> getNext() {
+        return next;
+    }
+
+    public ListNode<E> getPrev() {
+        return prev;
+    }
 }
