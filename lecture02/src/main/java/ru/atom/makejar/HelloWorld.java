f428404d99037bc0b1a7ec608145c348b54dc3a9
artelhin <artelhin@gmail.com>
2018-03-10 18:34:38 +0300

    dont pay attention

--- lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
+++ lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
@@ -5,7 +5,8 @@ package ru.atom.makejar;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(getHelloWorld());
+        //*System.out.println(getHelloWorld());*/
+        System.out.println(null == null);
     }
 
     public static String getHelloWorld() {
