502b7fc4affb18e8ee9f241a6651ccfd3404360c
DeviantBadge <zhenya_vorobyov@mail.ru>
2018-03-11 03:03:37 -0700

    New Clear Lecture02 task

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,19 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
-    // fields
+public class Point /* super class and interfaces here if necessary */ implements Collider {
+    private int x;
+    private int y;
     // and methods
+
+    public Point() {
+        x = y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     /**
      * @param o - other object to check equality with
@@ -14,12 +24,39 @@ public class Point /* super class and interfaces here if necessary */ {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Point)) return false;
 
         // cast from Object to Point
         Point point = (Point) o;
 
         // your code here
-        throw new UnsupportedOperationException();
+        return x == point.getX() && y == point.getY();
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        if (other instanceof Bar) {
+            return ((Bar) other).isColliding(this);
+        }
+        if (other instanceof Point) {
+            return equals(other);
+        }
+        return false;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
