81607af5bb41c57a50a24c12344f21d490106d60
nikolay <nikolay@debian>
2018-03-12 03:06:33 +0300

    do LinkedList

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,36 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+
+    private final E tmp;
+
+    private ListNode<E> next;
+    private ListNode<E> prev;
+
+    public E getTmp() {
+        return tmp;
+    }
+
+    public ListNode<E> getNext() {
+        return next;
+    }
+
+    public ListNode<E> getPrev() {
+        return prev;
+    }
+
+    public void setNext(ListNode<E> next) {
+        this.next = next;
+    }
+
+    public void setPrev(ListNode<E> prev) {
+        this.prev = prev;
+    }
+
+    public ListNode(E tmp) {
+
+        this.tmp = tmp;
+        next = null;
+        prev = null;
+    }
 }
