8d9d47f4fd45482487c74e1d4eedf2cb7d302a2f
akayukova <33858712+akayukova@users.noreply.github.com>
2018-03-14 13:22:29 +0300

    Restart is added, build gradle modified to create jar

--- homeworks/HW2/src/main/java/ru/atom/bullsncows/BullsAndCows.java
+++ homeworks/HW2/src/main/java/ru/atom/bullsncows/BullsAndCows.java
@@ -1,8 +1,6 @@
 package ru.atom.bullsncows;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class BullsAndCows {
@@ -11,12 +9,10 @@ public class BullsAndCows {
     private static char[] letters;
 
     private static boolean isCorrect(char[] answer) {
-        if (answer == null)
-            return false;
-        return Arrays.equals(answer, word);
+        return answer != null && Arrays.equals(answer, word);
     }
 
-    static char[] diffLetters() {
+    private static char[] diffLetters() {
         char[] letters = new char[word.length];
         int length = 0;
         String strWord = new String(word);
@@ -30,7 +26,7 @@ public class BullsAndCows {
     }
 
 
-    static int numberOfCows(char[] answer) {
+    private static int numberOfCows(char[] answer) {
         int cows = 0;
         for (int i = 0; i < letters.length; i++) {
             for (int j = 0; j < word.length; j++) {
@@ -44,7 +40,7 @@ public class BullsAndCows {
         return cows - numberOfBulls(answer);
     }
 
-    static int numberOfBulls(char[] answer) {
+    private static int numberOfBulls(char[] answer) {
         int bulls = 0;
         for (int i = 0; i < answer.length; i++) {
             if (answer[i] == word[i]) {
@@ -54,7 +50,7 @@ public class BullsAndCows {
         return bulls;
     }
 
-    static void loadNewWord() {
+    private static void loadNewWord() {
         word = WordFinder.getWord();
         letters = diffLetters();
     }
@@ -64,48 +60,55 @@ public class BullsAndCows {
         Scanner input = new Scanner(System.in);
         char[] answer;
         String strAnswer;
+        boolean restart = true;
 
         System.out.println("Hello! This is the Bulls & Cows game!");
         loadNewWord();
-        System.out.println("the secret word contains " + word.length + " letters ");
+        while (restart) {
+            System.out.println("the secret word contains " + word.length + " letters ");
 
-        // first step without printing of bulls and cows
-        System.out.println("Make a guess: ");
-        strAnswer = input.next().toLowerCase();
-        //user data validating
-        while (strAnswer.length() != word.length || !strAnswer.matches("[a-z]*")) {
-            System.out.println("the word mast be " + word.length +
-                    " letters length and contain only letters of the Latin alphabet");
-            System.out.println("Please, try again: ");
-            strAnswer = input.next().toLowerCase();
-        }
-        answer = strAnswer.toCharArray();
-        int attempt = 1;
-
-        //other steps
-        while (attempt < 10 && !isCorrect(answer)) {
-            System.out.println(answer);
-            System.out.println(word);
-            System.out.println(letters);
-            System.out.println("cows: " + numberOfCows(answer) + "  bulls: " + numberOfBulls(answer));
+            // first step without printing of bulls and cows
             System.out.println("Make a guess: ");
             strAnswer = input.next().toLowerCase();
+
+            //user data validating
             while (strAnswer.length() != word.length || !strAnswer.matches("[a-z]*")) {
                 System.out.println("the word mast be " + word.length +
                         " letters length and contain only letters of the Latin alphabet");
                 System.out.println("Please, try again: ");
                 strAnswer = input.next().toLowerCase();
             }
+
             answer = strAnswer.toCharArray();
-            attempt++;
-        }
-        if (isCorrect(answer)) {
-            System.out.print("You win! The correct word is ");
-            System.out.println(word);
-        } else {
-            System.out.println("You lose :(");
+            int attempt = 1;
+
+            //other steps
+            while (attempt < 10 && !isCorrect(answer)) {
+                System.out.println(answer);
+                System.out.println("cows: " + numberOfCows(answer) + "  bulls: " + numberOfBulls(answer));
+                System.out.println("Make a guess: ");
+                strAnswer = input.next().toLowerCase();
+
+                while (strAnswer.length() != word.length || !strAnswer.matches("[a-z]*")) {
+                    System.out.println("the word mast be " + word.length +
+                            " letters length and contain only letters of the Latin alphabet");
+                    System.out.println("Please, try again: ");
+                    strAnswer = input.next().toLowerCase();
+                }
+
+                answer = strAnswer.toCharArray();
+                attempt++;
+            }
+
+            if (isCorrect(answer)) {
+                System.out.print("You win! The correct word is ");
+                System.out.println(word);
+            } else {
+                System.out.println("You lose :(");
+            }
+
+            System.out.println("Once again? (type yes) To exit type any character");
+            restart = input.next().toLowerCase().equals("yes");
         }
     }
-
-
 }
