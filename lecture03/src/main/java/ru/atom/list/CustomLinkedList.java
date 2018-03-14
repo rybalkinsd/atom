7b23a511dfaff86832f7f542f16dc017ee0b039f
Andrew Chupakhin <atom1543@yandex.ru>
2018-03-14 15:36:34 +0300

    Added some methods to CustomLinkedListTest.

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -1,63 +1,149 @@
 package ru.atom.list;
 
+
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 
 public class CustomLinkedList<E> implements List<E> {
+    private ListNode<E> header;
+
+    public CustomLinkedList() {
+        header = new ListNode<E>();
+    }
+
+    class CustomLinkedListIterator implements Iterator<E> {
+        ListNode<E> curr;
+
+        CustomLinkedListIterator() {
+            curr = CustomLinkedList.this.header;
+        }
+
+        public boolean hasNext() {
+            return (curr.getNext() != CustomLinkedList.this.header
+                    && curr.getNext() != null) ? true : false;
+        }
+
+        public E next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            curr = curr.getNext();
+            return curr.getData();
+        }
+
+    }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        int result = 0;
+        ListNode<E> curr = header.getNext();
+        while (curr != header) {
+            ++result;
+            curr = curr.getNext();
+        }
+        return result;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return header == header.getNext();
     }
 
     @Override
     public boolean contains(Object o) {
-        throw new UnsupportedOperationException();
+        E element = (E) o;
+        for (E object : this) {
+            if (object == element)
+                return true;
+        }
+        return false;
     }
 
     @Override
     public Iterator<E> iterator() {
-        throw new UnsupportedOperationException();
+        return new CustomLinkedListIterator();
     }
 
     @Override
     public boolean add(E e) {
-        throw new UnsupportedOperationException();
+        ListNode<E> lastNode = header.getPrev();
+        ListNode<E> node = new ListNode();
+        lastNode.setNext(node);
+        node.setData(e);
+        node.setPrev(lastNode);
+        node.setNext(header);
+        header.setPrev(node);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
+        E element = (E) o;
+        ListNode<E> curr = header.getNext();
+        while (curr != header) {
+            if (curr.getData().equals(element)) {
+                ListNode<E> prev = curr.getPrev();
+                ListNode<E> next = curr.getNext();
+                // delete all links to current ListNode
+                prev.setNext(next);
+                next.setPrev(prev);
+                curr.setNext(null);
+                curr.setPrev(null);
+                curr = null;
+                return true;
+            }
+            curr = curr.getNext();
+        }
+        return false;
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        header.setPrev(header);
+        header.setNext(header);
+        // All ListNodes will be deleted by GC
     }
 
     @Override
     public E get(int index) {
-        throw new UnsupportedOperationException();
+        if (index < 0 || index > (this.size() - 1))
+            throw new IndexOutOfBoundsException();
+        int currIndex = 0;
+        E result = null;
+        for (E element : this) {
+            if (currIndex == index) {
+                result = element;
+                break;
+            }
+            ++currIndex;
+        }
+        return result;
     }
 
     @Override
     public int indexOf(Object o) {
-        throw new UnsupportedOperationException();
+        E element = (E) o;
+        int index = 0;
+        for (E object : this) {
+            if (object.equals(element))
+                return index;
+            ++index;
+        }
+        return -1;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException();
+        for (E object : c) {
+            if (!this.add(object))
+                return false;
+        }
+        return true;
     }
-
 
     /*
       !!! Implement methods below Only if you know what you are doing !!!
@@ -65,8 +151,8 @@ public class CustomLinkedList<E> implements List<E> {
     @Override
     public boolean containsAll(Collection<?> c) {
         for (Object o : c) {
-            if (!contains(o)) {
-                return true;
+            if (!this.contains(o)) {
+                return false;
             }
         }
         return true;
