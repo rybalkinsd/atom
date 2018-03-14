6e698a582234918cb7f02f9f31fded7ad994aa66
Mikhail Mutafyan <mishamut@rambler.ru>
2018-03-14 21:04:38 +0300

    Lecture 4: HTTP and Spring practice

--- lecture04/src/main/java/ru/atom/chat/server/ChatController.java
+++ lecture04/src/main/java/ru/atom/chat/server/ChatController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
@@ -31,13 +32,13 @@ public class ChatController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> login(@RequestParam("name") String name) {
         if (name.length() < 1) {
-            return ResponseEntity.badRequest().body("Too short name, sorry :(");
+            return ResponseEntity.badRequest().body("The name is too short, sorry\n");
         }
         if (name.length() > 20) {
-            return ResponseEntity.badRequest().body("Too long name, sorry :(");
+            return ResponseEntity.badRequest().body("The name is too long, sorry\n");
         }
         if (usersOnline.containsKey(name)) {
-            return ResponseEntity.badRequest().body("Already logged in:(");
+            return ResponseEntity.badRequest().body("Already logged in");
         }
         usersOnline.put(name, name);
         messages.add("[" + name + "] logged in");
@@ -59,16 +60,52 @@ public class ChatController {
     /**
      * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
      */
-    //TODO
+    @RequestMapping(
+            path="logout",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> logout(@RequestParam("name") String name) {
+        if (usersOnline.containsKey(name)) {
+            usersOnline.remove(name);
+            messages.add("[" + name + "] logged out");
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().body("This user is not logged in\n");
+    }
 
     /**
      * curl -X POST -i localhost:8080/chat/say -d "name=I_AM_STUPID&msg=Hello everyone in this chat"
      */
-    //TODO
+    @RequestMapping(
+            path="say",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("This user is not logged in\n");
+        }
+        if (msg.length() < 1) {
+            return ResponseEntity.badRequest().body("The message is too short\n");
+        }
+        if (msg.length() > 50) {
+            return ResponseEntity.badRequest().body("The message is too long\n");
+        }
+        messages.add("[" + name + "] " + msg);
+        return ResponseEntity.ok().build();
+    }
 
 
     /**
      * curl -i localhost:8080/chat/chat
      */
-    //TODO
+    @RequestMapping(
+            path = "chat",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity chat() {
+        String responseBody = String.join("\n", new ArrayList<>(messages));
+        return ResponseEntity.ok(responseBody);
+    }
 }
