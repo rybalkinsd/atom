0dde1f2d655f388c983b18adf4d9006fc756370a
Andrew Chupakhin <atom1543@yandex.ru>
2018-03-15 16:46:57 +0300

    Added extras methods to CustomLinkedList.

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -2,10 +2,11 @@ package ru.atom.list;
 
 
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Iterator;
 import java.util.ListIterator;
+import java.lang.reflect.Array;
 import java.util.NoSuchElementException;
 
 
@@ -16,10 +17,10 @@ public class CustomLinkedList<E> implements List<E> {
         header = new ListNode<E>();
     }
 
-    class CustomLinkedListIterator implements Iterator<E> {
+    class CustomIterator implements Iterator<E> {
         ListNode<E> curr;
 
-        CustomLinkedListIterator() {
+        CustomIterator() {
             curr = CustomLinkedList.this.header;
         }
 
@@ -35,6 +36,80 @@ public class CustomLinkedList<E> implements List<E> {
             return curr.getData();
         }
 
+    }
+
+    class CustomListIterator implements ListIterator<E> {
+        int currIndex;
+        ListNode<E> curr;
+
+        CustomListIterator() {
+            currIndex = -1;
+            curr = CustomLinkedList.this.header;
+        }
+
+        public boolean hasNext() {
+            return (curr.getNext() != CustomLinkedList.this.header
+                    && curr.getNext() != null) ? true : false;
+        }
+
+        public E next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            curr = curr.getNext();
+            ++currIndex;
+            return curr.getData();
+        }
+
+        public boolean hasPrevious() {
+            return (curr.getPrev() != CustomLinkedList.this.header
+                    && curr.getPrev() != null) ? true : false;
+        }
+
+        public E previous() {
+            if (!hasPrevious())
+                throw new NoSuchElementException();
+            curr = curr.getPrev();
+            --currIndex;
+            return curr.getData();
+        }
+
+        public int nextIndex() {
+            if (!hasNext())
+                return size();
+            return currIndex + 1;
+        }
+
+        public int previousIndex() {
+            if (!hasPrevious())
+                return -1;
+            return currIndex - 1;
+        }
+
+        public void add(E element) {
+            ListNode<E> prev = curr.getPrev();
+            ListNode<E> newNode = new ListNode<E>(element, curr, prev);
+            prev.setNext(newNode);
+            curr.setPrev(newNode);
+            ++currIndex;
+        }
+
+        public void set(E element) {
+            if (curr == CustomLinkedList.this.header)
+                throw new UnsupportedOperationException();
+            curr.setData(element);
+        }
+
+        public void remove() {
+            if (curr == CustomLinkedList.this.header)
+                throw new UnsupportedOperationException();
+            ListNode<E> prev = curr.getPrev();
+            ListNode<E> next = curr.getNext();
+            prev.setNext(next);
+            next.setPrev(prev);
+            curr.setPrev(null);
+            curr.setNext(null);
+            curr = prev;
+        }
     }
 
     @Override
@@ -65,7 +140,7 @@ public class CustomLinkedList<E> implements List<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return new CustomLinkedListIterator();
+        return new CustomIterator();
     }
 
     @Override
@@ -145,8 +220,26 @@ public class CustomLinkedList<E> implements List<E> {
         return true;
     }
 
+    public void print() {
+        for (E element : this) {
+            System.out.print(element);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        CustomLinkedList<Integer> intList = new CustomLinkedList();
+        intList.add(42);
+        intList.add(38);
+        intList.addAll(Arrays.asList(1, 2, 2, 3, 3, 1));
+        intList.print();
+        intList.retainAll(Arrays.asList(1, 2));
+        intList.print();
+    }
+
     /*
-      !!! Implement methods below Only if you know what you are doing !!!
+      Extra methods
      */
     @Override
     public boolean containsAll(Collection<?> c) {
@@ -158,98 +251,176 @@ public class CustomLinkedList<E> implements List<E> {
         return true;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        if (index < 0 || index > this.size())
+            throw new IndexOutOfBoundsException();
+
+        if (index == this.size()) {
+            for (E object : c) {
+                if (!this.add(object))
+                    return false;
+            }
+            return true;
+        }
+
+        ListNode<E> curr = header.getNext();
+        ListNode<E> oldLast = header.getPrev();
+        int currIndex = 0;
+        while (curr != header) {
+            if (currIndex == index) {
+                ListNode<E> prev = curr.getPrev();
+                prev.setNext(header);
+                header.setPrev(prev);
+                // add elements from collection
+                for (E object : c) {
+                    if (!this.add(object))
+                        return false;
+                }
+                // add rest tail
+                ListNode<E> newLast = header.getPrev();
+                newLast.setNext(curr);
+                curr.setPrev(newLast);
+                header.setPrev(oldLast);
+                break;
+
+            }
+            ++currIndex;
+            curr = curr.getNext();
+        }
+        return true;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        ListIterator<E> it = this.listIterator();
+        while (it.hasNext()) {
+            if (c.contains(it.next())) {
+                it.remove();
+            }
+        }
+        return true;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        ListIterator<E> it = this.listIterator();
+        while (it.hasNext()) {
+            if (!c.contains(it.next())) {
+                it.remove();
+            }
+        }
+        return true;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public void add(int index, E element) {
+        addAll(index, Arrays.asList(element));
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public E remove(int index) {
-        return null;
+        if (index < 0 || index > (this.size() - 1))
+            throw new IndexOutOfBoundsException();
+        ListNode<E> curr = header.getNext();
+        int currIndex = 0;
+        E removedElement = null;
+        while (curr != header) {
+            if (currIndex == index) {
+                removedElement = curr.getData();
+                ListNode<E> prev = curr.getPrev();
+                ListNode<E> next = curr.getNext();
+                prev.setNext(next);
+                next.setPrev(prev);
+                break;
+            }
+            ++currIndex;
+            curr = curr.getNext();
+        }
+        return removedElement;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        E element = (E) o;
+        int index = size() - 1;
+        ListNode<E> curr = header.getPrev();
+        while (curr != header) {
+            if (curr.getData().equals(element))
+                return index;
+            curr = curr.getPrev();
+            --index;
+        }
+        return -1;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        return new CustomListIterator();
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public ListIterator<E> listIterator(int index) {
-        return null;
+        if (index < 0 || index > (this.size() - 1))
+            throw new IndexOutOfBoundsException();
+        ListIterator<E> result = new CustomListIterator();
+        while (result.nextIndex() != index) {
+            result.next();
+        }
+        return result;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return null;
+        if (fromIndex < 0 || toIndex > size() || fromIndex > toIndex)
+            throw new IndexOutOfBoundsException();
+        CustomLinkedList<E> resultList = new CustomLinkedList();
+        ListIterator<E> it = this.listIterator(fromIndex);
+        while (it.nextIndex() != toIndex) {
+            resultList.add(it.next());
+        }
+        return resultList;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] result = new Object[this.size()];
+        int index = 0;
+        for (E element : this) {
+            result[index++] = element;
+        }
+        return result;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a == null)
+            throw new NullPointerException();
+        int index = 0;
+        if (a.length < this.size()) {
+            T[] result = (T[]) Array.newInstance(a[0].getClass(), this.size());
+            for (E element : this) {
+                result[index++] = (T) element;
+            }
+            return result;
+        }
+
+        for (E element : this) {
+            a[index++] = (T) element;
+        }
+
+        if (a.length > this.size()) {
+            a[index] = null;
+        }
+        
+        return a;
     }
 
-    /**
-     * Do not implement
-     */
     @Override
     public E set(int index, E element) {
-        return null;
+        ListIterator<E> it = this.listIterator(index);
+        E prevElement = it.next();
+        it.set(element);
+        return prevElement;
     }
 }
