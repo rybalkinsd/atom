c7bc7b829d0e2416dde9bfd8bcf22c7e6268c056
atash0804 <atash0804@yandex.ru>
2018-03-15 14:01:13 +0300

    Delete BCMain.java

--- homeworks/HW2/BCMain.java
+++
@@ -1,9 +0,0 @@
-package bulls;
-public class BCMain {
-    public static void main(String[] args) {
-        BCInterface cur_game = new BCInterface();
-        do {
-            cur_game.play();
-        } while (cur_game.active());
-    }
-}
