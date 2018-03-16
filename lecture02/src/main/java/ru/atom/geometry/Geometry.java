2b3a272239aa7bf3e79fe79b7573b6b101be4b98
Ivan Marchuk <ivans.marchuk@gmail.com>
2018-03-16 12:01:41 +0100

    Homework02

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -24,7 +24,8 @@ public final class Geometry {
      * @return new Bar
      */
 
-    public static Collider createBar(int firstCornerX, int firstCornerY, int secondCornerX, int secondCornerY) {
+    public static Collider createBar(int firstCornerX, int firstCornerY,
+                                     int secondCornerX, int secondCornerY) {
         return new Bar(firstCornerX, firstCornerY, secondCornerX, secondCornerY);
 
         //throw new UnsupportedOperationException();
