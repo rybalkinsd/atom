b3d74c404c41a51f30b3a315a4d72a5a361c5f89
Raschudesny <rifkinsm@gmail.com>
2018-03-16 16:02:41 +0300

    Rollback all branch changes by git reset --hard HEAD ~5

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,42 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+
+    private E data;
+    private ListNode<E> next;
+    private ListNode<E> prev;
+
+    public ListNode(E element) {
+        this.data = element;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public E getData() {
+        return this.data;
+    }
+
+    public void setData(E newdata) {
+        this.data = newdata;
+    }
+
+
+    public ListNode<E> getNext() {
+        return next;
+    }
+
+    public ListNode<E> getPrev() {
+        return prev;
+    }
+
+    public void setNext(ListNode<E> nextNode) {
+        this.next = nextNode;
+    }
+
+    public void setPrev(ListNode<E> prevNode) {
+
+        this.prev = prevNode;
+    }
+
+
 }
