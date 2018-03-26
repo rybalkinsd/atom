6ea1b6a7d3dbccc14e377cb9bc9d84438e46f637
olgaM <ololowl11235@gmail.com>
2018-03-26 20:43:36 +0300

    base

--- web_hackaton/src/main/java/ru/atom/chat/ChatController.java
+++ web_hackaton/src/main/java/ru/atom/chat/ChatController.java
@@ -82,7 +82,12 @@ public class ChatController {
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity logout(@RequestParam("name") String name) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("No such name  :(");
+        }
+        usersOnline.remove(name);
+        messages.add("[" + name + "] logged out");
+        return ResponseEntity.ok().build();
     }
 
 
@@ -94,7 +99,13 @@ public class ChatController {
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+    public ResponseEntity say(@RequestParam("name") String name, @RequestParam("msg") String msg,
+                              @RequestParam("day") String day, @RequestParam("day") String month,
+                              @RequestParam("day") String year) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("No such name  :(");
+        }
+        messages.add("[" + name + " : " + day + "." + month + "." + year + "] " + msg);
+        return ResponseEntity.ok().build();
     }
 }
