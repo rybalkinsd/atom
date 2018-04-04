6f1f518ea05af2034ce38ed4a9690a001b3704a7
a.pomosov <alpieex@gmail.com>
2018-04-04 15:28:06 +0300

    lectures 6 7

--- lecture06/src/main/java/ru/atom/dao/DbConnector.java
+++ lecture06/src/main/java/ru/atom/dao/DbConnector.java
@@ -19,9 +19,9 @@ class DbConnector {
     private static final String URL;
     private static final String HOST = "54.224.37.210";
     private static final int PORT = 5432;
-    private static final String DB_NAME = "atomN";
-    private static final String USER = "atomN";
-    private static final String PASSWORD = "atomN";
+    private static final String DB_NAME = "chatdb_atom1";
+    private static final String USER = "atom1";
+    private static final String PASSWORD = "atom1";
 
     static {
         try {
