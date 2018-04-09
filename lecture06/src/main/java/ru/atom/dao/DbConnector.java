34ad11fe1c2835748daa606300fc89c8a58c9f4c
a.pomosov <alpieex@gmail.com>
2018-04-09 16:54:57 +0300

    h2 migration

--- lecture06/src/main/java/ru/atom/dao/DbConnector.java
+++ lecture06/src/main/java/ru/atom/dao/DbConnector.java
@@ -15,28 +15,23 @@ import java.sql.SQLException;
 class DbConnector {
     private static final Logger log = LoggerFactory.getLogger(DbConnector.class);
 
-    private static final String URL_TEMPLATE = "jdbc:postgresql://%s:%d/%s";
-    private static final String URL;
-    private static final String HOST = "54.224.37.210";
-    private static final int PORT = 5432;
-    private static final String DB_NAME = "chatdb_atomN";
-    private static final String USER = "atomN";
-    private static final String PASSWORD = "atomN";
+    //private static final String URL_TEMPLATE = "jdbc:h2://%s:%d/%s";
+    static final String DB_URL = "jdbc:h2:~/test";
+    private static final String USER = "atom";
+    private static final String PASSWORD = "";
 
     static {
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             log.error("Failed to load jdbc driver.", e);
             System.exit(-1);
         }
-
-        URL = String.format(URL_TEMPLATE, HOST, PORT, DB_NAME);
         log.info("Success. DbConnector init.");
     }
 
     static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
     }
 
     private DbConnector() { }
