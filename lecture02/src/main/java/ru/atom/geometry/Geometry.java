22c9912a082a37be114a3db7cbd1414d96e0410c
lightokhv <admin_u@mail.ru>
2018-05-03 18:03:53 +1000

    fix

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -21,18 +21,19 @@ public final class Geometry {
      * (It is not relevant, which opposite corners you choose to define bar)
      * @return new Bar
      */
+
     public static Collider createBar(int firstCornerX, int firstCornerY, int secondCornerX, int secondCornerY) {
-       return new Bar(firstCornerX, firstCornerY, secondCornerX, secondCornerY);
-        // throw new UnsupportedOperationException();
+       
+        return new Bar(firstCornerX, firstCornerY, secondCornerX, secondCornerY);
     }
 
     /**
      * 2D point
      * @return new Point
      */
+
     public static Collider createPoint(int x, int y) {
 
         return new Point(x, y);
-        //throw new UnsupportedOperationException();
     }
 }
