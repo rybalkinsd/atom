be4858307e24d36d98c023ed948af3fc3b54ca31
Alex Tkach <AlyoshaOzero@gmail.com>
2018-05-09 14:01:45 +0300

    Remarks after code review.

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -37,12 +37,19 @@ public class Point implements Collider {
 
         // cast from Object to Point
         Point point = (Point) o;
-
         return this.x == point.x && this.y == point.y;
     }
 
     @Override
     public boolean isColliding(Collider other) {
-        return this.equals(other);
+        if (other instanceof Point) {
+            return this.equals(other);
+        } else if (other instanceof Bar) {
+            Bar bar = (Bar) other;
+            return (this.x >= bar.getFirstCornerPoint().getX() && this.x <= bar.getSecondCornerPoint().getX())
+                    && (this.y >= bar.getFirstCornerPoint().getY() && this.y <= bar.getSecondCornerPoint().getY());
+        }
+
+        return false;
     }
 }
