754f3b600817b4b8eeed2ef22d249f43e39f32e6
Stanislav Mosyagin <mosstnslv@gmail.com>
2018-07-10 14:19:47 +0500

    hw is done

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -1,12 +1,15 @@
 package ru.atom;
 
+import javax.swing.text.LabelView;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * In this assignment you need to implement the following util methods.
  * Note:
- *  throw new UnsupportedOperationException(); - is just a stub
+ * throw new UnsupportedOperationException(); - is just a stub
  */
 public class Util {
-
 
 
     /**
@@ -16,7 +19,16 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int result = 0;
+        if (values.length > 0) {
+            result = values[0];
+            for (int i = 1; i < values.length; i++) {
+                if (result < values[i]) {
+                    result = values[i];
+                }
+            }
+        }
+        return result;
     }
 
     /**
@@ -26,7 +38,13 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        int result = 0;
+
+        for (int i: values) {
+            result += i;
+        }
+
+        return result;
     }
 
 
