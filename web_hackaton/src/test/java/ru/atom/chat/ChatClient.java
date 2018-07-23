d8d94cbceeaede1630114bc722eceea658b64b9b
Alex Tkach <AlyoshaOzero@gmail.com>
2018-07-23 15:31:33 +0300

    My web hackaton improvements

--- web_hackaton/src/test/java/ru/atom/chat/ChatClient.java
+++ web_hackaton/src/test/java/ru/atom/chat/ChatClient.java
@@ -3,11 +3,10 @@ package ru.atom.chat;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.RequestBody;
 
 import java.io.IOException;
-
 
 public class ChatClient {
     private static final OkHttpClient client = new OkHttpClient();
@@ -21,7 +20,6 @@ public class ChatClient {
                 .post(RequestBody.create(mediaType, "name=" + name))
                 .url(PROTOCOL + HOST + PORT + "/chat/login")
                 .build();
-
         return client.newCall(request).execute();
     }
 
@@ -31,15 +29,24 @@ public class ChatClient {
                 .url(PROTOCOL + HOST + PORT + "/chat/chat")
                 .addHeader("host", HOST + PORT)
                 .build();
-
         return client.newCall(request).execute();
     }
 
     public static Response viewOnline() throws IOException {
-        throw new UnsupportedOperationException();
+        Request request = new Request.Builder()
+                .get()
+                .url(PROTOCOL + HOST + PORT + "/chat/online")
+                .addHeader("host", HOST + PORT)
+                .build();
+        return client.newCall(request).execute();
     }
 
-    public static Response say(String name, String msg) {
-        throw new UnsupportedOperationException();
+    public static Response say(String name, String msg) throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request = new Request.Builder()
+                .post(RequestBody.create(mediaType, "name=" + name + "&msg=" + msg))
+                .url(PROTOCOL + HOST + PORT + "/chat/say")
+                .build();
+        return client.newCall(request).execute();
     }
 }
