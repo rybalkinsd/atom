44e5ef12816b295eaed49e87cc57966453e42f4a
Aleksandr <aleksandrtolcih@gmail.com>
2018-08-27 17:12:12 +0300

    fix custom linked list

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,37 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    private E item;
+    private ListNode<E> next;
+    private ListNode<E> prev;
+
+    public ListNode(ListNode<E> prev, E item, ListNode<E> next) {
+        this.item = item;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public E getItem() {
+        return item;
+    }
+
+    public void setItem(E item) {
+        this.item = item;
+    }
+
+    public ListNode<E> getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode<E> next) {
+        this.next = next;
+    }
+
+    public ListNode<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(ListNode<E> prev) {
+        this.prev = prev;
+    }
 }
