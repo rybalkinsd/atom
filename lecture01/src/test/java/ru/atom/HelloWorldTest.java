fc45b3422ba5e5e14246923c79d61b2789a3ee32
a.pomosov <alpieex@gmail.com>
2018-09-04 19:24:41 +0300

    Refactor lecture01, Cleanup gradle configs

--- lecture01/src/test/java/ru/atom/HelloWorldTest.java
+++ lecture01/src/test/java/ru/atom/HelloWorldTest.java
@@ -2,15 +2,11 @@ package ru.atom;
 
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.IsNot.not;
+import static junit.framework.Assert.assertEquals;
 
 public class HelloWorldTest {
     @Test
-    public void getHelloWorld() throws Exception {
-        assertThat(HelloWorld.getHelloWorld(), is(equalTo("Hello, World!")));
-        assertThat(HelloWorld.getHelloWorld(), is(not(equalTo("Some strange string"))));
+    public void getHelloWorld() {
+        assertEquals("WRONG STRING", HelloWorld.getHelloWorld());
     }
 }
