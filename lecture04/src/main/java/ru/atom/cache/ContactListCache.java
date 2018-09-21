24534c8687734c9d34a5adbdd5cfd88d92645c5a
vgalichin <v.galichin@gmail.com>
2018-09-21 10:16:24 +0300

    lecture04 finished

--- lecture04/src/main/java/ru/atom/cache/ContactListCache.java
+++ lecture04/src/main/java/ru/atom/cache/ContactListCache.java
@@ -1,33 +1,40 @@
 package ru.atom.cache;
 
-
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * What about Map?
  */
 public class ContactListCache extends AbstractCache<Person, List<? extends Person>> {
+    Map<Person, List<? extends Person>> cache;
+
     public ContactListCache(int capacity) {
         super(capacity);
+        cache = new ConcurrentHashMap<>(this.capacity);
     }
 
     @Override
     public boolean put(Person person, List<? extends Person> people) {
-        throw new UnsupportedOperationException();
+        cache.put(person, people);
+        capacity = getSize();
+        return true;
     }
 
     @Override
     public List<? extends Person> get(Person person) {
-        throw new UnsupportedOperationException();
+        return cache.get(person);
     }
 
     @Override
     public int getSize() {
-        throw new UnsupportedOperationException();
+        return cache.size();
     }
 
     private boolean removeAny() {
-        throw new UnsupportedOperationException();
+        cache.clear();
+        capacity = getSize();
+        return true;
     }
-
 }
