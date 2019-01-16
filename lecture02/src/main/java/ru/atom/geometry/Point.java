5fb083e8a67bc9d0302608a87d75cd0e87e5bb95
Sergey <oneso.null@icloud.com>
2019-01-16 12:02:28 +0300

    fix lecture02

--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,18 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
-    // fields
-    // and methods
+public class Point implements Collider {
+
+    private int x = 0;
+    private int y = 0;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
 
     /**
      * @param o - other object to check equality with
@@ -20,6 +29,12 @@ public class Point /* super class and interfaces here if necessary */ {
         Point point = (Point) o;
 
         // your code here
-        throw new UnsupportedOperationException();
+        return this.x == point.getX() &&
+                this.y == point.getY();
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        return this.equals(other);
     }
 }
