b9be0f0e3c47ce3283ed715f1cebb44c28b9f404
daiverg <43170010+daiverg@users.noreply.github.com>
2019-03-03 17:28:58 +0100

    Revert "Revert "Update HelloWorld.java""
    This reverts commit 3fe6774be499fbfd473d64f50b0630281fe14426.

--- lecture01/src/main/java/ru.atom/HelloWorld.java
+++ lecture01/src/main/java/ru.atom/HelloWorld.java
@@ -8,4 +8,10 @@ public class HelloWorld {
     public static String getHelloWorld() {
         return "Hello, World!";
     }
+    public static String getHelloWorld() {
+        return "Hell111o, World!";
+    }
+    public static String getHelloWorld() {
+        return "Hello, Wo2222rld!";
+    }
 }
