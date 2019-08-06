6de586424466a43e1a03373585313f928133a802
Alex <lexei90a@gmail.com>
2019-08-07 00:00:51 +0400

    Started hw2

---
+++ homeworks/HW2/src/main/java/ru/atom/bullsandcows/App.java
@@ -0,0 +1,8 @@
+package ru.atom.bullsandcows;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bulls and Cows game!");
+    }
+}
---
+++ homeworks/HW2/src/test/java/ru/atom/bullsandcows/AppTest.java
@@ -0,0 +1,9 @@
+package ru.atom.bullsandcows;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class AppTest {
+
+    private App app;
+}
