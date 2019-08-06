1217537e6895209f4618a7eddd699f850f4e101e
Alex <lexei90a@gmail.com>
2019-08-06 18:02:28 +0400

    Completed lecture 3

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -1,61 +1,135 @@
 package ru.atom.list;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 
 public class CustomLinkedList<E> implements List<E> {
 
+    private int size;
+    private ListNode<E> sentinel;
+
+    public CustomLinkedList() {
+        size = 0;
+        sentinel = new ListNode<>(null, null, null);
+        sentinel.setPrev(sentinel);
+        sentinel.setNext(sentinel);
+    }
+
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        throw new UnsupportedOperationException();
+        ListNode<E> ptr = sentinel.getNext();
+        while (ptr != sentinel) {
+            if (ptr.getValue().equals(o)) {
+                return true;
+            }
+            ptr = ptr.getNext();
+        }
+        return false;
+    }
+
+    private class CustomIterator implements Iterator<E> {
+        private ListNode<E> current;
+
+        CustomIterator() {
+            current = sentinel.getNext();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != sentinel;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            E nextItem = current.getValue();
+            current = current.getNext();
+            return nextItem;
+        }
     }
 
     @Override
     public Iterator<E> iterator() {
-        throw new UnsupportedOperationException();
+        return new CustomIterator();
     }
 
     @Override
     public boolean add(E e) {
-        throw new UnsupportedOperationException();
+        size++;
+        ListNode<E> newNode = new ListNode<>(sentinel.getPrev(), e, sentinel);
+        sentinel.getPrev().setNext(newNode);
+        sentinel.setPrev(newNode);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
+        ListNode<E> ptr = sentinel.getNext();
+        while (ptr != sentinel && !ptr.getValue().equals(o)) {
+            ptr = ptr.getNext();
+        }
+        if (ptr == sentinel) {
+            return false;
+        }
+        size--;
+        ptr.getPrev().setNext(ptr.getNext());
+        ptr.getNext().setPrev(ptr.getPrev());
+        return true;
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        size = 0;
+        sentinel.setNext(sentinel);
+        sentinel.setPrev(sentinel);
     }
 
     @Override
     public E get(int index) {
-        throw new UnsupportedOperationException();
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        E target = null;
+        for (E element : this) {
+            if (index == 0) {
+                target = element;
+                break;
+            }
+            index--;
+        }
+        return target;
     }
 
     @Override
     public int indexOf(Object o) {
-        throw new UnsupportedOperationException();
+        int index = 0;
+        for (E element : this) {
+            if (element.equals(o)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException();
+        for (E element : c) {
+            add(element);
+        }
+        return true;
     }
 
 
