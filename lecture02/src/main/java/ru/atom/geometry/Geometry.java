bf46ee263c4894fc4449221a2b27493d59f1d1aa
sotorag <50326275+sotorag@users.noreply.github.com>
2019-08-06 09:42:44 +0300

    Update Geometry.java

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -1,8 +1,5 @@
 package ru.atom.geometry;
 
-import static java.lang.Integer.max;
-import static java.lang.Integer.min;
-
 /**
  *  ^ Y
  *  |
@@ -13,8 +10,6 @@ import static java.lang.Integer.min;
  */
 
 public final class Geometry {
-    private Geometry() {
-    }
     /**
      * Bar is a rectangle, which borders are parallel to coordinate axis
      * Like selection bar in desktop, this bar is defined by two opposite corners
@@ -23,14 +18,10 @@ public final class Geometry {
      *
      * @return new Bar
      */
+    public static Collider createBar(int firstCornerX, int firstCornerY, int secondCornerX, int secondCornerY) {
 
-    public static Collider createBar(int firstX, int firstY, int secondX, int secondY) {
-
-        Collider collider = new Bar();
-
-        ((Bar) collider).setDownLeftPoint((Point)createPoint(min(firstX,secondX),min(firstY,secondY)));
-        ((Bar) collider).setUpRightPoint((Point)createPoint(max(firstX,secondX),max(firstY,secondY)));
-        return collider;
+        Collider bar = new Bar(firstCornerX,firstCornerY,secondCornerX,secondCornerY);
+        return bar;
     }
 
     /**
@@ -39,11 +30,7 @@ public final class Geometry {
      * @return new Point
      */
     public static Collider createPoint(int x, int y) {
-        
-        Collider point = new Point();
-
-        ((Point) point).setX(x);
-        ((Point) point).setY(y);
+        Collider point = new Point(x,y);
         return point;
     }
 }
