406c686f9e7cd9c3451e018af33877ddeb37bff7
Alex <lexei90a@gmail.com>
2019-08-07 18:25:25 +0400

    completed homework 2

--- homeworks/HW2/src/main/java/ru/atom/bullsandcows/App.java
+++ homeworks/HW2/src/main/java/ru/atom/bullsandcows/App.java
@@ -1,8 +1,32 @@
 package ru.atom.bullsandcows;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(App.class);
+
+    private static List<String> getWords(String filename) {
+        List<String> words = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                words.add(line);
+            }
+        } catch (IOException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+        return words;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Bulls and Cows game!");
+        List<String> words = getWords("dictionary.txt");
+        Game game = new Game(words);
+        game.start();
     }
 }
--- homeworks/HW2/src/test/java/ru/atom/bullsandcows/AppTest.java
+++
@@ -1,9 +0,0 @@
-package ru.atom.bullsandcows;
-
-import org.junit.Test;
-import org.junit.Before;
-
-public class AppTest {
-
-    private App app;
-}
