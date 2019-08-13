ee0a222b05bfdd355f43a8e14eb2577fc738eede
sotorag <50326275+sotorag@users.noreply.github.com>
2019-08-13 10:19:08 +0300

    Update Bull.java

--- homeworks/HW2/src/main/java/ru/atom/Bull.java
+++ homeworks/HW2/src/main/java/ru/atom/Bull.java
@@ -6,8 +6,6 @@ public class Bull {
     private int bull=0;
     public void count(ArrayList in,ArrayList word) {
         for (int i = 0; i < word.size(); i++) {
-            System.out.println(in);
-            System.out.print(word);
             if (word.get(i).equals(in.get(i))) {
                 bull++;
             }
