89839b92bd37ea11a2c3cc2de6f2720df3da1fc3
sotorag <50326275+sotorag@users.noreply.github.com>
2019-08-15 09:03:56 +0300

    Update Winner.java

--- homeworks/HW2/src/main/java/ru/atom/Winner.java
+++ homeworks/HW2/src/main/java/ru/atom/Winner.java
@@ -1,11 +1,8 @@
 package main.java.ru.atom;
 
 public class Winner {
-    public int winner(int size,int bull,int chance) {
-        if(bull==size) {
-            System.out.println("Congratulations!!! You win");
-            return 0;
-        }
-        return chance;
+    public void winner() {
+        System.out.println("Congratulations!!! You win");
+        brake;
     }
 }
