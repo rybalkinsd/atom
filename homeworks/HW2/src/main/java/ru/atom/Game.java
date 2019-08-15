c87dd67ed926adfe6674358638279310b573757c
sotorag <50326275+sotorag@users.noreply.github.com>
2019-08-15 09:10:51 +0300

    Update Game.java

--- homeworks/HW2/src/main/java/ru/atom/Game.java
+++ homeworks/HW2/src/main/java/ru/atom/Game.java
@@ -4,21 +4,16 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class Game {
-    private int chance;
-
-    private ArrayList<Character> in;
-    private ArrayList<Character> word = new ArrayList<Character>(Word.cowsAndBulls().chars()
-            .mapToObj(e -> (char) e)
-            .collect(Collectors.toList()));
-
     public void game() {
-        chance = 10;
-
-        for (; chance > 0; chance--) {
+        for (int chance = 10; chance > 0; chance--) {
+            ArrayList<Character> word = new ArrayList<Character>(Word.cowsAndBulls().chars()
+                                                                 .mapToObj(e -> (char) e)
+                                                                 .collect(Collectors.toList()));
+            
             System.out.println("You have " + chance + "chance:");
 
             CorrectInput correctInput = new CorrectInput();
-            in = correctInput.correctInput(word.size());
+            ArrayList<Character> in = correctInput.correctInput(word.size());
             
             Bull bull = new Bull();
             bull.count(in, word);
@@ -29,10 +24,7 @@ public class Game {
             System.out.println("bulls:" + bull.getBull() + "cows:" + cow.getCow());
 
             GameOver gameOver = new GameOver();
-            gameOver.gameOver(word, bull.getBull(), chance);
-
-            Winner winner = new Winner();
-            chance = winner.winner(word.size(), bull.getBull(), chance);
+            gameOver.gameOver(word, chance);
         }
     }
 }
