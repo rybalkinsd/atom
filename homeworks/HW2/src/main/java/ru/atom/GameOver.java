917480e265b30e7579a724a6cdd186254ab87681
sotorag <50326275+sotorag@users.noreply.github.com>
2019-08-15 09:10:00 +0300

    Update GameOver.java

--- homeworks/HW2/src/main/java/ru/atom/GameOver.java
+++ homeworks/HW2/src/main/java/ru/atom/GameOver.java
@@ -3,8 +3,8 @@ package main.java.ru.atom;
 import java.util.ArrayList;
 
 public class GameOver {
-    public void gameOver(ArrayList word,int bull,int chance) {
-        if (chance == 1 && bull != word.size()) {
+    public void gameOver(ArrayList word,int chance) {
+        if (chance == 1) {
             System.out.println("Game over");
             System.out.print("Answer:");
             word.forEach(o -> System.out.print(o));
