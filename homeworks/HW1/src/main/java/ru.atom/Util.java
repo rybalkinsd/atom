50cdeef195fd1dd5a1d22dc681a1f210291b3c09
Alex <lexei90a@gmail.com>
2019-12-06 21:24:41 +0400

    completed homework 1

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -1,13 +1,10 @@
 package ru.atom;
 
 /**
- * In this assignment you need to implement the following util methods.
- * Note:
- *  throw new UnsupportedOperationException(); - is just a stub
+ * In this assignment you need to implement the following util methods. Note:
+ * throw new UnsupportedOperationException(); - is just a stub
  */
 public class Util {
-
-
 
     /**
      * Returns the greatest of {@code int} values.
@@ -16,7 +13,13 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = Integer.MIN_VALUE;
+        for (int val : values) {
+            if (val > max) {
+                max = val;
+            }
+        }
+        return max;
     }
 
     /**
@@ -26,8 +29,11 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        long sum = 0;
+        for (int val : values) {
+            sum += val;
+        }
+        return sum;
     }
-
 
 }
