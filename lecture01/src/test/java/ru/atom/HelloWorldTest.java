fdf2fb0403c7f795c10a7018d4516a52a3e3fb90
AgafonovK <konstantinagaf@gmail.com>
2020-11-26 15:21:39 +0500

    'myfirst1'

--- lecture01/src/test/java/ru/atom/HelloWorldTest.java
+++ lecture01/src/test/java/ru/atom/HelloWorldTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class HelloWorldTest {
+
     @Test
     public void getHelloWorld() {
         assertEquals("Hello, World!", HelloWorld.getHelloWorld());
